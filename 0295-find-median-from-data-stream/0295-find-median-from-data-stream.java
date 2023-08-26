963c83c8a948e61655b584401233b71e49d1333c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-08-26 12:13:23 +0530

    Time: 93 ms (80.49%), Space: 66.3 MB (86.56%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -8,8 +8,7 @@ class MedianFinder {
     }
     
     public void addNum(int num) {
-        
-              if (!minHeap.isEmpty() && num < minHeap.peek()) {
+           if (!minHeap.isEmpty() && num < minHeap.peek()) {
             maxHeap.offer(num);
             if (maxHeap.size() > minHeap.size() + 1) {
                 minHeap.offer(maxHeap.poll());
@@ -24,7 +23,7 @@ class MedianFinder {
     }
     
     public double findMedian() {
-        if(minHeap.size()==maxHeap.size()){
+        if((minHeap.size()+maxHeap.size())%2==0){
             return (minHeap.peek()+maxHeap.peek())/2.0;}
         else{
             if(minHeap.size()>maxHeap.size())
