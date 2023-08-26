cb120fae5b6ea2783fa252e1967f113424c06389
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-08-26 08:06:01 +0530

    Time: 31 ms (66.26%), Space: 59.1 MB (72.21%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        Deque<Integer> dq = new ArrayDeque<Integer>();
+        
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty()&&dq.peekFirst()<i-k+1)
+                dq.pollFirst();
+            
+            while(!dq.isEmpty()&&nums[dq.peekLast()]<nums[i])
+                dq.pollLast();
+            
+            dq.offer(i);
+            
+            if(i-k+1>=0)
+                res[i-k+1]=nums[dq.peekFirst()];
+        
+        }
+        
+        
+        return res;
+    }
+}
