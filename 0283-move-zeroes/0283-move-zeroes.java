6bcbc511190a8aba090e04206545eaf251ff18bd
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-08-26 08:47:46 +0530

    Time: 2 ms (54.28%), Space: 45 MB (79.97%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int countZero = 0;
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)
+                countZero++;
+            else{
+                if(countZero!=0){
+                    nums[i-countZero]=nums[i];
+                    nums[i]=0;
+                }
+            }
+        }
+        
+        
+    }
+}
