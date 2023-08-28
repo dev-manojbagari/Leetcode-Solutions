92daf2b6d0317f113ea6215911e206def0cfa423
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-08-28 16:42:43 +0530

    Time: 8 ms (64.43%), Space: 54.7 MB (97.81%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    public List<List<String>> partition(String s) {
+        List<List<String>> list = new ArrayList<>();
+        partition(0,s,list,new ArrayList<>());
+        return list;
+    }
+    
+    
+    void partition(int index,String s,List<List<String>> list,List<String> tempList){
+        if(index==s.length()){
+            list.add(new ArrayList<>(tempList));
+            return;
+        }
+            
+        
+        for(int i=index;i<s.length();i++){
+            if(isPalindrome(s.substring(index,i+1))){
+                tempList.add(s.substring(index,i+1));
+                partition(i+1,s,list,tempList);
+                tempList.remove(tempList.size()-1);
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    boolean isPalindrome(String s){
+        int left=0,right=s.length()-1;
+        
+        while(left<=right){
+            if(s.charAt(left)!=s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
