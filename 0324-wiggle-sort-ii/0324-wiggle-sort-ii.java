00eb809f4d8164b18ef86990933194a3f427f5ae
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-08-29 14:25:56 +0530

    Time: 62 ms (8.62%), Space: 47.4 MB (11.53%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,60 +1,67 @@
 class Solution {
-public void wiggleSort(int[] nums) {
-    int n = nums.length;
-    int median = findKthLargest(nums, (n + 1) / 2);
 
-    int left = 0, i = 0, right = n - 1;
-    while (i <= right) {
-        int virtualIdx = virtualIndex(i, n);
-        if (nums[virtualIdx] > median) {
-            swap(nums, virtualIndex(left++, n), virtualIdx);
-            i++;
-        } else if (nums[virtualIdx] < median) {
-            swap(nums, virtualIndex(right--, n), virtualIdx);
-        } else {
-            i++;
-        }
-    }
-}
-    private int virtualIndex(int i, int n) {
-    return (2 * i + 1) % (n | 1);
-}
-    
-    
-      int findKthLargest(int[] nums, int k) {
-        int left=0,right=nums.length-1;
-        int ans = -1;
-        while(left<=right){
-            int index = quickSelect(nums,left,right);
-            
-            if(index==k-1){
-                ans = nums[index];
-                break;
-            }else if(index<k-1){
-                left=index+1;
-            }else{
-                right=index-1;
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        int median = findKthLargest(nums, (n + 1) / 2);
+
+        int left = 0, i = 0, right = n - 1;
+        while (i <= right) {
+            int virtualIdx = virtualIndex(i, n);
+            if (nums[virtualIdx] > median) {
+                swap(nums, virtualIndex(left++, n), virtualIdx);
+                i++;
+            } else if (nums[virtualIdx] < median) {
+                swap(nums, virtualIndex(right--, n), virtualIdx);
+            } else {
+                i++;
             }
         }
-        
-        return ans;  
     }
-  
-    int quickSelect(int[] nums,int start,int end){
-        
-        int pivot = nums[end];
-        for(int curr=start;curr<end;curr++){
-            if(nums[curr]>pivot)
-                swap(nums,curr,start++);
+
+    private int virtualIndex(int i, int n) {
+        // Check if n is even
+        boolean isEven = n % 2 == 0;
+
+        // If n is even, add 1 to it to make it odd
+        if (isEven) {
+            n = n + 1;
         }
-        swap(nums,start,end);
+
+        // Calculate the virtual index
+        return (2 * i + 1) % n;
+    }
+
+    int findKthLargest(int[] nums, int k) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int index = quickSelect(nums, left, right);
+
+            if (index == k - 1) {
+                ans = nums[index];
+                break;
+            } else if (index < k - 1) {
+                left = index + 1;
+            } else {
+                right = index - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int quickSelect(int[] nums, int start, int end) {
+        int pivot = nums[end];
+        for (int curr = start; curr < end; curr++) {
+            if (nums[curr] > pivot) swap(nums, curr, start++);
+        }
+        swap(nums, start, end);
         return start;
     }
-    
-    void swap(int[] nums,int i,int j){
+
+    void swap(int[] nums, int i, int j) {
         int temp = nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-    
 }
