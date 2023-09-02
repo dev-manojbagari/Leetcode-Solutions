9170e0902ccfa1606261bfd7d06e20fa9e170f9f
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-02 08:38:19 +0530

    Time: 24 ms (7.05%), Space: 43.8 MB (41.11%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,18 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
+        if(head==null)
+            return head;
+        ListNode slow = head,fast=head.next;
+        while(fast!=null&&fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) {
-                ListNode slow2 = head;
-                while (slow2 != slow) {
-                    slow = slow.next;
-                    slow2 = slow2.next;
-                }
-                return slow;
+            if(slow ==fast){
+                
+                return cycleHead(head,slow);
             }
         }
+        
         return null;
     }
+    
+    ListNode cycleHead(ListNode head,ListNode node){
+        int len = findCycleLength(node);
+        
+        ListNode ptr1 = head;
+        ListNode ptr2 = head;
+        int count =0;
+        while(count!=len){
+            ptr2=ptr2.next;
+            count++;
+        }
+        
+        while(ptr2.next!=ptr1){
+            System.out.println(2);
+            ptr1=ptr1.next;
+            ptr2=ptr2.next;
+        }
+        
+        return ptr1;
+        
+    }
+    
+    
+    int findCycleLength(ListNode node){
+        ListNode curr = node;
+        int count =0;
+        while(curr.next!=node){
+            curr= curr.next;
+            count++;
+        }
+        return count;
+    }
+    
+    
+    
 }
