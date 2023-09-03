b77bd5b2ebe4ff2cf0ca06e335e995b14835ac7b
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-03 07:19:29 +0530

    Time: 1 ms (98.98%), Space: 43.8 MB (81.19%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,34 +1,25 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
         List<int[]> res = new ArrayList<>();
-        if(intervals.length==0){
-            res.add(newInterval);
-            return res.toArray(new int[1][2]);
-        }
-       
-        List<int[]> list = new ArrayList<>();
-        
-        for(int[] interval:intervals)
-            list.add(interval);
-        list.add(newInterval);
-        
-        list.sort((a,b)->a[0]-b[0]);
-        
-        for(int[] interval:list){
-            System.out.println(Arrays.toString(interval));
-        } 
-       
-        res.add(list.get(0));
-        
-        for(int i=1;i<list.size();i++){
-            int[] lastInterval = res.get(res.size()-1);
-            if(lastInterval[1]>=list.get(i)[0]){
-                lastInterval[1]=Math.max(lastInterval[1],list.get(i)[1]);
-            }else{
-                res.add(list.get(i));
+        boolean intervalInserted = false;
+        for(int i=0;i<n;i++) {
+            if(intervals[i][1] < newInterval[0]) {
+                res.add(intervals[i]);
+            } else if(intervals[i][0] > newInterval[1]) {
+                if(!intervalInserted) {
+                    res.add(newInterval);
+                    intervalInserted = true;
+                }
+                res.add(intervals[i]);
+            } else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             }
         }
-        
-        return res.toArray(new int[res.size()-1][2]);
+        if(!intervalInserted) {
+            res.add(newInterval);
+        }
+        return res.toArray(new int[0][]); // convert to 2D array
     }
 }
