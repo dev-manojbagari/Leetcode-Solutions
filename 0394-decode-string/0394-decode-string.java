eaf8a15f9ab595822255907c740bf1f9dc57cc47
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-05 17:33:24 +0530

    Time: 1 ms (79.54%), Space: 40 MB (98.19%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,87 +1,59 @@
 import java.util.*;
 
 class Solution {
-//     public String decodeString(String s) {
-//         Stack<Integer> countStack = new Stack<>();
-//         Stack<StringBuilder> stringStack = new Stack<>();
+     public String decodeString(String s) {
+             Stack<Integer> countStack = new Stack<>();
+             Stack<StringBuilder> stringStack = new Stack<>();
         
-//         int k=0;
-//         StringBuilder curr= new StringBuilder();
-//         for(char c:s.toCharArray()){
+        int k=0;
+        StringBuilder curr= new StringBuilder();
+        for(char c:s.toCharArray()){
             
-//             if(Character.isDigit(c)){
-//                 k = k*10+c;
-//             }else if(c=='['){
-//                 countStack.push(k);
-//                 stringStack.push(curr);
-//                 k=0;
-//                 curr=new StringBuilder();
-//             }else if(c==']'){
-//                 String temp = curr.toString();
-//                 curr=stringStack.pop();
-                
-//                 for(int i=countStack.pop();i>=0;i--)
-//                     curr.append(temp);
-//             }else{
-//                 curr.append(c);
-//             }
-            
-//         }
-        
-//         return curr.toString();
-        
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   public String decodeString(String s) {
-        Deque<Integer> countStack = new ArrayDeque<>();
-        Deque<StringBuilder> stringStack = new ArrayDeque<>();
-        StringBuilder cur = new StringBuilder();
-        int k = 0;
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                k = k * 10 + ch - '0';
-            } else if (ch == '[') {
+            if(Character.isDigit(c)){
+                k = k*10+c-'0';
+            }else if(c=='['){
                 countStack.push(k);
-                stringStack.push(cur);
-                cur = new StringBuilder();
-                k = 0;
-            } else if (ch == ']') {
-                StringBuilder tmp = cur;
-                cur = stringStack.pop();
-                for (k = countStack.pop(); k > 0; --k) cur.append(tmp);
-            } else cur.append(ch);
+                stringStack.push(curr);
+                k=0;
+                curr=new StringBuilder();
+            }else if(c==']'){
+                StringBuilder temp = curr;
+                curr=stringStack.pop();
+                int j=countStack.pop();
+                for(int i=0;i<j;i++)
+                    curr.append(temp);
+            }else{
+                curr.append(c);
+            }
+            
         }
-        return cur.toString();
-    }
+        
+        return curr.toString();
+        
+    
+
+   // public String decodeString(String s) {
+   //      Deque<Integer> countStack = new ArrayDeque<>();
+   //      Deque<StringBuilder> stringStack = new ArrayDeque<>();
+   //      StringBuilder cur = new StringBuilder();
+   //      int k = 0;
+   //      for (char ch : s.toCharArray()) {
+   //          if (Character.isDigit(ch)) {
+   //              k = k * 10 + ch - '0';
+   //          } else if (ch == '[') {
+   //              countStack.push(k);
+   //              stringStack.push(cur);
+   //              cur = new StringBuilder();
+   //              k = 0;
+   //          } else if (ch == ']') {
+   //              StringBuilder tmp = cur;
+   //              cur = stringStack.pop();
+   //              for (k = countStack.pop(); k > 0; --k) cur.append(tmp);
+   //          } else cur.append(ch);
+   //      }
+   //      return cur.toString();
+   //  }
 
 
+}
 }
