e0c2db3ffe0ee7387ec7a5c687167ba5115dfc18
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-05 14:53:12 +0530

    Time: 2 ms (100.00%), Space: 43.8 MB (10.82%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -1,35 +1,47 @@
-import java.util.HashMap;
-
-public class Solution {
-
-    int result = 0;
-
-    public int pathSum(TreeNode root, int sum) {
-        HashMap<Long, Integer> cache = new HashMap<>();
-        cache.put(0l, 1);
-        dfs(root, sum, 0, cache);
-        return result;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count =0;
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null)
+            return 0;
+        Map<Long,Integer> map = new HashMap<>();
+        map.put(0l,1);
+        pathSum(root,targetSum,0,map);
+        return count;
     }
-
-    private void dfs(TreeNode node, int target, long currPathSum, HashMap<Long, Integer> cache) {  // Use long instead of int
-        if (node == null) {
+    
+    void pathSum(TreeNode root,int targetSum,long curPathSum,Map<Long,Integer> map ){
+        if(root==null)
             return;
+        
+        curPathSum += root.val;
+        long oldPathSum = curPathSum-targetSum;
+        count += map.getOrDefault(oldPathSum,0);
+        
+        map.put(curPathSum,map.getOrDefault(curPathSum,0)+1);
+        
+        pathSum(root.left,targetSum,curPathSum,map);
+        pathSum(root.right,targetSum,curPathSum,map);
+        
+        if(map.get(curPathSum)==1){
+            map.remove(curPathSum);
+        }else{
+            map.put(curPathSum,map.get(curPathSum)-1);
         }
 
-        currPathSum = currPathSum+node.val;  // Use Math.addExact() to handle integer overflow
-        long oldPathSum = currPathSum - target;
-
-        result += cache.getOrDefault(oldPathSum, 0);
-        cache.put(currPathSum, cache.getOrDefault(currPathSum, 0) + 1);
-
-        dfs(node.left, target, currPathSum, cache);
-        dfs(node.right, target, currPathSum, cache);
-
-        // When moving to a different branch, the currPathSum is no longer available, hence remove one.
-        if (cache.get(currPathSum) == 1) {
-            cache.remove(currPathSum);
-        } else {
-            cache.put(currPathSum, cache.get(currPathSum) - 1);
-        }
     }
 }
