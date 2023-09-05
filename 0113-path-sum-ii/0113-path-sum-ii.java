7c4748d2beb685d1da24e1a7b167b28d8cc91618
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-05 13:45:17 +0530

    Time: 2 ms (22.53%), Space: 46.2 MB (7.73%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null)
+            return list;
+        preOrder(root, targetSum, 0, list, new ArrayList<>());
+        return list;
+    }
+
+    private void preOrder(TreeNode root, int targetSum, int curSum, List<List<Integer>> list, List<Integer> tempList) {
+        if (root == null)
+            return;
+        curSum += root.val;
+        tempList.add(root.val);
+
+        if (root.left == null && root.right == null) {
+            if (targetSum == curSum) {
+                list.add(new ArrayList<>(tempList));
+            }
+        } else {
+            preOrder(root.left, targetSum, curSum, list, new ArrayList<>(tempList));
+            preOrder(root.right, targetSum, curSum, list, new ArrayList<>(tempList));
+        }
+    }
+}
