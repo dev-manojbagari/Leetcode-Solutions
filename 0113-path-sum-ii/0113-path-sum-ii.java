aa2f1a24a1d273d49fe967ec9b2f28574399108b
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-05 13:50:43 +0530

    Time: 1 ms (99.98%), Space: 43.8 MB (70.80%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -1,45 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-           List<List<Integer>> list = new ArrayList<>();
-           if(root==null)
-                return list;
-            preOrder(root,targetSum,0,list,new ArrayList());
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null)
+            return list;
+        preOrder(root, targetSum, 0, list, new ArrayList<>());
         return list;
     }
-    
-     private void preOrder(TreeNode root, int targetSum,int curSum,List<List<Integer>> list,List<Integer> tempList) {
-            if(root==null)
-                return;
-         curSum += root.val;
-            tempList.add(root.val);
-         
-           if(root.left==null&&root.right==null){
-                if(targetSum-curSum==0){
-                        list.add(new ArrayList(tempList));
-             
-                }
-           }else{
 
-            preOrder(root.left,targetSum,curSum,list,tempList);
-            preOrder(root.right,targetSum,curSum,list,tempList);
-           }
-            tempList.remove(tempList.size()-1);
-         
-         
+    private void preOrder(TreeNode root, int targetSum, int curSum, List<List<Integer>> list, List<Integer> tempList) {
+        if (root == null)
+            return;
+        curSum += root.val;
+        tempList.add(root.val);
+
+        if (root.left == null && root.right == null) {
+            if (targetSum == curSum) {
+                list.add(new ArrayList<>(tempList));
+            }
+        } else {
+            preOrder(root.left, targetSum, curSum, list, tempList);
+            preOrder(root.right, targetSum, curSum, list,tempList);
         }
-   }
+        tempList.remove(tempList.size()-1);
+    }
+}
