ded158066b0fe3577c520a3bd5d518b25d0014f2
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-06 05:39:14 +0530

    Time: 9 ms (88.93%), Space: 43.8 MB (80.72%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,40 +1,49 @@
-class Solution {
+public class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int[] pCount = new int[26];
-        int[] sCount = new int[26];
-        List<Integer> output = new ArrayList<>();
-
-        if (s.length() < p.length()) {
-            return output;
-        }
-
-        for (int i = 0; i < p.length(); i++) {
-            pCount[p.charAt(i) - 'a']++;
-            sCount[s.charAt(i) - 'a']++;
-        }
-
-        int i = p.length();
-        while (i < s.length()) {
-            if (isAnagram(pCount, sCount)) {
-                output.add(i - p.length());
-            }
-            sCount[s.charAt(i - p.length()) - 'a']--;
-            sCount[s.charAt(i) - 'a']++;
-            i++;
-        }
-        if (isAnagram(pCount, sCount)) {
-            output.add(i - p.length());
-        }
-
-        return output;
-    }
-
-    private boolean isAnagram(int[] pCount, int[] sCount) {
-        for (int i = 0; i < 26; i++) {
-            if (pCount[i] != sCount[i]) {
-                return false;
-            }
-        }
-        return true;
+       ArrayList<Integer> soln = new ArrayList<Integer>();
+       
+       if (s.length() == 0 || p.length() == 0 || s.length() < p.length()){
+           return new ArrayList<Integer>();
+       }
+       
+       int[] chars = new int[26];
+       for (Character c : p.toCharArray()){
+           chars[c-'a']++;
+       }
+       
+       int start = 0, end = 0, len = p.length(), diff = len;
+       
+       char temp;
+       for (end = 0; end < len; end++){
+           temp = s.charAt(end);
+           chars[temp-'a']--;
+           if (chars[temp-'a'] >= 0){
+               diff--;
+           }
+       }
+       
+       if (diff == 0){
+           soln.add(0);
+       }
+       
+       while (end < s.length()){
+           temp = s.charAt(start);
+           if (chars[temp-'a'] >= 0){
+               diff++;
+           }
+           chars[temp-'a']++;
+           start++;
+           temp = s.charAt(end);
+           chars[temp-'a']--;
+           if (chars[temp-'a'] >= 0){
+               diff--;
+           }
+           if (diff == 0){
+               soln.add(start);
+           }
+           end++;
+       }
+       
+       return soln;
     }
 }
