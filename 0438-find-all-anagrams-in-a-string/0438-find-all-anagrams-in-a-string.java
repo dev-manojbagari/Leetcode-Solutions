8d07cd8388ad9ddadbb8be623d57870b43edefd3
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-06 06:47:26 +0530

    Time: 9 ms (88.93%), Space: 44.3 MB (44.67%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,49 +1,45 @@
-public class Solution {
+class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-       ArrayList<Integer> soln = new ArrayList<Integer>();
-       
-       if (s.length() == 0 || p.length() == 0 || s.length() < p.length()){
-           return new ArrayList<Integer>();
-       }
-       
-       int[] chars = new int[26];
-       for (Character c : p.toCharArray()){
-           chars[c-'a']++;
-       }
-       
-       int start = 0, end = 0, len = p.length(), diff = len;
-       
-       char temp;
-       for (end = 0; end < len; end++){
-           temp = s.charAt(end);
-           chars[temp-'a']--;
-           if (chars[temp-'a'] >= 0){
-               diff--;
-           }
-       }
-       
-       if (diff == 0){
-           soln.add(0);
-       }
-       
-       while (end < s.length()){
-           temp = s.charAt(start);
-           if (chars[temp-'a'] >= 0){
-               diff++;
-           }
-           chars[temp-'a']++;
-           start++;
-           temp = s.charAt(end);
-           chars[temp-'a']--;
-           if (chars[temp-'a'] >= 0){
-               diff--;
-           }
-           if (diff == 0){
-               soln.add(start);
-           }
-           end++;
-       }
-       
-       return soln;
+        List<Integer> list = new ArrayList<>();
+        
+        if(p.length()>s.length())
+            return list;
+        
+        int[] cmap= new int[26];
+        
+        for(char c:p.toCharArray()){
+            cmap[c-'a']++;
+        }
+        int diff = p.length();
+        for(int i=0;i<p.length();i++){
+            char c = s.charAt(i);
+            cmap[c-'a']--;
+            if(cmap[c-'a']>=0)
+                diff--;
+        }
+        
+        if(diff==0){
+            list.add(0);
+        }
+        
+        int start=0, end=p.length();
+        for(;end<s.length();end++, start++)
+        {
+            char c = s.charAt(start);
+            if(cmap[c-'a'] >= 0)
+                diff++;
+            cmap[c-'a']++;
+            
+            c = s.charAt(end);
+            cmap[c-'a']--;
+            if(cmap[c-'a'] >= 0)
+                diff--;
+
+            // Check if diff is zero after adjusting the character counts
+            if(diff==0)
+                list.add(start+1);
+        }
+        
+        return list;
     }
 }
