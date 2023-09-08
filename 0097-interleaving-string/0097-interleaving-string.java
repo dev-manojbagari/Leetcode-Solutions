82497238b4f3c6b58a940cba873dbd2ed9f0f409
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-08 08:51:51 +0530

    Time: 3 ms (61.59%), Space: 40.7 MB (46.38%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,23 +1,29 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if(s1.length()+s2.length()!=s3.length())
+        int r = s1.length();
+        int c = s2.length();
+        int l = s3.length();
+        
+        if (r + c != l) {
             return false;
+        }
         
-        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+        boolean[] dp = new boolean[c + 1];
+        dp[0] = true;
         
-        for(int i=0;i<=s1.length();i++){
-            for(int j=0;j<=s2.length();j++){
-                if(i==0&&j==0)
-                    dp[i][j]=true;
-                else if(i==0)
-                    dp[i][j] = dp[i][j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1);
-                else if(j==0)
-                    dp[i][j] = dp[i-1][j]&&s1.charAt(i-1)==s3.charAt(i+j-1);
-                else
-                    dp[i][j] = dp[i-1][j]&&s1.charAt(i-1)==s3.charAt(i+j-1)||dp[i][j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1);
+        for (int j = 1; j <= c; j++) {
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+        
+        for (int i = 1; i <= r; i++) {
+            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+            
+            for (int j = 1; j <= c; j++) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i - 1 + j)) ||
+                        (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i - 1 + j));
             }
         }
         
-        return dp[s1.length()][s2.length()];
+        return dp[c];
     }
 }
