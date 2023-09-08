e07619d091433be91d2e01f4eb27ab676dadba5c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-08 07:52:27 +0530

    Time: 16 ms (5.11%), Space: 85 MB (5.32%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length()+s2.length()!=s3.length())
+            return false;
+        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s3.length()][s3.length()][s3.length()]);
+    }
+    
+    private boolean isInterleave(int i,int j,int k,String s1,String s2,String s3,Boolean[][][] cache){
+        if(k==s3.length())
+            return i==s1.length()&&j==s2.length();
+        
+        if(cache[i][j][k]!=null)
+            return cache[i][j][k];
+        
+        boolean ans =false;
+        
+        if(i<s1.length()&&s1.charAt(i)==s3.charAt(k)&&j<s2.length()&&s2.charAt(j)==s3.charAt(k))
+            ans= isInterleave(i+1,j,k+1,s1,s2,s3,cache)||isInterleave(i,j+1,k+1,s1,s2,s3,cache);
+        else if(i<s1.length()&&s1.charAt(i)==s3.charAt(k))
+            ans= isInterleave(i+1,j,k+1,s1,s2,s3,cache);
+        else if(j<s2.length()&&s2.charAt(j)==s3.charAt(k))
+            ans= isInterleave(i,j+1,k+1,s1,s2,s3,cache);
+        else 
+            ans= false;
+        
+        return cache[i][j][k]=ans;
+    }
+}
