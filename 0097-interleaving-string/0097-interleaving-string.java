1e8ea55334e9832aac4d7254fc8ded5287ad04c4
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-08 07:53:09 +0530

    Time: 7 ms (23.75%), Space: 64 MB (5.32%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length()+s2.length()!=s3.length())
             return false;
-        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s3.length()][s3.length()][s3.length()]);
+        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
     }
     
     private boolean isInterleave(int i,int j,int k,String s1,String s2,String s3,Boolean[][][] cache){
