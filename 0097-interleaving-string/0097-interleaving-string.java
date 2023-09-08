944dad95a0aa6f53a642080bbdc527789723c04d
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-08 08:38:59 +0530

    Time: 5 ms (37.00%), Space: 40.8 MB (46.38%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -2,27 +2,22 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length()+s2.length()!=s3.length())
             return false;
-        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
-    }
-    
-    private boolean isInterleave(int i,int j,int k,String s1,String s2,String s3,Boolean[][][] cache){
-        if(k==s3.length())
-            return i==s1.length()&&j==s2.length();
         
-        if(cache[i][j][k]!=null)
-            return cache[i][j][k];
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
         
-        boolean ans =false;
+        for(int i=0;i<=s1.length();i++){
+            for(int j=0;j<=s2.length();j++){
+                if(i==0&&j==0)
+                    dp[i][j]=true;
+                else if(i==0)
+                    dp[i][j] = dp[i][j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1);
+                else if(j==0)
+                    dp[i][j] = dp[i-1][j]&&s1.charAt(i-1)==s3.charAt(i+j-1);
+                else
+                    dp[i][j] = dp[i-1][j]&&s1.charAt(i-1)==s3.charAt(i+j-1)||dp[i][j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1);
+            }
+        }
         
-        if(i<s1.length()&&s1.charAt(i)==s3.charAt(k)&&j<s2.length()&&s2.charAt(j)==s3.charAt(k))
-            ans= isInterleave(i+1,j,k+1,s1,s2,s3,cache)||isInterleave(i,j+1,k+1,s1,s2,s3,cache);
-        else if(i<s1.length()&&s1.charAt(i)==s3.charAt(k))
-            ans= isInterleave(i+1,j,k+1,s1,s2,s3,cache);
-        else if(j<s2.length()&&s2.charAt(j)==s3.charAt(k))
-            ans= isInterleave(i,j+1,k+1,s1,s2,s3,cache);
-        else 
-            ans= false;
-        
-        return cache[i][j][k]=ans;
+        return dp[s1.length()][s2.length()];
     }
 }
