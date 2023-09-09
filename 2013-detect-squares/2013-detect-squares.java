3273613486ebe3ff8fa3aaefca3d57168b9bf625
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-09 14:09:36 +0530

    Time: 49 ms (90.75%), Space: 57.2 MB (6.94%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,20 +1,36 @@
-class DetectSquares { // 128 ms, faster than 60.00%
-    int[][] cntPoints = new int[1001][1001];
+class DetectSquares {
+    int[][] count = new int[1001][1001];
     List<int[]> points = new ArrayList<>();
-
-    public void add(int[] p) {
-        cntPoints[p[0]][p[1]] += 1;
-        points.add(p);
+    public DetectSquares() {
+        
     }
-
-    public int count(int[] p1) {
-        int x1 = p1[0], y1 = p1[1], ans = 0;
-        for (int[] p3 : points) {
-            int x3 = p3[0], y3 = p3[1];
-            if (Math.abs(x1-x3) == 0 || Math.abs(x1-x3) != Math.abs(y1-y3))
-                continue; // Skip empty square or invalid square point!
-            ans += cntPoints[x1][y3] * cntPoints[x3][y1];
+    
+    public void add(int[] point) {
+        if(count[point[0]][point[1]]==0){
+            points.add(point);
         }
-        return ans;
+        
+        count[point[0]][point[1]]++;
+    }
+    public int count(int[] point) {
+    int totalSquare=0;
+        int x1 = point[0],y1=point[1];
+        
+        for(int[] curPoint : points){
+            int x3 = curPoint[0],y3=curPoint[1];
+            if(x3!=x1&&y3!=y1&&Math.abs(x3-x1)==Math.abs(y3-y1))
+                totalSquare += count[x1][y3]*count[x3][y1]*count[x3][y3];
+        }
+        
+        
+        return totalSquare;        
+        
     }
 }
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares obj = new DetectSquares();
+ * obj.add(point);
+ * int param_2 = obj.count(point);
+ */
