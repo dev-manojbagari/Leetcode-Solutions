242e0c7022ca0bf34a9cecc0ebed10f7d5224fba
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-09 16:12:51 +0530

    Time: 18 ms (62.88%), Space: 43.9 MB (63.17%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,33 +1,32 @@
-class Solution {
+class Solution  {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         int n= nums.length;
         
         for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue; // skip duplicates
             for(int j=i+1;j<n;j++){
-           // if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-               int k = j+1;
-               int l = n-1;
+                if(j>i+1 && nums[j]==nums[j-1]) continue; // skip duplicates
+                int k = j+1;
+                int l = n-1;
                 
-               while(k<l){
-                   long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
-                   if(sum==target){
-                       res.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k],nums[l])));
-                       k++;
-                       l--;
-                   }else if(sum<target){
-                       k++;
-                   }else{
-                       l--;
-                   }
-                       
-               }
-                
+                while(k<l){
+                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum==target){
+                        res.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        while(k<l && nums[k]==nums[k+1]) k++; // skip duplicates
+                        while(k<l && nums[l]==nums[l-1]) l--; // skip duplicates
+                        k++;
+                        l--;
+                    }else if(sum<target){
+                        k++;
+                    }else{
+                        l--;
+                    }
+                }
             }
         }
-
-        
-        return new ArrayList<>(res);
+        return res;
     }
 }
