d519ba7b0ffa78a716c43a3fa68528cf9efca365
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-09 16:14:21 +0530

    Time: 16 ms (78.16%), Space: 44 MB (63.17%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,32 +1,40 @@
-class Solution  {
+class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         int n= nums.length;
         
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue; // skip duplicates
+                if(i>0&&nums[i]==nums[i-1])
+                    continue;
             for(int j=i+1;j<n;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue; // skip duplicates
-                int k = j+1;
-                int l = n-1;
+                if (j > i+1 && nums[j] == nums[j - 1]) 
+                    continue;
+               int k = j+1;
+               int l = n-1;
                 
-                while(k<l){
-                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
-                    if(sum==target){
-                        res.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                        while(k<l && nums[k]==nums[k+1]) k++; // skip duplicates
-                        while(k<l && nums[l]==nums[l-1]) l--; // skip duplicates
-                        k++;
-                        l--;
-                    }else if(sum<target){
-                        k++;
-                    }else{
-                        l--;
-                    }
-                }
+               while(k<l){
+                   long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
+                   if(sum==target){
+                       res.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k],nums[l])));
+                       k++;
+                       while(k<l&&nums[k]==nums[k-1])
+                           k++;
+                       l--;
+                       while(k<l&&nums[l]==nums[l+1])
+                           l--;
+                   }else if(sum<target){
+                       k++;
+                   }else{
+                       l--;
+                   }
+                       
+               }
+                
             }
         }
+
+        
         return res;
     }
 }
