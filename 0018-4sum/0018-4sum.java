e4f2e2bfc390238df940f3c68935faf8d578625b
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-09 16:08:56 +0530

    Time: 22 ms (45.97%), Space: 44.2 MB (44.87%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Set<List<Integer>> res = new HashSet<>();
+        Arrays.sort(nums);
+        int n= nums.length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+               int k = j+1;
+               int l = n-1;
+                
+               while(k<l){
+                   long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
+                   if(sum==target){
+                       res.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k],nums[l])));
+                       k++;
+                       l--;
+                   }else if(sum<target){
+                       k++;
+                   }else{
+                       l--;
+                   }
+                       
+               }
+                
+            }
+        }
+
+        
+        return new ArrayList<>(res);
+    }
+}
