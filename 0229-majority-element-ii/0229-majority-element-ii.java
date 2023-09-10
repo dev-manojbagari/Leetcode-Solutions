5099b0f9ff39df672a05195acc028b58189ac79e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-10 17:01:17 +0530

    Time: 4 ms (57.94%), Space: 47 MB (21.38%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Integer num1 = null;
+        Integer num2 = null;
+        int count1=0,count2=0;
+        
+        for(Integer num:nums){
+            if(num.equals(num1)){
+                count1++;
+            }else if(num.equals(num2)){
+                count2++;
+            }else if(count1==0){
+                num1=num;
+                count1=1;
+            }else if(count2==0){
+                num2=num;
+                count2=1;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1=0;count2=0;
+        int n = nums.length;
+        for(int num:nums){
+            if(num1==num)
+                count1++;
+            else if(num2==num)
+                count2++;
+        }
+        List<Integer> list = new ArrayList<>();
+        if(count1>n/3)
+            list.add(num1);
+        if(count2>n/3)
+            list.add(num2);
+        
+        return list;
+        
+    }
+}
