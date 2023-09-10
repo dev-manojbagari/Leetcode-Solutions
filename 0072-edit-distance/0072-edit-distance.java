ee10c897b2665977bd089ddb9988f620ba4cddec
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-10 12:23:37 +0530

    Time: 5 ms (69.13%), Space: 43.5 MB (67.00%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,24 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        return minDistance(word1.length()-1,word2.length()-1,word1,word2,new Integer[word1.length()][word2.length()]);
-    }
-    
-    int minDistance(int i,int j,String word1,String word2,Integer[][] cache){
-        if(j==-1){
-            return i+1;
-        }else if(i==-1){
-            return j+1;
+        int n1 = word1.length(),n2=word2.length();
+        
+        int[][] dp = new int[n1+1][n2+1];
+        
+        
+        for(int i=0;i<=word1.length();i++){
+            for(int j=0;j<=word2.length();j++){
+                if(i==0)
+                    dp[i][j] = j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.min(1+dp[i-1][j-1],Math.min(1+dp[i-1][j],1+dp[i][j-1]));
+                }
+            }
         }
         
-        if(cache[i][j]!=null)
-            return cache[i][j];
-        
-        int ans =0;
-        if(word1.charAt(i)==word2.charAt(j))
-            ans =minDistance(i-1,j-1,word1,word2,cache);
-        else
-            ans= Math.min(1+minDistance(i,j-1,word1,word2,cache),Math.min(1+minDistance(i-1,j,word1,word2,cache),1+minDistance(i-1,j-1,word1,word2,cache)));
-        
-        return cache[i][j]=ans;
+        return dp[n1][n2];
     }
 }
