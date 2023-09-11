9853a3a594c1404c11e76a3dc62a13bc092d4fa5
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-11 08:40:40 +0530

    Time: 587 ms (70.47%), Space: 84.7 MB (85.76%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int findMaximumXOR(int[] nums) {
         int max=0,mask=0;
-        
         for(int i=31;i>=0;i--){
             mask = mask|(1<<i);
             Set<Integer> set = new HashSet<>();
@@ -9,17 +8,20 @@ class Solution {
                 set.add(num&mask);
             }
             
-            int greedyTry = max|1<<i;
+            int greedyTry = max|(1<<i);
             
-            for(int leftPartOfNum:set){
-                if(set.contains(leftPartOfNum^greedyTry)){
+            for(int num:set){
+                if(set.contains(num^greedyTry)){
                     max=greedyTry;
                     break;
                 }
             }
+            
+            
+            
+            
         }
         
         return max;
-        
     }
 }
