39859ec066c98a135c66f74f6674010e3c2c3c4e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-11 14:06:47 +0530

    Time: 1 ms (99.40%), Space: 42.7 MB (91.23%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,36 +14,18 @@
  * }
  */
 class Solution {
-   public int widthOfBinaryTree(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        Queue<Integer> qIndex = new LinkedList<>();
-        q.add(root);
-        qIndex.add(1); //store index, assuming root's index is 1
-        int max = 0;
-        while(!q.isEmpty())
-        {
-            int size = q.size();
-            int start = 0, end = 0;
-            for(int i=0; i<size; i++)
-            {
-                TreeNode node = q.remove();
-                int index = qIndex.remove();
-                if(i==0) start = index; //start and end index for each level
-                if(i==size-1) end = index;
-                if(node.left!=null)
-                {
-                    q.add(node.left);
-                    qIndex.add(2*index);
-                }
-                
-                if(node.right!=null)
-                {
-                    q.add(node.right);
-                    qIndex.add(2*index+1);
-                }
-            }
-            max = Math.max(max, end - start + 1);
-        }
-        return max;    
+ private int max = 1;
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+        List<Integer> startOfLevel = new LinkedList<>();
+        helper(root, 0, 1, startOfLevel);
+        return max;
+    }
+    public void helper(TreeNode root, int level, int index, List<Integer> list) {
+        if (root == null) return;
+        if (level == list.size()) list.add(index);
+        max = Math.max(max, index + 1 - list.get(level));
+        helper(root.left, level + 1, index * 2, list);
+        helper(root.right, level + 1, index * 2 + 1, list);
     }
 }
