e26b9e90e950a6cdd2b9fec346f95730beb31f6a
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-11 14:16:25 +0530

    Time: 1 ms (99.40%), Space: 41.2 MB (99.54%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,18 +14,25 @@
  * }
  */
 class Solution {
- private int max = 1;
+    int maxWidth=0;
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        List<Integer> startOfLevel = new LinkedList<>();
-        helper(root, 0, 1, startOfLevel);
-        return max;
+        List<Integer> startOfLevel = new ArrayList<>();
+        
+        levelOrder(root,startOfLevel,0,1);
+        
+        return maxWidth;
     }
-    public void helper(TreeNode root, int level, int index, List<Integer> list) {
-        if (root == null) return;
-        if (level == list.size()) list.add(index);
-        max = Math.max(max, index + 1 - list.get(level));
-        helper(root.left, level + 1, index * 2, list);
-        helper(root.right, level + 1, index * 2 + 1, list);
+    
+    void levelOrder(TreeNode root,List<Integer> list,int curLevel,int index){
+        if(root==null)
+            return;
+        
+        if(list.size()==curLevel)
+            list.add(index);
+        
+        maxWidth = Math.max(maxWidth , index-list.get(curLevel)+1);
+        
+        levelOrder(root.left,list,curLevel+1,2*index);
+        levelOrder(root.right,list,curLevel+1,2*index+1);
     }
 }
