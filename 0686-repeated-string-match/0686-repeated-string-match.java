fdc37138cf60bd439a104b6c1b8bd2e1ce934308
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-11 14:54:57 +0530

    Time: 1707 ms (5.03%), Space: 41.4 MB (72.93%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -7,12 +7,24 @@ class Solution {
             count++;
         }
         
-        if(sb.toString().contains(b)) return count;
-        else if(sb.append(a).toString().contains(b))return ++count;
+        if(isFound(sb.toString(),b)) return count;
+        else if(isFound(sb.append(a).toString(),b))return count+1;
         else
             return -1;
-            
+    }
+    
+    boolean isFound(String a , String b){
+        for(int i=0;i<a.length();i++){
+             int start = i;
+             int j = 0;
+            while(start<a.length()&&j<b.length()&&a.charAt(start)==b.charAt(j)){
+                start++;
+                j++;
+            }
+            if(j==b.length())
+                return true;
+        }
         
-        
+        return false;
     }
 }
