18953f328dde378751c3a94eaf2746390bb166ad
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-12 15:45:17 +0530

    Time: 7 ms (85.18%), Space: 44 MB (61.76%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,37 +1,35 @@
 class Solution {
- public int calculate(String s) {
-    Stack<Integer> stack = new Stack<Integer>();
-    int result = 0;
-    int number = 0;
-    int sign = 1;
-    for(int i = 0; i < s.length(); i++){
-        char c = s.charAt(i);
-        if(Character.isDigit(c)){
-            number = 10 * number + (int)(c - '0');
-        }else if(c == '+'){
-            result += sign * number;
-            number = 0;
-            sign = 1;
-        }else if(c == '-'){
-            result += sign * number;
-            number = 0;
-            sign = -1;
-        }else if(c == '('){
-            //we push the result first, then sign;
-            stack.push(result);
-            stack.push(sign);
-            //reset the sign and result for the value in the parenthesis
-            sign = 1;   
-            result = 0;
-        }else if(c == ')'){
-            result += sign * number;  
-            number = 0;
-            result *= stack.pop();    //stack.pop() is the sign before the parenthesis
-            result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
-            
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result=0;
+        int sign =1;
+        int number =0;
+        for(char c:s.toCharArray()){
+            if(Character.isDigit(c)){
+                number = number*10+(c-'0');
+            }else if(c=='+'){
+                result += sign*number;
+                number=0;
+                sign=1;
+            }else if(c=='-'){
+                result += sign*number;
+                number=0;
+                sign=-1;
+            }else if(c=='('){
+                stack.push(result);
+                stack.push(sign);
+                result=0;
+                sign=1;
+            }else if(c==')'){
+                result += sign*number;
+                number =0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
         }
+        
+        result += sign*number;
+        
+        return result;
     }
-    if(number != 0) result += sign * number;
-    return result;
-}
 }
