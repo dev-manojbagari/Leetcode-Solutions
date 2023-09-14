6787922dbb42248fe6c63dfa9af0881c019c7d98
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-14 07:52:04 +0530

    Time: 7 ms (70.57%), Space: 40 MB (53.76%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -2,37 +2,35 @@ class Solution {
     public void solveSudoku(char[][] board) {
         solve(board,0,0);
     }
-  
+    
     boolean solve(char[][] board,int row,int col){
         
         for(int i=row;i<9;i++,col=0){
             for(int j=col;j<9;j++){
                 if(board[i][j]=='.'){
                     for(char num='1';num<='9';num++){
-                        if(vaild(board,i,j,num)){
+                        if(isValid(board,i,j,num)){
                             board[i][j]=num;
                             if(solve(board,i,j+1))
                                 return true;
                             board[i][j]='.';
                         }
                     }
-                 return false;
+                    return false;
                 }
             }
-        } 
+        }
         return true;
     }
     
-    boolean vaild(char[][] board,int row,int col,char num){
-        
+    boolean isValid(char[][] board,int row,int col,char num){
         int blockrow = (row/3)*3,blockcol=(col/3)*3;
+        
         for(int i=0;i<9;i++){
-            if(board[row][i]==num||board[i][col]==num||board[(blockrow+i/3)][blockcol+i%3]==num)
+            if(board[row][i]==num||board[i][col]==num||board[blockrow+i/3][blockcol+i%3]==num)
                 return false;
         }
         
         return true;
-        
     }
-    
 }
