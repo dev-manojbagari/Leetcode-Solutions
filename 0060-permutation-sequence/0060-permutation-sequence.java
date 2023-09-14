c03a77d28e1569beab013c6386eda4086d4dc78a
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-14 14:33:01 +0530

    Time: 2 ms (71.05%), Space: 39.9 MB (71.40%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,27 +1,30 @@
 class Solution {
-    String ans = "";
-  public String getPermutation(int n, int k) {
-    int fact[] = new int[]{1,1,2,6,24,120,720,5040,40320,362880};
-    
-    LinkedList<Integer> list = new LinkedList<Integer>();
-    for(int i=1; i<=n; i++) list.add(i);
-    solve(list,n,k,fact);
-    return ans;
-}
-
-public void solve(LinkedList<Integer> list, int n, int k, int[] fact){
-    if(n == 1){
-        ans+=list.get(list.size() - 1);
-        return;
+    String str = "";
+    public String getPermutation(int n, int k) {
+        int fact[] = new int[]{1,1,2,6,24,120,720,5040,40320,362880};
+        LinkedList list = new LinkedList<>();
+        for(int i=1;i<=n;i++)
+            list.add(i);
+        
+        getPermutation(n,k,fact,list);
+        return str;
     }
-    int index = k/fact[n-1];
-
-    if(k%fact[n-1] == 0) index--;
-
-    ans+=Integer.toString(list.get(index));
-    list.remove(index);
-    k = k - fact[n-1]*index;
-
-    solve(list,n-1,k,fact);
-}
+    
+    void getPermutation(int n,int k,int[] fact,LinkedList<Integer> list){
+        if(n==1)
+        {
+            str += list.get(list.size()-1);
+            return;
+        }
+        
+        int index = k/fact[n-1];
+        
+        if(k%fact[n-1]==0)
+            index--;
+        
+        str += Integer.toString(list.get(index));
+        list.remove(index);
+        k = k-index*fact[n-1];
+        getPermutation(n-1,k,fact,list);
+    }
 }
