91996e747ae5bc09d5b0aad262421f074b629b68
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-14 09:11:45 +0530

    Time: 2 ms (89.93%), Space: 44.2 MB (23.22%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -2,37 +2,35 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
-       
         for(char[] row:board)
             Arrays.fill(row,'.');
         
-        solve(res,0,n,board);
-        
+        solve(0,n,board,res);
         return res;
     }
     
-    void solve(List<List<String>> res,int curRow,int n,char[][] board){
+    void solve(int curRow,int n,char[][] board,List<List<String>> res){
         if(curRow==n){
-           res.add(construct(board));
+            res.add(construct(board));
             return;
-        }
+        }        
         
         for(int curCol=0;curCol<n;curCol++){
-            if(check(curRow,curCol,board,n)){
+            if(isValidPosition(curRow,curCol,board)){
                 board[curRow][curCol]='Q';
-                solve(res,curRow+1,n,board);
+                solve(curRow+1,n,board,res);
                 board[curRow][curCol]='.';
             }
         }
+        
     }
-    
-    boolean check(int curRow,int curCol,char[][] board,int n){
-        for(int row=0;row<curRow;row++){
+    boolean isValidPosition(int curRow,int curCol,char[][] board){
+        
+        for(int row=0;row<curRow;row++)
             if(board[row][curCol]=='Q')
                 return false;
-        }
         
-        for(int row=curRow-1,col=curCol+1;row>=0&&col<n;row--,col++){
+        for(int row=curRow-1,col=curCol+1;row>=0&&col<board.length;row--,col++){
             if(board[row][col]=='Q')
                 return false;
         }
@@ -42,14 +40,16 @@ class Solution {
                 return false;
         }
         
+        
         return true;
     }
     
     List<String> construct(char[][] board){
         List<String> list = new ArrayList<>();
-        for(char[] row :board)
+        for(char[] row:board){
             list.add(new String(row));
-       
+        }
+        
         return list;
     }
 }
