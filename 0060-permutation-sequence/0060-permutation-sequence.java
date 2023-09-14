057532d0f51b16a60c762dd2aa68674d71744f89
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-14 14:17:21 +0530

    Time: 2 ms (71.05%), Space: 40.6 MB (20.37%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    String ans = "";
+  public String getPermutation(int n, int k) {
+    int fact[] = new int[]{1,1,2,6,24,120,720,5040,40320,362880};
+    
+    ArrayList<Integer> list = new ArrayList<Integer>(n+1);
+    for(int i=1; i<=n; i++) list.add(i);
+    solve(list,n,k,fact);
+    return ans;
+}
+
+public void solve(ArrayList<Integer> list, int n, int k, int[] fact){
+    if(n == 1){
+        ans+=list.get(list.size() - 1);
+        return;
+    }
+    int index = k/fact[n-1];
+
+    if(k%fact[n-1] == 0) index--;
+
+    ans+=Integer.toString(list.get(index));
+    list.remove(index);
+    k = k - fact[n-1]*index;
+
+    solve(list,n-1,k,fact);
+}
+}
