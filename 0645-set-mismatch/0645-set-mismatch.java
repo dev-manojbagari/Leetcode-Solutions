9375504fc51699494ebfcc5ee651fc8429d0ee71
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-16 20:35:29 +0530

    Time: 3 ms (69.03%), Space: 44.9 MB (31.44%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,41 +1,25 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        int repeating =0,missing=0;
-        for(int i=0;i<n;i++){
-            int index = Math.abs(nums[i])-1;
-           if(nums[index]<0){
-               repeating = index+1;
-           }else{
-               nums[index]=-nums[index];    
-           }
+        int repeating = 0;
+        for(int i=0;i<nums.length;i++){
+            int idx = Math.abs(nums[i])-1;
+            if(nums[idx]<0){
+                repeating = idx+1;
+            }else{
+                nums[idx] = -1*nums[idx];
+            }
         }
         
-        
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                missing = i+1;
+        int missing=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0)
+            {
+                missing= i+1;
                 break;
             }
         }
         
         return new int[]{repeating,missing};
+        
     }
 }
-
-
-   // public int[] findErrorNums(int[] nums) {
-   //      int[] res = new int[2]; // duplicate, missing
-   //      //For each number we found, set nums[number-1] to its negative value (<0)
-   //      for(int i=0; i<nums.length; i++) {
-   //          int idx = Math.abs(nums[i])-1; //since index starts from 0, and the set starts from 1
-   //          if(nums[idx]> 0) nums[idx] = -nums[idx];
-   //          else res[0] = idx+1; //have already been found
-   //      }
-   //      // At this point, only nums[missingNumber-1] > 0
-   //      for(int i=0; i<nums.length; i++) {
-   //          if(nums[i] <0) nums[i] = -nums[i]; //restore the original values
-   //          else res[1]=i+1;//since index starts from 0, and the set starts from 1
-   //      }
-   //      return res;
-   //  }
