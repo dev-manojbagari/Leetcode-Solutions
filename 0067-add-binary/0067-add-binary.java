0287e2851e6e501936539548c51c0e44dab15260
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-16 14:19:28 +0530

    Time: 2 ms (56.67%), Space: 40.8 MB (97.58%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,25 +1,25 @@
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        
-        int carry = 0;
-        
-        while (i >= 0 || j >= 0) {
-            int sum = carry;
-            
-            if (i >= 0) sum += a.charAt(i--) - '0';
-            if (j >= 0) sum += b.charAt(j--) - '0';
-            
-            result.append(sum % 2);
-            carry = sum / 2; // update the carry for next iteration
+        int i = a.length()-1,j=b.length()-1;
+        int sum=0,carry=0;
+        while(i>=0||j>=0){
+            sum = carry;
+            if(i>=0)
+                sum += a.charAt(i)-'0';
+            if(j>=0)
+                sum += b.charAt(j)-'0';
+            sb.append(sum%2);
+            carry = sum/2;
+            i--;
+            j--;
         }
         
-        if (carry != 0)
-            result.append(carry); // append the remaining carry
+        if(carry>0)
+            sb.append(carry);
         
-        return result.reverse().toString(); // reverse because we've built the string in reverse order
+        
+        return sb.reverse().toString();
     }
 }
