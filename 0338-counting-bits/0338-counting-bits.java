5c69c4ff64b734782107f14722c73225eed2b2c7
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-16 17:03:18 +0530

    Time: 2 ms (82.71%), Space: 46.1 MB (85.84%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -7,7 +7,7 @@ class Solution {
             if(i%2==0)
                 res[i]=res[i>>1];
             else
-                res[i]=res[i-1]+1;
+                res[i]=res[i>>1]+1;
         }
         
         return res;
