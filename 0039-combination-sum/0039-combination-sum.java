90fb0f3da99cf6f4d64f8d557bd58b1bd0bcd074
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-18 13:07:36 +0530

    Time: 2 ms (74.87%), Space: 43.9 MB (57.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,37 +1,23 @@
 class Solution {
-    Set<String> set;
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
-        char[] cmap = new char[41];
-        set= new HashSet<>();
-        combinationSum(candidates,target,list,new ArrayList<>(),cmap);;
+        Arrays.sort(nums);
+        combinationSum(nums, target, 0, list, new ArrayList<>());
         return list;
     }
     
-    void combinationSum(int[] candidates,int target,List<List<Integer>> list,List<Integer> tempList,char[] cmap){
-        if(target==0){
-            String key = new String(cmap);
-            if(!set.contains(key)){
-                list.add(new ArrayList<>(tempList));
-                set.add(key);
-            }
+    void combinationSum(int[] nums, int target, int start, List<List<Integer>> list, List<Integer> tempList) {
+        if(target == 0){
+            list.add(new ArrayList<>(tempList));
             return;
         }
         
-        
-        
-        for(int i=0;i<candidates.length;i++){
-            if(target-candidates[i]>=0){
-                tempList.add(candidates[i]);
-                cmap[candidates[i]]++;
-                combinationSum(candidates,target-candidates[i],list,tempList,cmap);
-                cmap[candidates[i]]--;
-                tempList.remove(tempList.size()-1);
+        for(int i = start; i < nums.length; i++){
+            if(target - nums[i] >= 0){
+                tempList.add(nums[i]);
+                combinationSum(nums, target - nums[i], i, list, tempList);
+                tempList.remove(tempList.size() - 1);
             }
-            
         }
-        
-        
-        
     }
 }
