22d5405cecac44418301aa7b543b731204a28429
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-18 13:35:52 +0530

    Time: 2 ms (99.22%), Space: 40.9 MB (96.78%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,6 +1,6 @@
 class Solution {
     public String multiply(String num1, String num2) {
-           if("0".equals(num1)||"0".equals(num2))
+        if(num1.equals("0")||num2.equals("0"))
             return "0";
         int[] res = new int[num1.length()+num2.length()-1];
         
@@ -9,12 +9,15 @@ class Solution {
                 res[i+j] += (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
             }
         }
+        int carry=0;
         for(int i=res.length-1;i>0;i--){
-            res[i-1] += res[i]/10;
-            res[i]=res[i]%10;
+            int sum = res[i]+carry;
+            res[i] = (sum)%10;
+            carry = (sum)/10;
         }
-        // if(carry>0)
-            // res[0] += carry;
+        
+        if(carry>0)
+            res[0] += carry;
         
         StringBuilder sb = new StringBuilder();
         for(int num:res)
