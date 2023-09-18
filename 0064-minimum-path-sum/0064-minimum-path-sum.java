a349e85070a1bfec6628a0a97cbaa2f2e7ad13da
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-18 16:47:27 +0530

    Time: 2 ms (75.35%), Space: 44.2 MB (24.35%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,22 +1,24 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int rows=grid.length,cols=grid[0].length;
-        int[][] dp = new int[rows][cols];
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(i==0&&j==0){
-                    dp[i][j] = grid[i][j];
-                }else if(i==0){
-                    dp[i][j] = grid[i][j]+dp[i][j-1];
-                }else if(j==0){
-                    dp[i][j] = grid[i][j]+dp[i-1][j];
-                }else{
-                    dp[i][j] = grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
-                }
+        int[] t = new int[grid[0].length];
+        
+        t[0]=grid[0][0];
+       
+        for(int i=1;i<t.length;i++){
+            t[i] = t[i-1]+grid[0][i];    
+        }
+        // System.out.println(Arrays.toString(t));
+        for(int row = 1;row<grid.length;row++){
+            for(int col=0;col<grid[0].length;col++){
+                if(col==0)
+                    t[col]= t[col]+grid[row][col];
+                else
+                    t[col] = grid[row][col]+Math.min(t[col-1],t[col]);
             }
+            // System.out.println(Arrays.toString(t));
         }
         
+        return t[t.length-1];
         
-        return dp[rows-1][cols-1];
     }
 }
