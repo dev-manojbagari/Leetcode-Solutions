1164c3185473fe5723a9d455131f56fcd6e986f6
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-19 09:18:40 +0530

    Time: 1 ms (99.93%), Space: 43.7 MB (76.06%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -1,26 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> list = new ArrayList<>();
-        if (root == null)
-            return list;
-        preOrder(root, targetSum, 0, list, new ArrayList<>());
+        List<List<Integer>>  list = new ArrayList<>();
+        
+        pathSum(root,targetSum,list,new ArrayList<>());
+        
         return list;
     }
-
-    private void preOrder(TreeNode root, int targetSum, int curSum, List<List<Integer>> list, List<Integer> tempList) {
-        if (root == null)
+    
+    private void pathSum(TreeNode root,int targetSum,List<List<Integer>> list,List<Integer> tempList){
+        if(root==null)
             return;
-        curSum += root.val;
+        
         tempList.add(root.val);
 
-        if (root.left == null && root.right == null) {
-            if (targetSum == curSum) {
+        if(root.left==null&&root.right==null){
+            if(targetSum-root.val==0){
                 list.add(new ArrayList<>(tempList));
             }
-        } else {
-            preOrder(root.left, targetSum, curSum, list, tempList);
-            preOrder(root.right, targetSum, curSum, list,tempList);
+            
+        }else{
+            pathSum(root.left,targetSum-root.val,list,tempList);
+            pathSum(root.right,targetSum-root.val,list,tempList);
         }
+        
         tempList.remove(tempList.size()-1);
     }
 }
