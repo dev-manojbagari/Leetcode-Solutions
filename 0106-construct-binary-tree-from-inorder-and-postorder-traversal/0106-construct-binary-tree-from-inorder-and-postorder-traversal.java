4418a927318a373e150c1d8a749d9feb558e0b09
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-19 08:56:24 +0530

    Time: 1 ms (97.69%), Space: 43.6 MB (19.68%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,27 +14,29 @@
  * }
  */
 class Solution {
-    int postIndex=0;
+    int postIndex;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        postIndex = postorder.length-1;
         Map<Integer,Integer> inMap = new HashMap<>();
         
         for(int i=0;i<inorder.length;i++){
             inMap.put(inorder[i],i);
         }
-        postIndex=postorder.length-1;
-     return buildTree(postorder,inMap,0,postorder.length-1);   
-    }
         
-    private TreeNode buildTree(int[] postorder,Map<Integer,Integer> inMap,int start,int end){
-         if(postIndex>=postorder.length||start>end)
-               return null;
-    
-         TreeNode node = new TreeNode(postorder[postIndex--]);
-        
-         int index = inMap.get(node.val);
-         node.right = buildTree(postorder,inMap,index+1,end);
-         node.left = buildTree(postorder,inMap,start,index-1);
-                        
-          return node;  
+        return buildTree(postorder,inMap,0,postorder.length-1);
         }
+    
+    private TreeNode buildTree(int[] postorder,Map<Integer,Integer> inMap,int start,int end){
+        if(postIndex<0||start>end)
+            return null;
+        
+        TreeNode node = new TreeNode(postorder[postIndex--]);
+        int index  = inMap.get(node.val);
+        
+        node.right = buildTree(postorder,inMap,index+1,end);
+        node.left =  buildTree(postorder,inMap,start,index-1);
+        
+        return node;
+    }
+    
 }
