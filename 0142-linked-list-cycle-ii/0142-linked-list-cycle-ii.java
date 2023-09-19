599f4645a9c1065634dfcfd0e45584c7585d96cb
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-19 12:59:18 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (6.39%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,28 +11,25 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if(head==null)
-            return head;
-        ListNode slow = head,fast=head;
+        if(head==null||head.next==null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        
         while(fast!=null&&fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
-            if(slow ==fast){
-                ListNode slow2=head;
-                while(slow2!=slow){
-                    slow=slow.next;
-                    slow2=slow2.next;
+            if(fast==slow){
+               ListNode ptr1 = head;
+               ListNode ptr2 = slow;
+               while(ptr1!=ptr2){
+                   ptr1 = ptr1.next;
+                   ptr2= ptr2.next;
                 }
-            return slow;    
+            return ptr2;
             }
-            
         }
         
-        return null;
+            return null;
     }
-    
-
-    
-    
-    
 }
