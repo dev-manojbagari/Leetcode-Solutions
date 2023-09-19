cb2f2f532adfab53310fe3cceb2fb8095a03ea2b
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-19 08:45:36 +0530

    Time: 5 ms (37.05%), Space: 40.8 MB (36.83%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
-    }
-    
-    private boolean isInterleave(int i,int j,int k,String s1,String s2,String s3,Boolean[][][] state){
-        if(i==s1.length()&&j==s2.length()&&k==s3.length())
-            return true;
+         if(s3.length()!=s1.length()+s2.length())
+              return false;
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
         
-        if(state[i][j][k]!=null)
-            return state[i][j][k];
         
-        boolean ans=false;
+        for(int i=0;i<s1.length()+1;i++){
+            for(int j=0;j<s2.length()+1;j++){
+                if(i==0&&j==0)
+                    dp[i][j]=true;
+                else if(i==0)
+                    dp[i][j] = dp[i][j-1]&&s3.charAt(i+j-1)==s2.charAt(j-1);
+                else if(j==0)
+                    dp[i][j] = dp[i-1][j]&&s3.charAt(i+j-1)==s1.charAt(i-1);
+                else
+                    dp[i][j] = dp[i-1][j]&&s3.charAt(i+j-1)==s1.charAt(i-1)|| dp[i][j-1]&&s3.charAt(i+j-1)==s2.charAt(j-1);
+            }
+        }
         
-        if(i<s1.length()&&k<s3.length()&&s1.charAt(i)==s3.charAt(k)&&j<s2.length()&&s2.charAt(j)==s3.charAt(k))
-            ans = isInterleave(i+1,j,k+1,s1,s2,s3,state)|| isInterleave(i,j+1,k+1,s1,s2,s3,state);
-        else if(i<s1.length()&&k<s3.length()&&s1.charAt(i)==s3.charAt(k))
-            ans= isInterleave(i+1,j,k+1,s1,s2,s3,state); 
-        else if(j<s2.length()&&k<s3.length()&&s2.charAt(j)==s3.charAt(k))
-            ans =isInterleave(i,j+1,k+1,s1,s2,s3,state);
-        else
-            ans= false;
-        
-        return state[i][j][k] =ans;
+        return dp[s1.length()][s2.length()];
     }
 }
