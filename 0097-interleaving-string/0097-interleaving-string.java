cfa89786c1d609d46efa1b1ee343a1ba1e396774
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-19 07:45:14 +0530

    Time: 5 ms (37.05%), Space: 61.9 MB (5.27%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,29 +1,26 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int r = s1.length();
-        int c = s2.length();
-        int l = s3.length();
+        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
+    }
+    
+    private boolean isInterleave(int i,int j,int k,String s1,String s2,String s3,Boolean[][][] state){
+        if(i==s1.length()&&j==s2.length()&&k==s3.length())
+            return true;
         
-        if (r + c != l) {
-            return false;
-        }
+        if(state[i][j][k]!=null)
+            return state[i][j][k];
         
-        boolean[] dp = new boolean[c + 1];
-        dp[0] = true;
+        boolean ans=false;
         
-        for (int j = 1; j <= c; j++) {
-            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
-        }
+        if(i<s1.length()&&k<s3.length()&&s1.charAt(i)==s3.charAt(k)&&j<s2.length()&&s2.charAt(j)==s3.charAt(k))
+            ans = isInterleave(i+1,j,k+1,s1,s2,s3,state)|| isInterleave(i,j+1,k+1,s1,s2,s3,state);
+        else if(i<s1.length()&&k<s3.length()&&s1.charAt(i)==s3.charAt(k))
+            ans= isInterleave(i+1,j,k+1,s1,s2,s3,state); 
+        else if(j<s2.length()&&k<s3.length()&&s2.charAt(j)==s3.charAt(k))
+            ans =isInterleave(i,j+1,k+1,s1,s2,s3,state);
+        else
+            ans= false;
         
-        for (int i = 1; i <= r; i++) {
-            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
-            
-            for (int j = 1; j <= c; j++) {
-                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i - 1 + j)) ||
-                        (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i - 1 + j));
-            }
-        }
-        
-        return dp[c];
+        return state[i][j][k] =ans;
     }
 }
