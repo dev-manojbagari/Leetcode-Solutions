2e2fd907403a117452a359520e015a2784cda172
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-19 12:42:43 +0530

    Time: 26 ms (64.72%), Space: 41.9 MB (44.95%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,54 +1,41 @@
 /*
- * Definition for a Node.
- * class Node {
- *     public int val;
- *     public List<Node> neighbors;
- *
- *     public Node() {
- *         val = 0;
- *         neighbors = new ArrayList<Node>();
- *     }
- *
- *     public Node(int _val) {
- *         val = _val;
- *         neighbors = new ArrayList<Node>();
- *     }
- *
- *     public Node(int _val, ArrayList<Node> _neighbors) {
- *         val = _val;
-*          neighbors = _neighbors;
-*      }
-* };
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
 */
 
 class Solution {
-    private HashMap <Node, Node> visited = new HashMap <> ();
-
+    Map<Node ,Node> visited = new HashMap<>();
     public Node cloneGraph(Node node) {
-
-        if (node == null) {
-            return node;
-        }
-
-        // If the node was already visited before.
-        // Return the clone from the visited dictionary.
-        if (visited.containsKey(node)) {
+        if(node==null)
+            return null;
+        
+        if(visited.containsKey(node)){
             return visited.get(node);
         }
-
-        // Create a clone for the given node.
-        // Note that we don't have cloned neighbors as of now, hence [].
-        Node cloneNode = new Node(node.val, new ArrayList());
         
-        // The key is original node and value being the clone node.
-        visited.put(node, cloneNode);
-
+        Node cloneNode = new Node(node.val,new ArrayList<>());
         
-       for(Node neighbor:node.neighbors){
-           cloneNode.neighbors.add(cloneGraph(neighbor));
-       } 
-       
-       return cloneNode;
-
+        visited.put(node,cloneNode);
+        
+        for(Node neighbor:node.neighbors){
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        
+        return cloneNode;
     }
 }
