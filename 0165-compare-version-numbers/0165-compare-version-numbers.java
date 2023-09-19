6661ac9be8bde00ba8497d400be73e6fc522bfab
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-19 16:25:52 +0530

    Time: 1 ms (81.49%), Space: 39.8 MB (99.72%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -3,17 +3,15 @@ class Solution {
         String[] s1 = version1.split("\\.");
         String[] s2 = version2.split("\\.");
         
-        int length = Math.max(s1.length,s2.length);
-        
-        for(int i=0;i<length;i++){
+        for(int i=0;i<Math.max(s1.length,s2.length);i++){
             Integer n1 = i>=s1.length?0:Integer.parseInt(s1[i]);
             Integer n2 = i>=s2.length?0:Integer.parseInt(s2[i]);
-            int compare = n1.compareTo(n2);
-            if(compare!=0)
-                return compare;
+            if(n1==n2)
+                continue;
+            else
+                return n1.compareTo(n2);
             
         }
-        
         
         return 0;
     }
