9b936365d7dafe85b5ebd90b98f4fa601a1afe96
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-20 09:10:31 +0530

    Time: 72 ms (31.90%), Space: 46.7 MB (49.00%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -11,15 +11,15 @@ class Solution {
         boolean[][] dp = new boolean[targetSum+1][nums.length+1];
         
         for(int curSum=0;curSum<targetSum+1;curSum++){
-            for(int index=0;index<=nums.length;index++){
+            for(int curLen=0;curLen<=nums.length;curLen++){
                 if(curSum==0)
-                    dp[curSum][index]=true;
-                else if(index==0)
-                    dp[curSum][index]=false;
-                else if(curSum-nums[index-1]>=0)
-                    dp[curSum][index]= dp[curSum][index-1]||dp[curSum-nums[index-1]][index-1];
+                    dp[curSum][curLen]=true;
+                else if(curLen==0)
+                    dp[curSum][curLen]=false;
+                else if(curSum-nums[curLen-1]>=0)
+                    dp[curSum][curLen]= dp[curSum][curLen-1]||dp[curSum-nums[curLen-1]][curLen-1];
                 else 
-                    dp[curSum][index]= dp[curSum][index-1];
+                    dp[curSum][curLen]= dp[curSum][curLen-1];
             }
         }
         
