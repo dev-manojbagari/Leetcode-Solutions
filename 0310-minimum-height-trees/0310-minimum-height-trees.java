e33176ab5c973e34ec046dbc564ceb9bd4d9d074
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-20 15:16:15 +0530

    Time: 15 ms (92.16%), Space: 55 MB (77.96%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,54 +1,48 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        List<Integer> res = new ArrayList<>();
-        
-        
-        
         if(n==1)
-        {
-            res.add(0);
-            return res;
-        }
+            return Arrays.asList(0);    
         List<List<Integer>> adjList = new ArrayList<>();
         
         for(int i=0;i<n;i++)
-            adjList.add(new ArrayList());
+            adjList.add(new ArrayList<>());
         
         int[] inDegree = new int[n];
         
-        for(int i=0;i<edges.length;i++){
+        for(int i=0;i<edges.length;i++)
+        {
             adjList.get(edges[i][0]).add(edges[i][1]);
             adjList.get(edges[i][1]).add(edges[i][0]);
             inDegree[edges[i][0]]++;
             inDegree[edges[i][1]]++;
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(inDegree[i]==1){
+        
+        Queue<Integer> q  = new LinkedList<>();
+        
+        for(int i=0;i<inDegree.length;i++){
+            if(inDegree[i]==1)
                 q.add(i);
-            }
         }
         
         while(n>2){
-            int size = q.size();
-            n -=size;
-            
+            int size  = q.size();
+            n = n-size;
             for(int i=0;i<size;i++){
                 int node = q.poll();
-                
                 for(int x:adjList.get(node)){
-                    inDegree[x]--;
+                     inDegree[x]--;
                     if(inDegree[x]==1){
                         q.add(x);
                     }
-                    
                 }
             }
         }
         
-        res.addAll(q);    
-            
+        List<Integer> res = new ArrayList<>();
+        res.addAll(q);
+        
         return res;
+        
+        
     }
 }
