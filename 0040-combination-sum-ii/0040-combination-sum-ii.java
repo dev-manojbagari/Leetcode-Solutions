5ae67cc39259f835da2ba5cd76bda9aaf71a4b76
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-21 09:30:33 +0530

    Time: 3 ms (72.48%), Space: 42.4 MB (51.16%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,23 +1,24 @@
 class Solution {
-      public List<List<Integer>> combinationSum2(int[] nums, int target) {
+   public List<List<Integer>> combinationSum2(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        combinationSum(nums, target, 0, list, new ArrayList<>());
+        backtrack(0,nums,list,new ArrayList<>(),target);
         return list;
     }
     
-    void combinationSum(int[] nums, int target, int start, List<List<Integer>> list, List<Integer> tempList) {
-        if(target == 0){
+    void backtrack(int start,int[] nums,List<List<Integer>> list,List<Integer> tempList,int target){
+        if(target==0){
             list.add(new ArrayList<>(tempList));
             return;
         }
         
-        for(int i = start; i < nums.length; i++){
+        
+        for(int i=start;i<nums.length;i++){
             if(i>start&&nums[i]==nums[i-1]) continue;
-            if(target - nums[i] >= 0){
+            if(target-nums[i]>=0){
                 tempList.add(nums[i]);
-                combinationSum(nums, target - nums[i], i+1, list, tempList);
-                tempList.remove(tempList.size() - 1);
+                backtrack(i+1,nums,list,tempList,target-nums[i]);
+                tempList.remove(tempList.size()-1);
             }
         }
     }
