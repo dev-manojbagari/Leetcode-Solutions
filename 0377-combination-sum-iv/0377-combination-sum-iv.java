14d110507450279da899bf68fe5bbcc44ef1d90c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-21 12:24:44 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (81.58%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,35 +1,27 @@
 class Solution {
-    public int combinationSum4(int[] nums, int target) {
-     
-        int[] dp = new int[target+1];
-        dp[0] = 1;
-        
-        for(int i=1;i<=target;i++){
-            for(int j=0;j<nums.length;j++){
-                if(i-nums[j]>=0){
-                dp[i] +=dp[i-nums[j]];                    
-                    
-                }
-            }
-        }
-        
-        return dp[target];
-        
+ public int combinationSum4(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+      //  Arrays.sort(nums);
+       int count= backtrack(0,nums,list,new ArrayList<>(),target,new Integer[target+1]);
+        return count;
     }
     
-    int f(int[] nums,int target,Integer[] cache){
-        if(target==0)
+    int backtrack(int start,int[] nums,List<List<Integer>> list,List<Integer> tempList,int target,Integer[] dp ){
+        if(target==0){
             return 1;
-        if(cache[target]!=null)
-            return cache[target];
+        }
         
-        int ans =0;
+        if(dp[target]!=null)
+            return dp[target];
+        
+        int count =0;
         for(int i=0;i<nums.length;i++){
             if(target-nums[i]>=0){
-                ans += f(nums,target-nums[i],cache);
+               count += backtrack(i,nums,list,tempList,target-nums[i],dp);
             }
         }
-
-        return cache[target] = ans;
+        
+        return dp[target] = count;
     }
+    
 }
