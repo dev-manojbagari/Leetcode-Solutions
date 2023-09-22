de32e260bc66e3525f9447e02f65a323d6958383
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-22 08:29:17 +0530

    Time: 9 ms (88.73%), Space: 43.5 MB (96.19%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,45 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        if(s.length()<p.length())
-            return list;
-        int[] charCount = new int[26];
-        for(int i=0;i<p.length();i++){
-            char c = p.charAt(i);
-            charCount[c-'a']++;
+        int[] map = new int[26];
+        for(char c:p.toCharArray()){
+            map[c-'a']++;
         }
-        
+        int diff = p.length();
+        List<Integer> res = new ArrayList<>();
+        if(s.length()==0||p.length()==0||s.length()<p.length())
+            return res;
         int start=0,end=0;
-        int diff=p.length();
-        for(;end<p.length();end++){
+        for(end=0;end<p.length();end++){
             char c = s.charAt(end);
-            charCount[c-'a']--;
-            if(charCount[c-'a']>=0)
+            map[c-'a']--;
+            if(map[c-'a']>=0)
                 diff--;
         }
         
         if(diff==0)
-            list.add(0);
-        
-        while(end<s.length()){
-          char c  = s.charAt(start);
-            if(charCount[c-'a'] >= 0){ // Change this line
-                diff++; 
-            }
-            charCount[c-'a']++;
-            c= s.charAt(end);
-            charCount[c-'a']--;
-            if(charCount[c-'a']>=0){
-                diff--;
-            }
+            res.add(0);
+
+        for(;end<s.length();end++){
+            char c = s.charAt(start++);
+            map[c-'a']++;
+            if(map[c-'a']>0)
+                diff++;
             
+            c = s.charAt(end);
+            map[c-'a']--;
+            if(map[c-'a']>=0)
+                diff--;
             if(diff==0)
-                list.add(start+1); // Change this line
-            start++;
-            end++;
+                res.add(start);
+            
         }
         
-        
-        return list;
+        return res;
     }
 }
