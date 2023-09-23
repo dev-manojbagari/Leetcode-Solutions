d691436ab9af101c19c8f247f22e8a2bab180f10
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-23 11:40:54 +0530

    Time: 2 ms (94.13%), Space: 40.3 MB (79.05%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,24 +1,24 @@
 class Solution {
-    int count =0;
     public int countSubstrings(String s) {
         
+    int count =0;
         for(int i=0;i<s.length();i++){
-            countPalindrome(i,i,s);
-            countPalindrome(i,i+1,s);
+          count+=  countPalindrome(i,i,s);
+           count+= countPalindrome(i,i+1,s);
         }
         
         return count;
     }
     
-    void countPalindrome(int i,int j,String s){
-        
+    int countPalindrome(int i,int j,String s){
+       int count=0; 
         while(i>=0&&j<s.length()&&s.charAt(i)==s.charAt(j)){
             i--;
             j++;
             count++;
         }
         
-        
+        return count;
     }
     
 }
