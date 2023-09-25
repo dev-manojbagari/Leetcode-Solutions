02c89e92e8ce66bcb77e5e47760a8e27933614a5
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-25 08:37:41 +0530

    Time: 2 ms (71.26%), Space: 43.3 MB (58.63%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,38 +1,37 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int rows= grid.length,cols = grid[0].length;
-        boolean[][] vis = new boolean[rows][cols];
-        int maxArea =0;
-        int[] curArea = new int[1];
-        curArea[0]=0;
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(vis[i][j]==false){
-                    dfs(i,j,grid,vis,curArea);
-                    maxArea = Math.max(maxArea,curArea[0]);
-                    curArea[0]=0;
+        int maxArea = 0;
+        
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    maxArea=Math.max(maxArea,dfs(i,j,grid,vis));
                 }
             }
         }
-        
+            
+            
         return maxArea;
     }
     
-    
-    void dfs(int i,int j,int[][] grid,boolean[][] vis,int[] curArea){
+    int dfs(int i,int j,int[][] grid,boolean[][] vis){
         
-        if(i<0||i>=grid.length||j<0||j>=grid[0].length||vis[i][j]||grid[i][j]!=1)
-            return;
-
+        if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]==0||vis[i][j]==true)
+            return 0;
+        
         vis[i][j]=true;
-        curArea[0]++;
-                
-        dfs(i+1,j,grid,vis,curArea);
-        dfs(i,j+1,grid,vis,curArea);
-        dfs(i-1,j,grid,vis,curArea);
-        dfs(i,j-1,grid,vis,curArea);
         
+        int a = dfs(i+1,j,grid,vis);
+        int b = dfs(i,j+1,grid,vis);
+        int c = dfs(i-1,j,grid,vis);
+        int d = dfs(i,j-1,grid,vis);
+        
+        
+        return 1+(a+b+c+d);
         
     }
+    
+    
 }
