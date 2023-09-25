14a27cd2908840ba5d90126f8bf1297275a74fc9
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-25 09:30:37 +0530

    Time: 8 ms (53.07%), Space: 41.5 MB (31.41%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,50 +1,25 @@
 class Solution {
-    class Interval{
-        int start;
-        int end;
-        Interval(int start,int end){
-            this.start = start;
-            this.end = end;
-        }
-    }
-    
     public List<Integer> partitionLabels(String s) {
-        Map<Character, Interval> map = new HashMap<>();
+        Map<Character,Integer> lastOccurence = new HashMap<>();
+        
+        for(int i=0;i<s.length();i++){
+            lastOccurence.put(s.charAt(i),i);    
+        }
 
-	for (int i = 0; i < s.length(); i++) {
-	    char c = s.charAt(i);
-	    if (!map.containsKey(c)) {
-		map.put(c, new Interval(i, i));
-	    } else {
-		Interval curInterval = map.get(c);
-		curInterval.end = i;
-	    }
-	}
-
-	List<Interval> intervals = new ArrayList<>();
-	intervals.addAll(map.values());
-	intervals.sort((a, b) -> a.start - b.start);
-	List<Interval> mergedIntervals = new ArrayList<>();
-
-	mergedIntervals.add(intervals.get(0));
-
-	for (int i = 1; i < intervals.size(); i++) {
-	    Interval prev = mergedIntervals.get(mergedIntervals.size() - 1);
-	    Interval curr = intervals.get(i);
-
-	    if (prev.end > curr.start) {
-		prev.end = Math.max(prev.end, curr.end);
-	    } else {
-		mergedIntervals.add(curr);
-	    }
-	}
-
-	List<Integer> res = new ArrayList<>();
-
-	for (Interval curInterval : mergedIntervals) {
-	    res.add(curInterval.end - curInterval.start + 1);
-	}
-
-	return res;
+        int start=0,end=0;
+        List<Integer> partitions = new ArrayList<>();
+        
+        for(int i=0;i<s.length();i++){
+            end = Math.max(end,lastOccurence.get(s.charAt(i)));
+            if(i==end){
+                partitions.add(end-start+1);
+                start = end+1;
+            }
+        }
+        
+    
+    
+        return partitions;
+    
     }
 }
