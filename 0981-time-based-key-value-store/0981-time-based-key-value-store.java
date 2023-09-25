b8e868cd220e30d6ca039c43a230f00894c66740
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-25 16:00:29 +0530

    Time: 124 ms (71.97%), Space: 118.7 MB (60.36%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,49 +1,47 @@
 class TimeMap {
-    class Data{
-        String value;
-        int timestamp;
-        Data(String value,int timestamp){
-            this.value=value;
-            this.timestamp=timestamp;
+    
+    class Node{
+        String value;int timestamp;
+        Node(String value,int timestamp){
+            this.value= value;
+            this.timestamp = timestamp;
         }
     }
     
-    Map<String,List<Data>> map = new HashMap<>();
-    
+    Map<String,List<Node>> map;
+   
     public TimeMap() {
-            
+        map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-     map.computeIfAbsent(key,k->new ArrayList<Data>()).add(new Data(value,timestamp));   
+        map.computeIfAbsent(key,(k->new ArrayList<>())).add(new Node(value,timestamp));
     }
     
     public String get(String key, int timestamp) {
         if(!map.containsKey(key))
             return "";
-        else if(map.get(key).size()==1)
-            return map.get(key).get(0).value;
         else
             return ceiling(map.get(key),timestamp);
     }
     
-    private String ceiling(List<Data> list,int timestamp){
+    String ceiling(List<Node> list,int timestamp){
+        String ans = "";
+        int left=0,right= list.size()-1;
         
-        int left = 0,right=list.size()-1;
-        String ans ="";
         while(left<=right){
-           int mid = left+(right-left)/2;
+            int mid = left+(right-left)/2;
             
-           if(list.get(mid).timestamp<=timestamp){
-              ans = list.get(mid).value;
-              left=mid+1;
-           }else{
-               right=mid-1;
-           }
+            if(list.get(mid).timestamp<=timestamp){
+                ans = list.get(mid).value;
+                left= mid+1;
+            }else
+                right=mid-1;
         }
         
         return ans;
     }
+    
 }
 
 /**
