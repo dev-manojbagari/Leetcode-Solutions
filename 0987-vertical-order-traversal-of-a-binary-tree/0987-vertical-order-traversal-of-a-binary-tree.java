64a97ada640bc0682b8a94a221af3ef7e7cd2a4d
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-26 15:20:00 +0530

    Time: 3 ms (88.19%), Space: 41.5 MB (57.57%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,68 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    private class Pair {
+     class Pair{
         TreeNode node;
         int hd;
         int level;
-
-        Pair(TreeNode node, int hd, int level) {
-            this.node = node;
-            this.hd = hd;
-            this.level = level;
+        Pair(TreeNode node,int hd,int level){
+            this.node=node;
+            this.hd=hd;
+            this.level=level;
         }
     }
-
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        Map<Integer, List<Pair>> map = new HashMap<>();
         List<List<Integer>> list = new ArrayList<>();
-        q.add(new Pair(root, 0, 0));
-        int minHd = Integer.MAX_VALUE, maxHd = Integer.MIN_VALUE;
-        int curLevel = -1;
-        while (!q.isEmpty()) {
-            int levelSize = q.size();
-            curLevel++;
-            for (int i = 0; i < levelSize; i++) {
-                Pair p = q.poll();
-                TreeNode node = p.node;
-                int hd = p.hd;
-
-                minHd = Math.min(minHd, hd);
-                maxHd = Math.max(maxHd, hd);
-
-                if (map.containsKey(hd)) map.get(hd).add(p); else {
-                    List<Pair> l = new ArrayList<>();
-                    l.add(p);
-                    map.put(hd, l);
-                }
-
-                if (node.left != null) {
-                    q.add(new Pair(node.left, hd - 1, curLevel));
-                }
-
-                if (node.right != null) {
-                    q.add(new Pair(node.right, hd + 1, curLevel));
-                }
+        if(root==null)
+            return list;
+        Map<Integer,List<Pair>> map = new HashMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root,0,0));
+        int minHd=Integer.MAX_VALUE,maxHd = Integer.MIN_VALUE;
+        int curLevel=0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+            Pair pair = q.poll();
+            TreeNode node = pair.node;
+            int hd = pair.hd;
+            
+            if(map.get(hd)==null)
+                map.put(hd,new ArrayList<>());
+                
+            map.get(hd).add(pair);
+            
+            
+            minHd = Math.min(minHd,hd);
+            maxHd = Math.max(maxHd,hd);
+            
+            if(node.left!=null)
+                q.offer(new Pair(node.left,hd-1,curLevel));
+            
+            if(node.right!=null)
+                q.offer(new Pair(node.right,hd+1,curLevel));
+            
             }
+            curLevel++;
         }
-
-        for (int i = minHd; i <= maxHd; i++) {
-            Comparator<Pair> c = (a, b) -> {
-                if (a.level < b.level) {
-                    return -1;
-                } else if (a.level > b.level) {
-                    return 1;
-                } else {
-                    return Integer.compare(a.node.val, b.node.val);
-                }
-            };
-            List<Pair> x = map.get(i);
-            x.sort(c);
-            List<Integer> tempList = new ArrayList<Integer>();
-            x.forEach(h -> tempList.add(h.node.val));
-            list.add(tempList);
+        for(int i=minHd;i<=maxHd;i++){
+            List<Pair> tempList = map.get(i);
+            tempList.sort((a,b)->{
+                if(a.level==b.level)
+                    return a.node.val-b.node.val;
+                return a.hd-b.hd;
+            });
+            List<Integer> temp = new ArrayList<>();
+            for(Pair pair:tempList)
+                temp.add(pair.node.val);
+            
+            list.add(temp);
         }
-
+        
+        
         return list;
     }
 }
