1307b3d8e323243b158b375ed78bf175c5585552
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-26 08:10:28 +0530

    Time: 45 ms (91.37%), Space: 56.6 MB (32.37%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,30 +1,32 @@
 class DetectSquares {
     int[][] count = new int[1001][1001];
-    List<int[]> points = new ArrayList<>();
+    List<int[]> list = new ArrayList<>();
     public DetectSquares() {
         
     }
     
     public void add(int[] point) {
         if(count[point[0]][point[1]]==0){
-            points.add(point);
+            list.add(point);
         }
         
         count[point[0]][point[1]]++;
     }
+    
     public int count(int[] point) {
-    int totalSquare=0;
-        int x1 = point[0],y1=point[1];
+        int c =0;
         
-        for(int[] curPoint : points){
-            int x3 = curPoint[0],y3=curPoint[1];
-            if(x3!=x1&&y3!=y1&&Math.abs(x3-x1)==Math.abs(y3-y1))
-                totalSquare += count[x1][y3]*count[x3][y1]*count[x3][y3];
+        for(int[] curPoint:list){
+            if(curPoint[0]==point[0]&&curPoint[1]==point[1])
+                continue;
+                        
+            if(Math.abs(curPoint[0]-point[0])==Math.abs(curPoint[1]-point[1]))
+                c += count[curPoint[0]][point[1]]*count[point[0]][curPoint[1]]*count[curPoint[0]][curPoint[1]];
+            
         }
         
         
-        return totalSquare;        
-        
+        return c;
     }
 }
 
