544882b191c375c7347b79f46fc0b568e006e5bd
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-26 06:27:50 +0530

    Time: 2 ms (83.66%), Space: 41.4 MB (34.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,47 +1,41 @@
 class Solution {
-    class Pair{
-        int i,j,curTime;
-        Pair(int i,int j,int curTime){
-            this.i=i;
-            this.j=j;
-            this.curTime=curTime;
-        }
-    }
-    
     public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
         int rows=grid.length,cols=grid[0].length;
-        
-        Queue<Pair> q = new LinkedList<>();
-        int[][] dirs= {{1,0},{0,1},{-1,0},{0,-1}};
-        int minTime = 0;
         int freshOranges=0;
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]==2){
-                    q.offer(new Pair(i,j,0));
+                    q.offer(new int[]{i,j,0});
                 }else if(grid[i][j]==1){
                     freshOranges++;
                 }
             }
         }
-        
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int time=0;
         while(!q.isEmpty()){
-            Pair node = q.poll();
-            
+            int[] cell = q.poll();
+            int i= cell[0];
+            int j= cell[1];
+            int curTime = cell[2];
             for(int[] dir:dirs){
-                int x= node.i+dir[0];
-                int y = node.j+dir[1];
-                
+                int x = i+dir[0];
+                int y = j+dir[1];
+            
                 if(x<0||x>=rows||y<0||y>=cols||grid[x][y]!=1)
                     continue;
-                
+            
                 grid[x][y]=2;
                 freshOranges--;
-                q.offer(new Pair(x,y,node.curTime+1));
-                minTime = node.curTime+1;
+                time = Math.max(time,curTime+1);
+                q.offer(new int[]{x,y,curTime+1});
             }
+            
         }
         
-        return freshOranges!=0?-1:minTime;
+        
+            
+        return freshOranges==0?time:-1;    
     }
 }
