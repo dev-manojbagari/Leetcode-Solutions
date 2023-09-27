5f891afeb575320ec19b5666eae3e6e71832293e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-27 16:06:42 +0530

    Time: 15 ms (82.01%), Space: 46.6 MB (37.28%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,28 +1,40 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return isMatch(0,0,s,p,new Boolean[s.length()][p.length()]);
+        return isMatch(s,p,s.length()-1,p.length()-1,new Boolean[s.length()+1][p.length()+1]);
     }
     
-    boolean isMatch(int si,int pi,String s,String p,Boolean cache[][]){
-        if(pi==p.length())
-            return si==s.length();
-        if(si==s.length()){
-            if(p.charAt(pi)=='*')
-                return isMatch(si,pi+1,s,p,cache);
-            else
-                return false;
-        }
-
-        if(cache[si][pi]!=null)
-            return cache[si][pi];
-
-        if(p.charAt(pi)=='?')
-            return cache[si][pi]=isMatch(si+1,pi+1,s,p,cache);
+    private boolean isMatch(String s,String p,int sl,int pl,Boolean[][] dp){
+            if(pl==-1)
+                return sl==-1;
+            else if(sl==-1){
+                if(allStars(p,pl))
+                    return true;
+                else
+                    return false;
+            }
         
-        else if(p.charAt(pi)=='*')
-            return cache[si][pi]=isMatch(si,pi+1,s,p,cache)||isMatch(si+1,pi,s,p,cache);
-        else
-            return cache[si][pi]=s.charAt(si)==p.charAt(pi)&&isMatch(si+1,pi+1,s,p,cache);
+        
+        
+        if(dp[sl][pl]!=null)
+            return dp[sl][pl];
+        boolean ans = false;
+        if(s.charAt(sl)==p.charAt(pl)||p.charAt(pl)=='?')
+            ans =isMatch(s,p,sl-1,pl-1,dp);
+        else if(p.charAt(pl)=='*')
+            ans = isMatch(s,p,sl,pl-1,dp)||isMatch(s,p,sl-1,pl,dp);
+        else 
+            ans= false;
+        
+        return dp[sl][pl]=ans;
     }
+    
+    boolean allStars(String str,int j){
+        for(int i=j;i>=0;i--)
+            if(str.charAt(i)!='*')
+                return false;
+        
+        return true;
+    }
+    
     
 }
