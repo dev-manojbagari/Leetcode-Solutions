3ca99f0ba02908b3d3c43264197e8fb99d1e7c54
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-28 18:49:39 +0530

    Time: 184 ms (34.70%), Space: 45.9 MB (56.21%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,44 +1,40 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         List<int[]> list = new ArrayList<>();
-        
         for(int[] building:buildings){
-             list.add(new int[]{building[0],-building[2]});
-             list.add(new int[]{building[1],building[2]}); 
+            list.add(new int[]{building[0],-building[2]});
+            list.add(new int[]{building[1],building[2]});
         }
         
-        list.sort((a,b)->{
-            if(a[0]!=b[0])
-                return a[0]-b[0];
-            else
-                return a[1]-b[1];
-        });
         
         List<List<Integer>> res = new ArrayList<>();
-        TreeMap<Integer,Integer> treeMap = new TreeMap<Integer,Integer>(Collections.reverseOrder());
-        int prevHeight = 0;
+        
+        
+       list.sort((a,b)->{
+            if(a[0]!=b[0])
+                return a[0]-b[0];
+            return a[1]-b[1];
+       }); 
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+         
+        int prevHeight=0;
+        
         for(int[] height:list){
             
-            if(height[1]<0)
-                treeMap.put(-height[1],treeMap.getOrDefault(-height[1],0)+1);
-            else
-            {
-                int count = treeMap.get(height[1])-1;
-                if(count==0)
-                    treeMap.remove(height[1]);
-                else 
-                    treeMap.put(height[1],count);
-            }
+            if(height[1]<0){
+                pq.offer(-height[1]);
+            }else
+                pq.remove(height[1]);
             
-            int curHeight = treeMap.isEmpty()?0:treeMap.firstKey();
+            int curHeight = pq.isEmpty()?0:pq.peek();
             
-            if(prevHeight!=curHeight){
+            if(curHeight!=prevHeight){
                 res.add(Arrays.asList(height[0],curHeight));
-                prevHeight = curHeight;
+                prevHeight=curHeight;
             }
         }
         
         return res;
-        
     }
 }
