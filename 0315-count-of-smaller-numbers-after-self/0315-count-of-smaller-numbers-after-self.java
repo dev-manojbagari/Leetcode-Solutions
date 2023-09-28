b84fbba033ff95f0e0bd2109ab69a5216a1a2364
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-28 18:08:30 +0530

    Time: 81 ms (63.07%), Space: 62.7 MB (18.64%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -1,67 +1,67 @@
 class Solution {
-    class Pair{
-        int index,value;
-        Pair(int index,int value){
-            this.index=index;
-            this.value=value;
-        }
-    }
+    record Pair(int index,int val){};
+    int[] res;
     public List<Integer> countSmaller(int[] nums) {
-        Pair[] pair = new Pair[nums.length];
+        Pair[] pairs = new Pair[nums.length];
+        
         for(int i=0;i<nums.length;i++){
-            pair[i] = new Pair(i,nums[i]);
+            pairs[i]= new Pair(i,nums[i]);
         }
-        int[] countSmaller = new int[nums.length];
-
-        mergeSort(pair,0,pair.length-1,countSmaller);
-
-        return Arrays.stream(countSmaller).boxed().toList();
+        
+        res = new int[nums.length];
+        mergeSort(0,pairs.length-1,pairs);
+        
+        return Arrays.stream(res).boxed().toList();
     }
     
-    void mergeSort(Pair[] pair,int start,int end,int[] cs){
+    void mergeSort(int start,int end,Pair[] pairs){
         if(start>=end)
             return;
         
         int mid = start+(end-start)/2;
         
-        mergeSort(pair,start,mid    ,cs);
-        mergeSort(pair,mid+1,end,cs);
+        mergeSort(start,mid,pairs);
+        mergeSort(mid+1,end,pairs);
         
-        sortedMerge(pair,start,mid,end,cs);
+        sortedMerge(start,mid,end,pairs);
     }
     
-    void sortedMerge(Pair[] pair ,int start,int mid,int end ,int[] cs){
-        Pair L[] = new Pair[mid-start+1];
-        Pair R[] = new Pair[end-mid];
+    void sortedMerge(int start,int mid,int end,Pair[] pairs){
+        Pair[] L = new Pair[mid-start+1];        
+        Pair[] R = new Pair[end-mid];        
         
-        for(int i=0;i<L.length;i++)
-            L[i]= pair[start+i];
+        for(int i=0;i<L.length;i++){
+            L[i]=pairs[start+i];
+        }
         
-        
-        for(int i=0;i<R.length;i++)
-            R[i]= pair[mid+1+i];
-        
+        for(int i=0;i<R.length;i++){
+            R[i]=pairs[mid+1+i];
+        }
         
         int i=0,j=0,k=start;
         
         while(i<L.length&&j<R.length){
-            if(L[i].value<=R[j].value){
-                cs[L[i].index]+=j;
-                pair[k++]=L[i++];
+            if(L[i].val()<=R[j].val()){
+                res[L[i].index]+=j;
+                pairs[k]=L[i];
+                i++;
+                k++;
             }else{
-                pair[k]=R[j];
+                pairs[k]=R[j];
                 j++;
                 k++;
             }
         }
         
         while(i<L.length){
-            cs[L[i].index]+=j;
-            pair[k++] = L[i++];
+             res[L[i].index]+=j;
+             pairs[k++]=L[i++];
         }
-       
-       while(j<R.length)
-            pair[k++] = R[j++];
+        
+        while(j<R.length){
+             pairs[k++]=R[j++];
+        }
+        
         
     }
 }
