49290c25da5fe92aa90f240f1cbcdc7c4f9f7af9
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-28 15:13:56 +0530

    Time: 169 ms (64.15%), Space: 43.8 MB (79.03%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,87 +1,80 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-        Trie trie= new Trie();
-        
+        int rows= board.length,cols=board[0].length;
+        boolean[][] vis = new boolean[rows][cols];
+        Trie trie = new Trie();
         for(String word:words){
             trie.add(word);
         }
         
-        
         List<String> list = new ArrayList<>();
-        boolean[][] vis = new boolean[board.length][board[0].length];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                  dfs(i,j,board,vis,trie.head,"",list);  
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                    dfs(i,j,board,0,vis,trie.head,list);
             }
         }
         
         return list;
     }
     
-    void dfs(int i,int j,char[][] board,boolean[][] vis,TrieNode trieNode,String str,List<String> list){
+    void dfs(int i,int j,char[][] board,int curIndex,boolean[][] vis,Trie.Node trieNode,List<String> list){
         
         if(i<0||i>=board.length||j<0||j>=board[0].length||vis[i][j])
-            return ;
-        
-        char c = board[i][j];
-        if(trieNode.next[c-'a']==null)
             return;
         
         vis[i][j]=true;
         
-        trieNode = trieNode.next[c-'a'];
+        char c = board[i][j];
         
+        if(trieNode.next[c-'a']==null){
+            vis[i][j]=false;
+            return;
+        }
+        
+        trieNode = trieNode.next[c-'a'];
         if(trieNode.isWord){
             list.add(trieNode.word);
             trieNode.isWord=false;
         }
         
-        dfs(i+1,j,board,vis,trieNode,str,list);
-        dfs(i,j+1,board,vis,trieNode,str,list);
-        dfs(i-1,j,board,vis,trieNode,str,list);
-        dfs(i,j-1,board,vis,trieNode,str,list);
- 
+           dfs(i+1,j,board,curIndex+1,vis,trieNode,list);
+           dfs(i,j+1,board,curIndex+1,vis,trieNode,list);
+           dfs(i-1,j,board,curIndex+1,vis,trieNode,list);
+           dfs(i,j-1,board,curIndex+1,vis,trieNode,list);
+            
+
         vis[i][j]=false;
-        
     }
     
-    class TrieNode{
-        char val;
-        TrieNode[] next = new TrieNode[26];
-        boolean isWord=false;
-        String word = null;
-        TrieNode(char val){
-            this.val = val;
-        }
-    }
     
     class Trie{
-
-        TrieNode head=new TrieNode('\0');
-        
-        boolean startsWith(String str){
-            TrieNode curr = head;
-            
-            for(char c:str.toCharArray()){
-                curr = curr.next[c-'a'];
-                if(curr==null)
-                    break;
+        class Node{
+            Node[] next;
+            char val;
+            boolean isWord;
+            String word;
+            Node(char val){
+                this.val = val;
+                next = new Node[26];
             }
-            
-            return curr!=null;
+        }
+        Node head;
+        
+        Trie(){
+            head = new Node('\0');
         }
         
-        
-        void add(String word){
-            TrieNode curr = head;
-            for(char c:word.toCharArray()){
-                if(curr.next[c-'a']==null)
-                    curr.next[c-'a']=new TrieNode(c);
-                curr = curr.next[c-'a'];                
-            }
+        void add(String str){
+            Node curr = head;
             
+            for(char c:str.toCharArray()){
+                if(curr.next[c-'a']==null){
+                    curr.next[c-'a']=new Node(c);
+                }
+                curr = curr.next[c-'a'];
+            }
             curr.isWord=true;
-            curr.word=word;
+            curr.word = str;
         }
     }
 }
