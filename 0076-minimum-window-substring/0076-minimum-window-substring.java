2c5df4ec388d45629899b5f28a66f37d4a07ede1
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-28 08:50:17 +0530

    Time: 3 ms (91.33%), Space: 43.9 MB (68.19%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] map = new int[256];
+        
+        for(char c:t.toCharArray()){
+            map[c]++;
+        }
+        
+        int start=0,end=0,count=t.length();
+        int minLen = s.length()+1;
+        int minStart=0;
+        for(;end<s.length();end++){
+            char c = s.charAt(end);
+            map[c]--;
+            if(map[c]>=0)
+                count--;
+            
+            while(count==0){
+                if(minLen>end-start+1){
+                    minStart = start;
+                    minLen = end-start+1;
+                }
+                c = s.charAt(start);
+                map[c]++;
+                if(map[c]>0)
+                    count++;
+                start++;
+            }
+            
+        }
+        
+        return minLen==s.length()+1?"":s.substring(minStart,minStart+minLen);
+    }
+}
