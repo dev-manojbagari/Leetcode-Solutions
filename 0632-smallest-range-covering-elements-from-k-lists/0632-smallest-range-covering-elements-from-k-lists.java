bae981ade3a4aef2d6aa6d454260dd90dd0510c3
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-29 16:43:13 +0530

    Time: 28 ms (81.58%), Space: 50 MB (42.25%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,36 +1,45 @@
 class Solution {
+    class Data{
+        int val,index,listIndex;
+        Data(int val,int index,int listIndex){
+            this.val=val;
+            this.index=index;
+            this.listIndex=listIndex;
+        }
+    }
+    
     public int[] smallestRange(List<List<Integer>> nums) {
-        PriorityQueue<int[]> minHeap= new PriorityQueue<>((a,b)->a[0]-b[0]);
+        PriorityQueue<Data> pq = new PriorityQueue<>((a,b)->a.val-b.val);
+        int n = nums.size();
         int max=Integer.MIN_VALUE;
-
-        for(int i=0;i<nums.size();i++){
-            minHeap.offer(new int[]{nums.get(i).get(0),i,0});
+        
+        for(int i=0;i<n;i++){
+            pq.offer(new Data(nums.get(i).get(0),0,i));
             max = Math.max(max,nums.get(i).get(0));
         }
+        
+        int[] res = new int[2];
+        int start=0;
         int minRange = Integer.MAX_VALUE;
-        int start =0;
-        while(minHeap.size()==nums.size()){
-            int[] data = minHeap.poll();
-             
-            if(max-data[0]+1<minRange){
-                minRange = max-data[0]+1;
-                start = data[0];
+        while(pq.size()==n){
+            Data data = pq.poll();
+            
+            if(max-data.val+1<minRange){
+                minRange = max-data.val+1;
+                start = data.val;
             }
             
-            int curIndex = data[2];
-            int curList = data[1];
-            int curListSize = nums.get(curList).size();
+            int curIndex = data.index;
+            int curListIndex = data.listIndex;
+            int curListSize =  nums.get(curListIndex).size();
             
             if(curIndex+1<curListSize){
-                int nextNum = nums.get(curList).get(curIndex+1);
-                minHeap.offer(new int[]{nextNum,curList,curIndex+1});
-                max = Math.max(max,nextNum);
+                int num = nums.get(curListIndex).get(curIndex+1);
+                pq.offer(new Data(num,curIndex+1,curListIndex));
+                max = Math.max(max,num);
             }
-                     
         }
         
-        
-        
-        return new int[]{start,start+minRange-1};
+        return new int[]{start ,start+minRange-1};
     }
 }
