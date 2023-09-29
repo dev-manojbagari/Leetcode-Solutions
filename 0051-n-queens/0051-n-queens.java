d8f1672f876bd450001d394d1dbeb9f40df9e321
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-29 08:55:35 +0530

    Time: 2 ms (89.37%), Space: 44.1 MB (40.09%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,35 +1,40 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
-        for(char[] row:board)
-            Arrays.fill(row,'.');
+        List<List<String>> list = new ArrayList<>();
         
-        solve(0,n,board,res);
-        return res;
+        for(char[] row:board){
+            Arrays.fill(row,'.');
+        }
+        
+        solve(0,n,board,list);
+
+        return list;
     }
     
     void solve(int curRow,int n,char[][] board,List<List<String>> res){
+        
         if(curRow==n){
             res.add(construct(board));
             return;
-        }        
+        }
         
         for(int curCol=0;curCol<n;curCol++){
-            if(isValidPosition(curRow,curCol,board)){
+            if(isBoardVaild(board,curRow,curCol)){
                 board[curRow][curCol]='Q';
                 solve(curRow+1,n,board,res);
                 board[curRow][curCol]='.';
             }
         }
-        
     }
-    boolean isValidPosition(int curRow,int curCol,char[][] board){
+    
+    boolean isBoardVaild(char[][] board,int curRow,int curCol){
         
-        for(int row=0;row<curRow;row++)
+        for(int row = 0;row<curRow;row++){
             if(board[row][curCol]=='Q')
                 return false;
-        
+        }
+
         for(int row=curRow-1,col=curCol+1;row>=0&&col<board.length;row--,col++){
             if(board[row][col]=='Q')
                 return false;
@@ -40,16 +45,20 @@ class Solution {
                 return false;
         }
         
-        
         return true;
     }
+    
+    
     
     List<String> construct(char[][] board){
         List<String> list = new ArrayList<>();
         for(char[] row:board){
             list.add(new String(row));
         }
-        
         return list;
     }
+    
+    
+    
+    
 }
