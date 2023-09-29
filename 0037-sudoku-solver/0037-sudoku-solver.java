b455ab6d9dc9047174aee2cf96cedb7d8d1078e4
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-29 05:49:24 +0530

    Time: 7 ms (65.71%), Space: 40.4 MB (24.04%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,36 +1,38 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        solve(board,0,0);
+        solveSudoku(board,0,0);
     }
     
-    boolean solve(char[][] board,int row,int col){
-        
+    boolean solveSudoku(char[][] board,int row,int col){
         for(int i=row;i<9;i++,col=0){
             for(int j=col;j<9;j++){
                 if(board[i][j]=='.'){
                     for(char num='1';num<='9';num++){
-                        if(isValid(board,i,j,num)){
+                        if(isVaild(i,j,board,num)){
                             board[i][j]=num;
-                            if(solve(board,i,j+1))
+                            if(solveSudoku(board,i,j+1))
                                 return true;
                             board[i][j]='.';
-                        }
-                    }
+                        }                        
+                    }            
                     return false;
                 }
+                
             }
         }
         return true;
     }
     
-    boolean isValid(char[][] board,int row,int col,char num){
-        int blockrow = (row/3)*3,blockcol=(col/3)*3;
-        
+    boolean isVaild(int row,int col,char[][] board,int num){
+        int blockRow = (row/3)*3,blockCol=(col/3)*3;
         for(int i=0;i<9;i++){
-            if(board[row][i]==num||board[i][col]==num||board[blockrow+i/3][blockcol+i%3]==num)
+            if(board[row][i]==num||board[i][col]==num||board[blockRow+i/3][blockCol+i%3]==num){
                 return false;
+            }
         }
-        
         return true;
     }
+    
+    
+    
 }
