492644c15c57c9433184590613fe88dd02eb71d1
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-30 05:48:17 +0530

    Time: 76 ms (44.51%), Space: 54.1 MB (34.55%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,58 +1,82 @@
 class Solution {
+    record Pair(int index,int val){};
+    int[] res;
+   int count=0;
     public int reversePairs(int[] nums) {
-       return mergeSort(nums,0,nums.length-1);
+        Pair[] pairs = new Pair[nums.length];
         
+        for(int i=0;i<nums.length;i++){
+            pairs[i]= new Pair(i,nums[i]);
+        }
+        
+        res = new int[nums.length];
+        
+        mergeSort(0,pairs.length-1,pairs);
+        
+        return count;
     }
     
-    int mergeSort(int[] nums,int left,int right){
-        if(left>=right)
-            return 0;
+
+    
+    void mergeSort(int start,int end,Pair[] pairs){
+        if(start>=end)
+            return;
         
-        int mid = left+(right-left)/2;
-        int count=0;    
-     count += mergeSort(nums,left,mid);
-     count += mergeSort(nums,mid+1,right);
-       
-        int i=left,j=mid+1;
+        int mid = start+(end-start)/2;
+        
+        mergeSort(start,mid,pairs);
+        mergeSort(mid+1,end,pairs);
+        
+        int i=start,j=mid+1;
         
         while(i<=mid){
             
-            while(j<=right&&nums[i]/2.0>nums[j])
+            while(j<=end&&(pairs[i].val/2.0)>pairs[j].val)
                 j++;
             
-            count += j-(mid+1);
+            count+= j-(mid+1);
             i++;
-            
         }
-     //  Arrays.sort(nums, left, right+1);
-            sortedMerge(nums,left,mid,right);
-         return count;
+        
+        
+        
+        sortedMerge(start,mid,end,pairs);
     }
     
-    void sortedMerge(int[] nums,int left,int mid,int right){
-
-        int[] L = new int[mid-left+1];
-        int[] R = new int[right-mid];
+    void sortedMerge(int start,int mid,int end,Pair[] pairs){
+        Pair[] L = new Pair[mid-start+1];        
+        Pair[] R = new Pair[end-mid];        
         
-        for(int i=0;i<L.length;i++)
-            L[i]= nums[left+i];
-        
-        for(int i=0;i<R.length;i++)
-            R[i]= nums[mid+1+i];
-        
-        int i=0,j=0,k=left;
-        
-        while(i<L.length&&j<R.length){
-            if(L[i]<=R[j])
-                nums[k++] =L[i++];
-            else
-                nums[k++] =R[j++];
+        for(int i=0;i<L.length;i++){
+            L[i]=pairs[start+i];
         }
         
-        while(i<L.length)
-            nums[k++] =L[i++];
+        for(int i=0;i<R.length;i++){
+            R[i]=pairs[mid+1+i];
+        }
         
-        while(j<R.length)
-                nums[k++] =R[j++];
+        int i=0,j=0,k=start;
+        
+        while(i<L.length&&j<R.length){
+            if(L[i].val()<=R[j].val()){
+                res[L[i].index]+=j;
+                pairs[k]=L[i];
+                i++;
+                k++;
+            }else{
+                pairs[k]=R[j];
+                j++;
+                k++;
+            }
+        }
+        
+        while(i<L.length){
+             res[L[i].index]+=j;
+             pairs[k++]=L[i++];
+        }
+        
+        while(j<R.length){
+             pairs[k++]=R[j++];
+        }
     }
 }
