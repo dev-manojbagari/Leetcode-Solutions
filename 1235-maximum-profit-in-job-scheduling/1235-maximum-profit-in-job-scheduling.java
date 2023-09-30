02ca77acdab543adebf2d4cd5479269d90ef9ff5
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-09-30 12:18:42 +0530

    Time: 27 ms (58.23%), Space: 51.8 MB (92.19%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,72 +1,54 @@
-// dp[end_time] = max profit can be obtained from time 0 to end_time
-
-// induction rule
-// for each job = [s, e, p], where s = start time, e = end time, p = profit,
-// case 1: don't do this job  -> nothing changes, dp[end_time] = dp[previous end_time]
-// case 2: do this job -> dp[end_time] = dp[previous end_time <= s that gives max profit] + p
-//         find the max profit we can make before start time s (using binary search), so we can know the max profit we can make after doing this job
-// Therefore,
-// dp[end_time] = max( dp[previous end_time],  dp[previous end_time <= s that gives max profit] + p )
-
-// base case
-// dp[0] = 0 as we make profit = 0 at time = 0.
-
 class Solution {
-    class Job {
-        int start;
-        int end;
-        int profit;
-
-        public Job(int start, int end, int profit) {
-            this.start = start;
-            this.end = end;
-            this.profit = profit;
+    class Job{
+        int startTime,endTime,profit;
+        Job(int startTime,int endTime,int profit){
+            this.startTime=startTime;
+            this.endTime=endTime;
+            this.profit= profit;
         }
     }
-
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        // first sort jobs by end_time to make it easy to process each job and fill DP table
-        int n = profit.length;
-        Job[] jobs = new Job[n];
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
+        
+        List<Job> jobs = new ArrayList<>();
+        for(int i=0;i<startTime.length;i++){
+            jobs.add(new Job(startTime[i],endTime[i],profit[i]));
         }
-
-        Arrays.sort(jobs, (j1, j2) -> Integer.compare(j1.end, j2.end));
-        List<Integer> dpEndTime = new ArrayList<>();
+        
+        jobs.sort((a,b)->Integer.compare(a.endTime,b.endTime));
+        
+        
+        List<Integer> endTimes = new ArrayList<>();
         List<Integer> dpProfit = new ArrayList<>();
-        dpEndTime.add(0); // base case to avoid IndexOutBoundExp
+        endTimes.add(0);
         dpProfit.add(0);
-
-        for (Job job : jobs) {
-            int prevIdx = largestSmaller(dpEndTime, job.start); // previous job can end at job.start, so we use "job.start + 1"
-            int case2Profit = dpProfit.get(prevIdx) + job.profit;
-            int case1Profit = dpProfit.get(dpProfit.size() - 1);
+        for(Job job:jobs){
+            int prevJobId = prevJob(endTimes,job.startTime);
             
-            if (case2Profit > case1Profit) {
-                dpEndTime.add(job.end);
-                dpProfit.add(case2Profit);
+            int inclProfit = dpProfit.get(prevJobId)+job.profit;
+            int exclProfit = dpProfit.get(dpProfit.size()-1);
+            
+            if(inclProfit>exclProfit){
+                endTimes.add(job.endTime);
+                dpProfit.add(inclProfit);
             }
         }
-
-        return dpProfit.get(dpProfit.size() - 1);
+        
+        return dpProfit.get(dpProfit.size()-1);
     }
-
-    // return the index of the largest element < target in the given list (assume there must exist one element < target)
-    private int largestSmaller(List<Integer> list, int target) {
-        int left = 0;
-        int right = list.size() - 1;
-        int ans = right;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (list.get(mid) <= target) {
+    
+    int prevJob(List<Integer> endTimes,int startTime){
+        int left =  0,right =endTimes.size()-1;
+        int ans =-1;
+        while(left<=right){
+            int mid  = left+(right-left)/2;
+            
+            if(endTimes.get(mid)<=startTime){
                 ans = mid;
                 left = mid+1;
-            } else {
-                right = mid - 1;
-            }
+            }else
+                right = mid-1;
         }
-
+        
         return ans;
     }
 }
