2956b94c633f98699a1bc6aab0b620f2da7b989b
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-04 16:23:49 +0530

    Time: 1 ms (40.92%), Space: 40.3 MB (68.55%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,32 +1,30 @@
-class Solution {
-    public boolean checkValidString(String s) {
-        Stack<Integer> openStack = new Stack<>();
-        Stack<Integer> starStack  = new Stack<>();
-        
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c=='('){
-                openStack.push(i);
-            }else if(c==')'){
-                if(!openStack.isEmpty()){
-                    openStack.pop();
-                }else if(!starStack.isEmpty()){
-                    starStack.pop();
+    class Solution {
+        public boolean checkValidString(String s) {
+             Stack<Integer> openStack = new Stack<>();
+             Stack<Integer> starStack = new Stack<>();
+
+            for(int i=0;i<s.length();i++){
+                char c = s.charAt(i);
+                if(c=='('){
+                    openStack.push(i);
+                }else if(c=='*'){
+                    starStack.push(i);   
                 }else{
-                    return false;
+
+                    if(openStack.isEmpty()&&starStack.isEmpty())
+                        return false;
+                    if(!openStack.isEmpty())
+                        openStack.pop();
+                    else if(!starStack.isEmpty())
+                        starStack.pop();
                 }
-            }else{
-                    starStack.push(i);                
             }
+
+            while(!openStack.isEmpty()&&!starStack.isEmpty()){
+                if(openStack.pop()>starStack.pop())
+                    return false;
+            }
+            
+            return openStack.isEmpty();
         }
-        
-        while(!openStack.isEmpty()&&!starStack.isEmpty()){
-            if(openStack.pop()>starStack.pop())
-                return false;
-        }
-        
-        
-        
-        return openStack.isEmpty();
     }
-}
