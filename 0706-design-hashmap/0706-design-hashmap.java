61b963c887328a9e33bc98dac114c3cb559d7473
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-04 09:09:32 +0530

    Time: 30 ms (39.67%), Space: 54.4 MB (15.09%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,102 +1,20 @@
 class MyHashMap {
-    class LinkedList{
-        class Node{
-            int key,val;
-            Node next;
-            Node(int key,int val){
-                this.key=key;
-                this.val=val;
-            }
-        }
-        
-        Node head,tail;
-
-        LinkedList(){
-            head = null;
-            tail=null;
-        }
-        
-        void add(int key,int val){
-               Node cur=head;
-             while(cur!=null){
-               if(cur.key==key){
-                cur.val=val;  // Update value for existing key
-                 return;
-                 }
-       cur=cur.next;
-   }
-            
-            if(head==null){
-                head=tail=new Node(key,val);
-                
-            }
-            else{
-                tail.next=new Node(key,val);
-                tail=tail.next;
-            }
-        }
-        
-        void remove(int key){
-            Node prev=null;
-            Node curr = head;
-            while(curr!=null&&curr.key!=key){
-                prev=curr;
-                curr=curr.next;
-            }
-            
-            if(curr==null)
-                return;
-            if(prev==null)
-            {
-                Node temp = head;
-                head=head.next;
-                temp.next=null;
-            }else{
-                prev.next=curr.next;
-                 if(curr == tail){ // If we are removing last element update tail
-                     tail=prev;
-                }
-        
-                curr.next=null;
-            }
-            
-        }
-        
-        int search(int key){
-            Node curr= head;
-            while(curr!=null&&curr.key!=key)
-                curr=curr.next;
-            
-            return curr==null?-1:curr.val;
-        }
-        
-    }
-    
-    LinkedList[] la = new LinkedList[1000001];
+    int[] nodes;
     public MyHashMap() {
-        
+        nodes = new int[1000001];
+        Arrays.fill(nodes,-1);
     }
     
     public void put(int key, int value) {
-        if(la[key]==null)
-            la[key]=new LinkedList();
-        
-        la[key].add(key,value);
+        nodes[key]=value;
     }
     
     public int get(int key) {
-        if(la[key]==null)
-            return -1;
-        else 
-            return la[key].search(key);
+        return nodes[key];
     }
     
     public void remove(int key) {
-        if(la[key]==null)
-            return;
-        else {
-            la[key].remove(key);
-        }
+        nodes[key]=-1;
     }
 }
 
