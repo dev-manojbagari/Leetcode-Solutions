9239c8ecf2d6e737eef4d4faa3e3e23b723aa41e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-06 19:28:29 +0530

    Time: 9 ms (71.41%), Space: 41.5 MB (59.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,29 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] map = new int[26];
+        int[] cmap = new int[26];
         int start=0;
-        int dup =0;
-        int maxL=0;
-        int maxC=0;
-        char maxCC='A';
-        for(int end =0;end<s.length();end++){
+        int maxLen=0;
+        int unique=0,maxCount=0;
+        for(int end=0;end<s.length();end++){
             char c = s.charAt(end);
-            map[c-'A']++;
-            if(map[c-'A']>maxC){
-                maxC = map[c-'A'];
+            cmap[c-'A']++;
+            maxCount = Math.max(maxCount,cmap[c-'A']);
+            if(end-start+1-maxCount>k){
+                c = s.charAt(start++);
+                cmap[c-'A']--;
             }
             
-            if((end-start+1)-maxC>k){
-                c = s.charAt(start);
-                map[c-'A']--;
-                start++;
-            }
             
-
-            
-           maxL = Math.max(maxL,end-start+1); 
+                maxLen = Math.max(maxLen,end-start+1);
         }
         
-        return maxL;
+        return maxLen;
     }
 }
