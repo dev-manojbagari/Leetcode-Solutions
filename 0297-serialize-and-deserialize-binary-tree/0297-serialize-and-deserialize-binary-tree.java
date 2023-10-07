aeab6a331c99a2b4551d10c5b6372c83f18c8595
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-07 08:08:36 +0530

    Time: 9 ms (85.22%), Space: 44.6 MB (62.26%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -12,51 +12,41 @@ public class Codec {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
+        
         if(root==null)
             return sb.toString();
-        
-        serialize(root,sb);
-        
+        preorder(root,sb);
         return sb.toString();
     }
-
-    private void serialize(TreeNode root,StringBuilder sb){
-        if(root==null){
-            sb.append("#,");
-            return;
-        }
-        
-        sb.append(root.val+",");
-        
-        serialize(root.left,sb);
-        serialize(root.right,sb);
-        
-    }
     
+    void preorder(TreeNode node,StringBuilder sb){
+        if(node==null){
+           sb.append("#").append(",");
+            return;
+    }
+        
+        sb.append(node.val).append(",");
+        preorder(node.left,sb);
+        preorder(node.right,sb);
+    }
+
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data==null||data.length()==0)
+        if(data.length()==0)
             return null;
-        
         Queue<String> q = new LinkedList<>(Arrays.asList(data.split(",")));
         
         return deserialize(q);
     }
     
-    private TreeNode deserialize(Queue<String> q){
-        if(q.isEmpty())
+    TreeNode deserialize(Queue<String> q){
+        String  valStr = q.poll();
+        if(valStr==null||"#".equals(valStr))
             return null;
         
-        String val = q.poll();
-        
-        if(val.equals("#"))
-            return null;
-        
-        TreeNode node = new TreeNode(Integer.parseInt(val));
-        
-        node.left= deserialize(q);
-        node.right= deserialize(q);
-        
+        TreeNode node = new TreeNode(Integer.parseInt(valStr));
+        node.left = deserialize(q);
+        node.right = deserialize(q);
         return node;
     }
 }
