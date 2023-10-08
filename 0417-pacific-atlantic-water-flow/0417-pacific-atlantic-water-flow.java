92aa2949905dce156ecdd4eba625f556f05c2145
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-08 14:59:28 +0530

    Time: 4 ms (98.02%), Space: 45 MB (27.99%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,14 +1,13 @@
 class Solution {
-    public List<List<Integer>> pacificAtlantic(int[][] mat) {
-        int rows = mat.length,cols=mat[0].length;
-        
+    public List<List<Integer>> pacificAtlantic(int[][] h) {
+        int rows=h.length,cols=h[0].length;
         boolean[][] po = new boolean[rows][cols];
         boolean[][] ao = new boolean[rows][cols];
         
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(i==0||j==0){
-                    dfs(i,j,mat,po,-1);
+                    dfs(i,j,h,po,-1);
                 }
             }
         }
@@ -16,35 +15,33 @@ class Solution {
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(i==rows-1||j==cols-1){
-                    dfs(i,j,mat,ao,-1);
+                    dfs(i,j,h,ao,-1);
                 }
             }
         }
-        
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(po[i][j]&&ao[i][j]){
-                    res.add(Arrays.asList(i,j));
+                if(ao[i][j]&&po[i][j]){
+                    list.add(Arrays.asList(i,j));
                 }
             }
         }
         
-        return res;
+        
+        return list;
+        
     }
     
-    void dfs(int i,int j,int[][] mat,boolean[][] vis,int prev){
+     void dfs(int i,int j,int[][] grid,boolean[][] vis,int prev){
         
-        if(i<0||i>=mat.length||j<0||j>=mat[0].length||vis[i][j]||prev>mat[i][j])
+        if(i<0||i>=grid.length||j<0||j>=grid[0].length||vis[i][j]||prev>grid[i][j])
             return;
-        
         vis[i][j]=true;
         
-         dfs(i+1,j,mat,vis,mat[i][j]);
-         dfs(i,j+1,mat,vis,mat[i][j]);
-         dfs(i-1,j,mat,vis,mat[i][j]);
-         dfs(i,j-1,mat,vis,mat[i][j]);
+        dfs(i+1,j,grid,vis,grid[i][j]);
+        dfs(i,j+1,grid,vis,grid[i][j]);
+        dfs(i-1,j,grid,vis,grid[i][j]);
+        dfs(i,j-1,grid,vis,grid[i][j]);
     }
-    
-    
 }
