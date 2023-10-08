55165c19fefd05e10a2ac78bd731a56854c840a4
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-08 15:58:40 +0530

    Time: 2 ms (68.41%), Space: 44.3 MB (77.86%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,25 @@
 class Solution {
     public int jump(int[] nums) {
-       if(nums.length<=1)
-           return 0;
-        int ladder = nums[0];
-        int stairs = nums[0];
+        int n = nums.length;
+        if(n==1)
+            return 0;
+        int stair = 0;
+        int steps = nums[0];
+        int maxReach = nums[0];
         int jump=1;
-        for(int i=1;i<nums.length-1;i++){
-            if(i+nums[i]>ladder){
-                ladder = nums[i]+i;
+        for(int i=1;i<n;i++){
+            if(i==n-1)
+                return jump;
+            if(i+nums[i]>maxReach){
+                maxReach = i+nums[i];
             }
-            stairs--;
-            
-            if(stairs==0){
+            steps--;
+            if(steps==0){
+                steps = maxReach-i;
                 jump++;
-                stairs = ladder-i;
             }
         }
         
-        return jump;
-        
-    }
-}   
+        return -1;
+    } 
+}
