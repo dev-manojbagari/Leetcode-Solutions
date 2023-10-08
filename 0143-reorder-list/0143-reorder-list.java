3098493f93aa6fe77767bf0b7c0ea2df50eb951a
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-08 09:13:45 +0530

    Time: 3 ms (21.86%), Space: 46.6 MB (5.87%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,16 +10,19 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
+        if(head==null||head.next==null)
+            return;
         ListNode mid = getMid(head);
         ListNode midNext = mid.next;
-        mid.next = null;
+        mid.next=null;
         ListNode ptr1 = head;
         ListNode ptr2 = reverse(midNext);
         
         ListNode preHead = new ListNode(-1);
-        ListNode curr = preHead,temp=null;
-        int count =1;
-        while(ptr1!=null&&ptr2!=null){
+        ListNode curr =preHead,temp=null;
+        int count=1;
+        
+        while(ptr2!=null){
             if(count%2!=0){
                 temp = ptr1;
                 ptr1=ptr1.next;
@@ -29,44 +32,34 @@ class Solution {
                 ptr2=ptr2.next;
                 temp.next=null;
             }
-            
-            curr.next = temp;
-            curr= curr.next;
+            curr.next=temp;
+            curr=curr.next;
             count++;
         }
         if(ptr1!=null)
-            curr.next =ptr1;
-        else if(ptr2!=null)
-            curr.next = ptr2;
-        
-     //   head = preHead.next;
-        
-    }
-    
-    ListNode reverse(ListNode head){
-        ListNode curr = head,prev=null,next=null;
-        
-        while(curr!=null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        
-        return prev;        
+            curr.next=ptr1;
+        head=preHead.next;
     }
     
     ListNode getMid(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head.next;
-        
+        ListNode slow = head,fast=head.next;
         while(fast!=null&&fast.next!=null){
-            slow =slow.next;
-            fast = fast.next.next;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        
         return slow;
     }
     
+    ListNode reverse(ListNode head){
+        ListNode prev = null,curr=head,next=null;
+        
+        while(curr!=null){
+            next = curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
     
 }
