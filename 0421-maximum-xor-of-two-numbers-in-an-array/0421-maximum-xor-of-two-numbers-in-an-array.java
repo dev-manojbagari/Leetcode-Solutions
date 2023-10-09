a9e28baabfb30903066b76a7eeea335606401ac8
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-09 16:18:09 +0530

    Time: 617 ms (61.98%), Space: 220.7 MB (5.18%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -5,52 +5,76 @@ class Solution {
         Trie(){
             head = new Node();
         }
-        
-        void add(int val){
-            Node curr = head;
-            for(int i=31;i>=0;i--){
-                int bit = (val>>i)&1;
-                if(curr.next[bit]==null)
-                    curr.next[bit]=new Node();
-                curr = curr.next[bit];
-            }
-        }
-
-        int maxXor(int candidate){
-            Node curr = head;
-            int max=0;
-            for(int i=31;i>=0;i--){
-                int digit = (candidate>>i)&1;
-                if(curr.next[1-digit]!=null){
-                    max = max|(1<<i);
-                    curr=curr.next[1-digit];
-                }else{
-                    curr=curr.next[digit];
-                }
-            }
-            
-            return max;
-        }
-        
-        
         class Node{
             Node[] next;
+            int val;
             Node(){
                 next = new Node[2];
             }
         }
+        
+        void add(int num){
+            Node curr= head;
+            for(int i=31;i>=0;i--){
+                 int x = (num>>i)&1;
+                 if(curr.next[x]==null)
+                        curr.next[x]=new Node();
+                curr = curr.next[x];
+            }
+        }      
+        
+        int maxXor(int num){
+            Node curr = head;
+            int maxXor=0;
+            for(int i=31;i>=0;i--){
+                 int x = (num>>i)&1;
+                 if(curr.next[1-x]!=null){
+                     maxXor = maxXor|(1<<i);
+                     curr = curr.next[1-x];
+                 }else
+                    curr=curr.next[x]; 
+            }
+            return maxXor;
+        }
     }
     
+//             void insert(int num){
+//             Node curr =root;
+//             for(int i=31;i>=0;i--){
+//                 int bit = (num>>i)&1;
+//                 if(curr.child[bit]==null)
+//                     curr.child[bit] = new Node();
+//                 curr = curr.child[bit];
+//             }
+//         }
+        
+//         int maxXor(int num){
+//             Node curr =root;
+//             int maxXor = 0;
+//             for(int i=31;i>=0;i--){
+//                 int bit = (num>>i)&1;
+//                 if(curr.child[1-bit]!=null){
+//                     maxXor = maxXor|(1<<i);
+//                     curr=curr.child[1-bit];
+//                 }else
+//                     curr=curr.child[bit];
+//             }
+            
+//             return maxXor;
+//         }
     
     public int findMaximumXOR(int[] nums) {
-
-        Trie trie = new Trie();
-        for(int num:nums)
-            trie.add(num);
         
-        int max=0;
-        for(int num:nums)
-            max = Math.max(max, trie.maxXor(num));
+        Trie trie = new Trie();
+        for(int num:nums){
+            trie.add(num);
+        }
+        
+        int max =0;
+        
+        for(int num:nums){
+            max = Math.max(max,trie.maxXor(num));
+        }
         
         return max;
     }
