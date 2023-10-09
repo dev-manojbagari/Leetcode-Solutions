f845f322df80e28d5fc14032a8f6c4faacf4d276
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-09 16:58:05 +0530

    Time: 144 ms (74.19%), Space: 123.1 MB (69.36%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
@@ -1,74 +1,75 @@
 class Solution {
+    public int[] maximizeXor(int[] nums, int[][] queries) {
+        Arrays.sort(nums);
+        int index=0,n=queries.length; ;
+        int[][] temp  = new int[n][3];
+        for(int i=0;i<queries.length;i++){
+            temp[i][0] = queries[i][0];
+            temp[i][1] = queries[i][1];
+            temp[i][2] = i;
+        }
+        Arrays.sort(temp,(a,b)->a[1]-b[1]);
+        Trie trie = new Trie();
+        int[] res = new int[n];
+        for(int i=0;i<n;i++){
+            int num = temp[i][0];
+            int max= temp[i][1];
+            
+            while(index<nums.length&&nums[index]<=max){
+                trie.add(nums[index++]);
+            }
+            
+            if(trie.isEmpty())
+                res[temp[i][2]] = -1;
+            else
+                res[temp[i][2]] = trie.maxXor(num);
+        }
+        
+        return res;        
+        
+        
+    }
         class Trie{
         Node head;
         Trie(){
             head = new Node();
         }
-        
-        void add(int val){
-            Node curr = head;
-            for(int i=31;i>=0;i--){
-                int bit = (val>>i)&1;
-                if(curr.next[bit]==null)
-                    curr.next[bit]=new Node();
-                curr = curr.next[bit];
-            }
-        }
-
-        int maxXor(int candidate){
-            Node curr = head;
-            int max=0;
-            for(int i=31;i>=0;i--){
-                int digit = (candidate>>i)&1;
-                if(curr.next[1-digit]!=null){
-                    max = max|(1<<i);
-                    curr=curr.next[1-digit];
-                }else{
-                    curr=curr.next[digit];
-                }
-            }
-            
-            return max;
-        }
-        
-        
         class Node{
             Node[] next;
+            int val;
             Node(){
                 next = new Node[2];
             }
         }
-    }
-    public int[] maximizeXor(int[] nums, int[][] q) {
-       
-        int[][] temp = new int[q.length][3];
         
-        for(int i=0;i<q.length;i++){
-            temp[i][0]=q[i][0];
-            temp[i][1]=q[i][1];
-            temp[i][2]=i;
-        }
-        
-        Arrays.sort(temp,(a,b)->a[1]-b[1]);
-        Trie trie = new Trie();
-        int j=0;
-        Arrays.sort(nums);
-        int[] res = new int[q.length];
-       
-        for(int i=0;i<temp.length;i++){
-            
-            int limit = temp[i][1];
-            int oldJ = j;
-            int max=0;
-            while(j<nums.length&&nums[j]<=limit)
-                trie.add(nums[j++]);
-            if(j>0){
-                res[temp[i][2]]=trie.maxXor(temp[i][0]);
-            }else{
-                res[temp[i][2]]=-1;
+        void add(int num){
+            Node curr= head;
+            for(int i=31;i>=0;i--){
+                 int x = (num>>i)&1;
+                 if(curr.next[x]==null)
+                        curr.next[x]=new Node();
+                curr = curr.next[x];
             }
-        }   
+        }      
         
-        return res;
+        boolean isEmpty(){
+            return head.next[0]==null&&head.next[1]==null;
+        }
+        int maxXor(int num){
+            Node curr = head;
+            int maxXor=0;
+            for(int i=31;i>=0;i--){
+                 int x = (num>>i)&1;
+                 if(curr.next[1-x]!=null){
+                     maxXor = maxXor|(1<<i);
+                     curr = curr.next[1-x];
+                 }else
+                     curr=curr.next[x]; 
+            }
+            return maxXor;
+        }
     }
+    
+    
+  
 }
