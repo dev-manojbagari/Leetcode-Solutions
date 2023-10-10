91e43657c03f447a42a1a71224cc3f1ea6d506f6
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-10 09:29:41 +0530

    Time: 2176 ms (5.00%), Space: 55.7 MB (45.23%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         int n =nums.length;
+        shuffle(nums,n);
         int left=0,right=n-1;
         int ans = 0;
         while(left<=right){
@@ -31,7 +32,7 @@ class Solution {
         return start;
         }
         
-    void shuffet(int[] nums,int n){
+    void shuffle(int[] nums,int n){
         Random random = new Random();
         for(int i=n-1;i>=0;i--){
             int index = random.nextInt(i+1);
