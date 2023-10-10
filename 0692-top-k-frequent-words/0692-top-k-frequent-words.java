f94bbf48c6df64d4e39d0713df0f519939ce550c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-10 08:55:56 +0530

    Time: 6 ms (89.17%), Space: 43.3 MB (98.18%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -2,33 +2,33 @@ class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         Map<String,Integer> map = new HashMap<>();
         
-        for(String word:words){
-            map.put(word,map.getOrDefault(word,0)+1);    
-        }
+        for(String word:words)
+            map.put(word,map.getOrDefault(word,0)+1);
         
-        PriorityQueue<Map.Entry<String,Integer>> minHeap  = new PriorityQueue<>((a,b)->{
-            if(a.getValue()!=b.getValue())
-                return a.getValue()-b.getValue();
-            return b.getKey().compareTo(a.getKey());            
+        PriorityQueue<Map.Entry<String,Integer>> pq = new PriorityQueue<>((a,b)->{
+            
+            if(a.getValue().equals(b.getValue())){
+                return b.getKey().compareTo(a.getKey());
+            }else
+                return Integer.compare(a.getValue(),b.getValue());
         });
         
-        for(Map.Entry<String,Integer> entry:map.entrySet()){
-            if(minHeap.size()<k){
-                minHeap.offer(entry);
-            }else if(entry.getValue()>minHeap.peek().getValue()){
-                minHeap.poll();
-                minHeap.offer(entry);
-            }else if(entry.getValue()==minHeap.peek().getValue()&&entry.getKey().compareTo(minHeap.peek().getKey())<0){
-                minHeap.poll();
-                minHeap.offer(entry);
+        for(Map.Entry<String,Integer> entry :map.entrySet()){
+            if(pq.size()<k){
+                pq.offer(entry);
+            }else if(entry.getValue()>pq.peek().getValue()){
+                pq.poll();
+                pq.offer(entry);
+            }else if(entry.getValue()==pq.peek().getValue()&&entry.getKey().compareTo(pq.peek().getKey())<0){
+                pq.poll();
+                pq.offer(entry);
             }
         }
         
-        List<String> list = new ArrayList<>();
-
-        while(!minHeap.isEmpty())
-            list.add(0,minHeap.poll().getKey());
-        
-        return list;
+        List<String> res = new ArrayList<>();
+        while(!pq.isEmpty()){
+            res.add(0,pq.poll().getKey());
+        }
+        return res;
     }
 }
