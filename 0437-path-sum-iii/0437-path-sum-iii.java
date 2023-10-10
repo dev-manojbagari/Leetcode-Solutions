bbc8b706045199994f689822c76ab5a3fd98bfa8
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-10 19:16:04 +0530

    Time: 3 ms (79.46%), Space: 43.8 MB (7.74%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,33 +14,31 @@
  * }
  */
 class Solution {
-    int count =0;
+    int count=0;
     public int pathSum(TreeNode root, int targetSum) {
-
         Map<Long,Integer> map = new HashMap<>();
         map.put(0l,1);
-        pathSum(root,targetSum,0,map);
-        
+        pathSum(root,targetSum,map,0);
         return count;
     }
     
-    void pathSum(TreeNode root,int targetSum,long curSum,Map<Long,Integer> map){
+    void pathSum(TreeNode root,int targetSum,Map<Long,Integer> map,long curSum){
         if(root==null)
             return;
         
         curSum += root.val;
         
-        count += map.getOrDefault(curSum-targetSum,0);
-        
+        if(map.containsKey(curSum-targetSum))
+            count+=map.get(curSum-targetSum);
         map.put(curSum,map.getOrDefault(curSum,0)+1);
         
-        pathSum(root.left,targetSum,curSum,map);
-        pathSum(root.right,targetSum,curSum,map);
-      
+        pathSum(root.left,targetSum,map,curSum);
+        pathSum(root.right,targetSum,map,curSum);
+        
         if(map.get(curSum)==1)
             map.remove(curSum);
         else
             map.put(curSum,map.get(curSum)-1);
-        
+    
     }
 }
