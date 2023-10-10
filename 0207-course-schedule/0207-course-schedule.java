acfd4d891f61972f3a78107ba2bb1fd02dfc0005
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-10 11:49:07 +0530

    Time: 5 ms (66.31%), Space: 44.4 MB (23.89%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,37 +1,42 @@
 class Solution {
-    public boolean canFinish(int n, int[][] p) {
+    public boolean canFinish(int n, int[][] prereq) {
         List<List<Integer>> adjList = new ArrayList<>();
-        
+        int[] inDeg = new int[n];
         for(int i=0;i<n;i++)
             adjList.add(new ArrayList<>());
         
-        int[] inDegree = new int[n];
-        for(int i=0;i<p.length;i++){
-            adjList.get(p[i][1]).add(p[i][0]);
-            inDegree[p[i][0]]++;
+        for(int[] pre:prereq){
+            adjList.get(pre[1]).add(pre[0]);
+            inDeg[pre[0]]++;
         }
         
         Queue<Integer> q = new LinkedList<>();
         
-        for(int i=0;i<inDegree.length;i++){
-            if(inDegree[i]==0)
+        for(int i=0;i<inDeg.length;i++){
+            if(inDeg[i]==0){
                 q.offer(i);
+            }
         }
         
-        List<Integer> topoSort = new ArrayList<>();
+        boolean[] vis= new boolean[n];
+        List<Integer> list = new ArrayList<>();
         while(!q.isEmpty()){
             int node = q.poll();
-            topoSort.add(node);
-            for(int x:adjList.get(node))
-            {
-                inDegree[x]--;
-                if(inDegree[x]==0)
-                {
-                    q.offer(x);
+            vis[node]=true;
+            list.add(node);
+            for(int x:adjList.get(node)){
+                if(!vis[x]){
+                    inDeg[x]--;
+                    if(inDeg[x]==0){
+                        q.offer(x);
+                    }
                 }
             }
         }
         
-        return topoSort.size()==n;
+        return list.size()==n; 
+        
+        
+        
     }
 }
