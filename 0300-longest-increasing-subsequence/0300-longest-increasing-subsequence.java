ba26072cea3ab4298dfca4aefac07f0c951cee09
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-10 15:51:02 +0530

    Time: 3 ms (93.29%), Space: 43.1 MB (52.13%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,35 +1,35 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
+        int n=nums.length;
         int[] L = new int[n];
         L[0]=0;
-        int l=0;
-        for(int i=1;i<n;i++){
+        int len=0;
+        for(int i=0;i<n;i++){
             if(nums[i]<nums[L[0]]){
                 L[0]=i;
-            }else if(nums[i]>nums[L[l]]){
-                l++;
-                L[l]=i;
+            }else if(nums[L[len]]<nums[i]){
+                len++;
+                L[len]=i;
             }else{
-                int ceilingIndex = ceiling(nums,0,l,L,nums[i]);
+                int ceilingIndex = ceiling(nums,0,len,L,nums[i]);
                 L[ceilingIndex]=i;
-            }            
+            }
         }
         
-        
-        return l+1;
+        return len+1;
     }
     
     int ceiling(int[] nums,int left,int right,int[] L,int val){
         int ans = -1;
-        
         while(left<=right){
             int mid = left+(right-left)/2;
+            
             if(nums[L[mid]]>=val){
                 ans = mid;
-                right=mid-1;
-            }else
-                left = mid+1;
+                right = mid-1;
+            }else{
+                left=mid+1;
+            }
         }
         
         return ans;
