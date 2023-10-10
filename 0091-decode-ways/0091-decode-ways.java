cd409dfd95017c4257d14c1f6d4822f39daf5433
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-10 07:24:13 +0530

    Time: 1 ms (86.96%), Space: 41 MB (23.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,34 +1,18 @@
 class Solution {
     public int numDecodings(String s) {
-        if(s.charAt(0)=='0')
-            return 0;
-        return numDecodings(0,s,new Integer[s.length()]);
+        int n = s.length();
+        char[] ca = s.toCharArray();
+        int[] dp = new int[n+1];
+        dp[n]=1;
+        
+        for(int i=n-1;i>=0;i--){
+            if(ca[i]!='0'){
+            dp[i] =dp[i+1];
+            if(i+1<n&&(ca[i]=='1'||ca[i]=='2'&&ca[i+1]<='6'))
+                dp[i] += dp[i+2];
+            }
+        }
+        
+        return dp[0];
     }
-    
-    int numDecodings(int index,String s,Integer[] dp){
-        if(index>s.length())
-            return 0;
-        if(index==s.length())
-            return 1;
-        
-        if(s.charAt(index)=='0')
-            return 0;
-        
-        if(dp[index]!=null)
-            return dp[index];
-        
-        int ans = 0;
-        
-        ans += numDecodings(index+1,s,dp);
-        
-        if(s.charAt(index)=='1'||(index+1<s.length()&&s.charAt(index)=='2'&&s.charAt(index+1)<='6'))
-            ans += numDecodings(index+2,s,dp);
-        
-        
-        return dp[index]=ans;
-        
-        
-    }
-    
-    
 }
