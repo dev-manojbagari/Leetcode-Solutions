4927bb910041d7cf1385d18cd55bfe23187d45b7
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-10 08:01:17 +0530

    Time: 1 ms (96.95%), Space: 40.9 MB (33.65%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>(wordDict);
-        List<String> res = new ArrayList<>();
-        wordBreak(0,s,set,res,new StringBuilder());
-        return res;
+         Set<Integer> set = new HashSet(wordDict);
+         List<String> list = new ArrayList<>();
+         wordBreak(0,s,wordDict,set,list,new StringBuilder());
+        
+        return list;
     }
     
-    private void wordBreak(int i,String s,Set<String> set,List<String> list,StringBuilder sb){
-        if(i==s.length()){
+    void wordBreak(int index,String s,List<String> wordDict,Set<Integer> set,List<String> list,StringBuilder sb){
+        if(index==s.length()){
             list.add(sb.toString());
             return;
         }
-        
-        for(int j=i;j<s.length();j++){
-            if(set.contains(s.substring(i,j+1))){
+
+        for(int i=index;i<s.length();i++){
+            if(set.contains(s.substring(index,i+1))){
                 int len = sb.length();
-                if(sb.length()==0){
-                    sb.append(s.substring(i,j+1));
-                }else{
-                    sb.append(" "+s.substring(i,j+1));
-                }
-                
-                wordBreak(j+1,s,set,list,sb);
+                if(len==0)
+                    sb.append(s.substring(index,i+1));
+                else
+                    sb.append(" "+s.substring(index,i+1));
+                wordBreak(i+1,s,wordDict,set,list,sb);
                 sb.setLength(len);
             }
         }
+        
     }
 }
