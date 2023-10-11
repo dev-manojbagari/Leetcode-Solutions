f9b73bacc2345b006dba5cd862fa58251e15ddaf
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-11 14:46:42 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (58.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,9 +1,9 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int left=0,right=nums.length-1,i=0;
+        int left=0,right=nums.length-1;
+        int i=0;
         
         while(i<=right){
-            
             if(nums[i]==0){
                 swap(nums,i++,left++);
             }else if(nums[i]==2){
@@ -12,7 +12,6 @@ class Solution {
                 i++;
             }
         }
-        
         
     }
     
