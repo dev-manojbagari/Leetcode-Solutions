b6ae4e8f3ec020e5f4f717eb0f76ddc9bb12ebe2
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-11 08:33:39 +0530

    Time: 16 ms (24.12%), Space: 45.8 MB (38.89%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,37 +1,42 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int rows= mat.length,cols=mat[0].length;
+        int rows= mat.length;
+        int cols= mat[0].length;
+        
+        int[][] res = new int[rows][cols];
+
         Queue<int[]> q = new LinkedList<>();
         boolean[][] vis = new boolean[rows][cols];
         
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(mat[i][j]==0){
-                    q.offer(new int[]{i,j});
+                    q.offer(new int[]{i,j,0});
                     vis[i][j]=true;
                 }
             }
         }
+        
         int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
         while(!q.isEmpty()){
-            int[] cell = q.poll();
-            int i = cell[0];
-            int j = cell[1];
+            int[] data = q.poll();
+            int i  = data[0];
+            int j =  data[1];
             
             for(int[] dir:dirs){
                 int x = i+dir[0];
                 int y = j+dir[1];
                 
-                if(x<0||x>=mat.length||y<0||y>=mat[0].length||vis[x][y])
+                if(x<0||x>=mat.length||y<0||y>=mat[0].length||vis[x][y]||mat[x][y]==0)
                     continue;
                 
                 vis[x][y]=true;
-                mat[x][y] = mat[i][j]+1;
-                q.offer(new int[]{x,y});
+                res[x][y]=1+data[2];
+                q.offer(new int[]{x,y,1+data[2]});
+
             }
         }
         
-        
-        return mat;        
+        return res;
     }
 }
