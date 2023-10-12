278d066a0c9e78c04bc20aee4352f76fa33c3e15
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 07:59:16 +0530

    Time: 1 ms (100.00%), Space: 44.6 MB (55.79%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,5 +1,6 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        if(n==0)return tasks.length;
         int[] cmap = new int[26];
         
         for(char task:tasks){
@@ -11,13 +12,11 @@ class Solution {
         for(int count:cmap){
             if(maxFreq<count){
                 maxFreq = count;
-                groupSize=0;
-            }
-            if(maxFreq==count){
+                groupSize=1;
+            }else if(maxFreq==count){
                 groupSize++;
             }
         }
-        
         return Math.max(tasks.length,(n+1)*(maxFreq-1)+groupSize);
     }
 }
