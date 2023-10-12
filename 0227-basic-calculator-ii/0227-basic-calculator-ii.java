2eda7e096b6c30a488edf54c7b40228b943684ea
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 05:55:41 +0530

    Time: 20 ms (62.00%), Space: 45.1 MB (60.48%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,43 +1,35 @@
 class Solution {
     public int calculate(String s) {
-        char prevSign = '+';
+        char sign='+';
         int num=0;
         Stack<Integer> stack = new Stack<>();
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            if(isDigit(c)){
-                num = num*10+c-'0';
+            if('0'<=c&&c<='9'){
+                num = num*10+c-'0';                
             }
             
-            if(!isDigit(c)&&c!=' '||i==s.length()-1){
-                if(prevSign=='+')
-                {
-                    stack.push(+num);
-                }else if(prevSign=='-'){
+            if(!Character.isDigit(c)&&c!=' '||i==s.length()-1){
+                if(sign=='+'){
+                    stack.push(num);
+                }else if(sign=='-'){
                     stack.push(-num);
-                }else if(prevSign=='/'){
-                    stack.push(stack.pop()/num);
-                }else{
+                }else if(sign=='*'){
                     stack.push(stack.pop()*num);
+                }else if(sign=='/'){
+                    stack.push(stack.pop()/num);
                 }
-                
-                prevSign = c;
-                num=0;
+                    sign=c;
+                    num=0;
                 
             }
             
         }
-        
             int total=0;
             while(!stack.isEmpty()){
                 total += stack.pop();
             }
             
-            return total;            
-        
-    }
-    
-    boolean isDigit(char c){
-        return '0'<=c&&c<='9';
+            return total;
     }
 }
