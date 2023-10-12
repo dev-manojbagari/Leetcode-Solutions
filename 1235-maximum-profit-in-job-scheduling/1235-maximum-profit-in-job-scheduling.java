c05919c1845fff8d675420020255278c6cea2000
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 09:35:32 +0530

    Time: 27 ms (57.99%), Space: 52.6 MB (71.40%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,55 +1,55 @@
 class Solution {
-    // class Job{
-    //     int startTime,endTime,profit;
-    //     Job(int startTime,int endTime,int profit){
-    //         this.startTime=startTime;
-    //         this.endTime=endTime;
-    //         this.profit= profit;
-    //     }
-    // }
-    record Job(int startTime,int endTime,int profit){}
+    class Job{
+        int startTime,endTime,profit;
+        Job(int startTime,int endTime,int profit){
+            this.startTime=startTime;
+            this.endTime=endTime;
+            this.profit=profit;
+        }
+    }
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        
+        int n = startTime.length;
         List<Job> jobs = new ArrayList<>();
-        for(int i=0;i<startTime.length;i++){
+        
+        for(int i=0;i<n;i++){
             jobs.add(new Job(startTime[i],endTime[i],profit[i]));
         }
         
-        jobs.sort((a,b)->Integer.compare(a.endTime(),b.endTime()));
+        jobs.sort((a,b)->a.endTime-b.endTime);
         
-        
-        List<Integer> endTimes = new ArrayList<>();
+        List<Integer> dpEndTime = new ArrayList<>();
         List<Integer> dpProfit = new ArrayList<>();
-        endTimes.add(0);
+        dpEndTime.add(0);
         dpProfit.add(0);
+        
         for(Job job:jobs){
-            int prevJobId = prevJob(endTimes,job.startTime);
+            int prevIdx = largestSmaller(dpEndTime,job.startTime+1);
             
-            int inclProfit = dpProfit.get(prevJobId)+job.profit;
-            int exclProfit = dpProfit.get(dpProfit.size()-1);
+            int case1Profit = dpProfit.get(prevIdx)+job.profit;
+            int case2Profit = dpProfit.get(dpProfit.size()-1);
             
-            if(inclProfit>exclProfit){
-                endTimes.add(job.endTime());
-                dpProfit.add(inclProfit);
+            if(case1Profit>case2Profit){
+                dpEndTime.add(job.endTime);
+                dpProfit.add(case1Profit);
             }
+            
         }
         
         return dpProfit.get(dpProfit.size()-1);
     }
     
-    int prevJob(List<Integer> endTimes,int startTime){
-        int left =  0,right =endTimes.size()-1;
-        int ans =-1;
-        while(left<=right){
-            int mid  = left+(right-left)/2;
-            
-            if(endTimes.get(mid)<=startTime){
-                ans = mid;
-                left = mid+1;
-            }else
-                right = mid-1;
-        }
+    int largestSmaller(List<Integer> list,int val){
+        int left=0,right=list.size()-1;
+        int ans = right;
         
+        while(left<=right){
+             int mid = left+(right-left)/2;
+             if(list.get(mid)<val){
+                 ans = mid;
+                 left=mid+1;
+             }else
+                 right=mid-1;
+        }
         return ans;
     }
 }
