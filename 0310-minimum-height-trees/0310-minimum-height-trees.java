e9e9bd1cb4df315006712e2b06b52de458f2aedb
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 12:48:12 +0530

    Time: 20 ms (77.16%), Space: 55.7 MB (56.58%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,48 +1,49 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if(n==1)
-            return Arrays.asList(0);    
+        List<Integer> res = new ArrayList<>();
+        if(n==1){
+            res.add(0);
+            return res;
+        }
         List<List<Integer>> adjList = new ArrayList<>();
         
         for(int i=0;i<n;i++)
             adjList.add(new ArrayList<>());
         
-        int[] inDegree = new int[n];
+        int[] inD = new int[n];
         
-        for(int i=0;i<edges.length;i++)
-        {
-            adjList.get(edges[i][0]).add(edges[i][1]);
-            adjList.get(edges[i][1]).add(edges[i][0]);
-            inDegree[edges[i][0]]++;
-            inDegree[edges[i][1]]++;
+        for(int[] edge:edges){
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+            inD[edge[0]]++;
+            inD[edge[1]]++;
         }
         
-        Queue<Integer> q  = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         
-        for(int i=0;i<inDegree.length;i++){
-            if(inDegree[i]==1)
-                q.add(i);
+        
+        for(int i=0;i<n;i++){
+            if(inD[i]==1){
+                q.offer(i);
+            }
         }
         
         while(n>2){
-            int size  = q.size();
+            int size = q.size();
             n = n-size;
+            
             for(int i=0;i<size;i++){
                 int node = q.poll();
                 for(int x:adjList.get(node)){
-                     inDegree[x]--;
-                    if(inDegree[x]==1){
-                        q.add(x);
+                    inD[x]--;
+                    if(inD[x]==1){
+                        q.offer(x);
                     }
                 }
             }
         }
         
-        List<Integer> res = new ArrayList<>();
         res.addAll(q);
-        
         return res;
-        
-        
     }
 }
