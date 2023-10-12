7b1ac05add1bdde7173d84e690ba297bbbd606ba
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 06:10:07 +0530

    Time: 5 ms (90.42%), Space: 44.1 MB (42.13%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,35 +1,39 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
+        int sign = 1;
+        int num=0;
         int result=0;
-        int sign =1;
-        int number =0;
+        Stack<Integer> stack = new Stack<>();
+        
         for(char c:s.toCharArray()){
-            if(Character.isDigit(c)){
-                number = number*10+(c-'0');
+            
+            if('0'<=c&&c<='9'){
+                num = num*10+c-'0';
             }else if(c=='+'){
-                result += sign*number;
-                number=0;
+                result += sign*num;
                 sign=1;
+                num=0;
             }else if(c=='-'){
-                result += sign*number;
-                number=0;
+                result += sign*num;
                 sign=-1;
+                num=0;
             }else if(c=='('){
                 stack.push(result);
                 stack.push(sign);
                 result=0;
-                number=0;
                 sign=1;
             }else if(c==')'){
-                result += sign*number;
-                number =0;
-                result *= stack.pop();
-                result += stack.pop();
+                result +=sign*num;
+                num=0;
+                int prevSign = stack.pop();
+                int prevResult = stack.pop();
+                result = prevSign*result;
+                result += prevResult;
             }
+            
         }
         
-        result += sign*number;
+        result += sign*num;
         
         return result;
     }
