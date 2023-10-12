1866be761e6bf2b42100a812c1c0e671364870a7
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 06:42:29 +0530

    Time: 121 ms (80.95%), Space: 121 MB (7.31%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,47 +1,45 @@
 class TimeMap {
-    
-    class Node{
-        String value;int timestamp;
-        Node(String value,int timestamp){
-            this.value= value;
-            this.timestamp = timestamp;
+    class Data{
+        int timestamp;
+        String key,value;
+        Data(String key,String value,int timestamp){
+            this.key=key;
+            this.value=value;
+            this.timestamp=timestamp;
         }
     }
+    Map<String,List<Data>> map;
     
-    Map<String,List<Node>> map;
-   
     public TimeMap() {
         map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        map.computeIfAbsent(key,(k->new ArrayList<>())).add(new Node(value,timestamp));
+        map.computeIfAbsent(key,(k->new ArrayList<>())).add(new Data(key,value,timestamp));
     }
     
     public String get(String key, int timestamp) {
         if(!map.containsKey(key))
             return "";
-        else
-            return ceiling(map.get(key),timestamp);
+        return lastSmallerOrEqual(map.get(key),timestamp);
     }
     
-    String ceiling(List<Node> list,int timestamp){
-        String ans = "";
-        int left=0,right= list.size()-1;
-        
+    String lastSmallerOrEqual(List<Data> list,int timestamp){
+        int left=0,right=list.size()-1;
+        String ans ="";
         while(left<=right){
             int mid = left+(right-left)/2;
             
             if(list.get(mid).timestamp<=timestamp){
                 ans = list.get(mid).value;
-                left= mid+1;
+                left=mid+1;
             }else
                 right=mid-1;
         }
         
         return ans;
+        
     }
-    
 }
 
 /**
