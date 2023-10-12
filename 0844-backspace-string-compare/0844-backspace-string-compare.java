1c43a53b1507d5bba1eecf8a6d134dccc24cf9c5
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 16:06:06 +0530

    Time: 1 ms (85.85%), Space: 40.3 MB (81.51%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i = s.length()-1,j=t.length()-1;
-        int back =0;
-        
-        while(true){
-            while(i>=0&&(back>0||s.charAt(i)=='#'))
-            {
-                back += (s.charAt(i)=='#')?1:-1;
+        int i=s.length()-1,j=t.length()-1;
+        while(i>=0||j>=0){
+            int back=0;
+            while(i>=0&&(back>0||s.charAt(i)=='#')){
+                back += s.charAt(i)=='#'?1:-1;
                 i--;
             }
-            
             back=0;
-            
-            while(j>=0&&(back>0||t.charAt(j)=='#'))
-            {
-                back += (t.charAt(j)=='#')?1:-1;
+            while(j>=0&&(back>0||t.charAt(j)=='#')){
+                back += t.charAt(j)=='#'?1:-1;
                 j--;
             }
             
@@ -23,9 +18,10 @@ class Solution {
                 j--;
             }else
                 break;
+            
         }
         
+        return i==-1&&j==-1;
         
-        return i==-1&&j==-1;       
     }
 }
