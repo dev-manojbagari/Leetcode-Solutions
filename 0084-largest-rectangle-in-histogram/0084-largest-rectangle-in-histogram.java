f3c7cf1a991ddc67ae3425ccc9f00ca67768215b
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 08:41:45 +0530

    Time: 105 ms (31.40%), Space: 55.6 MB (56.36%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,37 +1,33 @@
 class Solution {
     public int largestRectangleArea(int[] h) {
         int n = h.length;
-        int[] SL = new int[n];
+        int sl[] = new int[n];
         Stack<Integer> stack1 = new Stack<>();
         
         for(int i=0;i<n;i++){
-            while(!stack1.isEmpty()&&h[stack1.peek()]>=h[i]){
+            while(!stack1.isEmpty()&&h[stack1.peek()]>=h[i])
                 stack1.pop();
-            }
             
-            SL[i] = stack1.isEmpty()?-1:stack1.peek();
+            sl[i]=stack1.isEmpty()?-1:stack1.peek();
             stack1.push(i);
         }
         
-        int[] SR = new int[n];
+        int sr[] = new int[n];
         Stack<Integer> stack2 = new Stack<>();
         
         for(int i=n-1;i>=0;i--){
-            while(!stack2.isEmpty()&&h[stack2.peek()]>=h[i]){
+            while(!stack2.isEmpty()&&h[stack2.peek()]>=h[i])
                 stack2.pop();
-            }
             
-            SR[i] = stack2.isEmpty()?n:stack2.peek();
+            sr[i]=stack2.isEmpty()?n:stack2.peek();
             stack2.push(i);
         }
-        
-        
-        int maxArea=0;
+        int max=0;
         for(int i=0;i<n;i++){
-            int curArea = (SR[i]-SL[i]-1)*h[i];
-            maxArea = Math.max(maxArea,curArea);
+            int width = sr[i]-sl[i]-1;
+            max = Math.max(max,width*h[i]);
         }
         
-        return maxArea;
+        return max;
     }
 }
