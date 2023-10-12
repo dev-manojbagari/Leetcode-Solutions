1a78215dd69c3f534fa3f856ac64a32ce49d20df
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-12 08:17:05 +0530

    Time: 3 ms (74.03%), Space: 44.8 MB (28.67%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,22 +1,26 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        if(n==0)return tasks.length;
-        int[] cmap = new int[26];
-        
-        for(char task:tasks){
-            cmap[task-'A']++;
-        }
-        
-        int maxFreq=0,groupSize=0;
-
-        for(int count:cmap){
-            if(maxFreq<count){
-                maxFreq = count;
-                groupSize=1;
-            }else if(maxFreq==count){
-                groupSize++;
+       if(n==0)return tasks.length;
+        int[] counter = new int[26];
+        int max = 0;
+        int maxCount = 0;
+        for(char task : tasks) {
+            counter[task - 'A']++;
+            if(max == counter[task - 'A']) {
+                maxCount++;
+            }
+            else if(max < counter[task - 'A']) {
+                max = counter[task - 'A'];
+                maxCount = 1;
             }
         }
-        return Math.max(tasks.length,(n+1)*(maxFreq-1)+groupSize);
+        
+        int partCount = max - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.length - max * maxCount;
+        int idles = Math.max(0,emptySlots - availableTasks);
+        
+        return tasks.length + idles;
     }
 }
