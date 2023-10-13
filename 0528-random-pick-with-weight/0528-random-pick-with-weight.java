efbd5ee7641ef57987f2367c1597bd62a2b691dd
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-13 09:18:41 +0530

    Time: 22 ms (97.92%), Space: 47 MB (93.85%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.java
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -1,38 +1,31 @@
 class Solution {
     int[] preSum;
-    Random random = new Random();
-    int[] w;
+    Random random;
     public Solution(int[] w) {
-        this.w=w;
         preSum = new int[w.length];
         preSum[0]=w[0];
+        random = new Random();
         for(int i=1;i<w.length;i++){
-            preSum[i]= preSum[i-1]+w[i];
+            preSum[i]=preSum[i-1]+w[i];
         }
-        // System.out.println(Arrays.toString(preSum));
     }
     
     public int pickIndex() {
-        
-        int randomVal = random.nextInt(preSum[preSum.length-1])+1;
-        int ceilingIndex = ceiling(preSum,randomVal);
-        
-        return ceilingIndex;
+        int randomVal = random.nextInt(preSum[preSum.length-1])+1; 
+        return ceiling(preSum,randomVal);     
     }
     
-    int ceiling(int[] preSum,int val){
+    int ceiling(int[] preSum, int val){
         int left=0,right=preSum.length-1;
-        int ans =-1;
-
+        int ans = -1;
         while(left<=right){
             int mid = left+(right-left)/2;
-            
-            if(val<=preSum[mid]){
+            if(preSum[mid]>=val){
                 ans = mid;
-                right= mid-1;
-            }else{
-                left = mid+1;
-        }}
+                right=mid-1;
+            }else
+                left=mid+1;
+        }
         
         return ans;
     }
