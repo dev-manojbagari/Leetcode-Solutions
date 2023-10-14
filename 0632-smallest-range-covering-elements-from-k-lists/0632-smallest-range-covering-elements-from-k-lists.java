7124b0d68d45d2f5b77ef4eee2f66c9097f65dc8
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-14 08:38:32 +0530

    Time: 29 ms (76.81%), Space: 49.7 MB (58.05%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -7,39 +7,37 @@ class Solution {
             this.listIndex=listIndex;
         }
     }
-    
     public int[] smallestRange(List<List<Integer>> nums) {
-        PriorityQueue<Data> pq = new PriorityQueue<>((a,b)->a.val-b.val);
-        int n = nums.size();
+        PriorityQueue<Data> pq = new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
         int max=Integer.MIN_VALUE;
-        
+        int n =nums.size();
         for(int i=0;i<n;i++){
             pq.offer(new Data(nums.get(i).get(0),0,i));
             max = Math.max(max,nums.get(i).get(0));
         }
         
         int[] res = new int[2];
+        int range = Integer.MAX_VALUE;
         int start=0;
-        int minRange = Integer.MAX_VALUE;
         while(pq.size()==n){
             Data data = pq.poll();
             
-            if(max-data.val+1<minRange){
-                minRange = max-data.val+1;
-                start = data.val;
+            if(max-data.val<range){
+                range=max-data.val;
+                res[0]=data.val;
+                res[1]=max;
+                //start=data.val;
             }
             
-            int curIndex = data.index;
-            int curListIndex = data.listIndex;
-            int curListSize =  nums.get(curListIndex).size();
+            if(data.index+1<nums.get(data.listIndex).size()){
+                Data next = new Data(nums.get(data.listIndex).get(data.index+1),data.index+1,data.listIndex);
+                pq.offer(next);
+                max=Math.max(max,next.val);
+            }          
             
-            if(curIndex+1<curListSize){
-                int num = nums.get(curListIndex).get(curIndex+1);
-                pq.offer(new Data(num,curIndex+1,curListIndex));
-                max = Math.max(max,num);
-            }
         }
-        
-        return new int[]{start ,start+minRange-1};
+        // res[0]=start;
+        // res[1]=start+range;
+        return res;
     }
 }
