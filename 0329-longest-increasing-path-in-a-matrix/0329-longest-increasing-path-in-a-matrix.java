4fd0a5ff5edb2e50896e992a2ceecca4d8e0efcf
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-14 11:24:00 +0530

    Time: 382 ms (5.02%), Space: 43.9 MB (41.53%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,38 +1,30 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        int rows= matrix.length,cols=matrix[0].length;
-        //boolean[][] vis = new boolean[rows][cols];
-        int max = 0;
+        int rows=matrix.length,cols=matrix[0].length;
+        int max=1;
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                max=Math.max(max,dfs(i,j,matrix,-1,0,new Integer[rows][cols]));
+                max = Math.max(max,dfs(i,j,matrix,-1,new Integer[rows][cols]));
             }
         }
         
         return max;
     }
     
-    int dfs(int i,int j,int[][] matrix,int prev,int len,Integer[][] dp){
+    int dfs(int i,int j,int[][] matrix,int prev,Integer[][] cache){
         
-        if(i<0||i>=matrix.length||j<0||j>=matrix[0].length||prev>=matrix[i][j])
+        if(i<0||i>=matrix.length||j<0||j>=matrix[0].length||prev>=matrix[i][j]){
             return 0;
+        }
         
-        if(dp[i][j]!=null)
-            return dp[i][j];
+        if(cache[i][j]!=null)
+            return cache[i][j];
         
-        // vis[i][j]=true;
-        prev = matrix[i][j];
-        int a = dfs(i+1,j,matrix,prev,len,dp);
-        int b = dfs(i,j+1,matrix,prev,len,dp);
-        int c = dfs(i-1,j,matrix,prev,len,dp);
-        int d = dfs(i,j-1,matrix,prev,len,dp);
+        int a = 1+dfs(i+1,j,matrix,matrix[i][j],cache);
+        int b = 1+dfs(i,j+1,matrix,matrix[i][j],cache);
+        int c = 1+dfs(i-1,j,matrix,matrix[i][j],cache);
+        int d = 1+dfs(i,j-1,matrix,matrix[i][j],cache);
         
-        // vis[i][j]=false;
-        
-        
-        return dp[i][j]=1+Math.max(Math.max(a,b),Math.max(c,d));
+        return cache[i][j]= Math.max(Math.max(a,b),Math.max(c,d));
     }
-    
-    
-    
 }
