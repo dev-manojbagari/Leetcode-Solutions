309829a99052b714042bf2ebae144fa7d1de5825
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-15 11:55:21 +0530

    Time: 3 ms (98.53%), Space: 41.6 MB (89.93%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,27 +1,45 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
+        int n = s.length();
         
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==' '){
-                if(sb.length()>0)
-                    stack.push(sb.toString());
-                sb = new StringBuilder();
-            }else{
-                sb.append(s.charAt(i));
-            }    
-        }
-        if(sb.length()>0)
-            stack.push(sb.toString());
-        StringBuilder res  = new  StringBuilder();
-        while(!stack.isEmpty()){
-            res.append(stack.pop());
-            if(stack.isEmpty())
-                break;
-            res.append(" ");
+        // Remove leading and trailing white spaces
+        int start = 0;
+        while (start < n && s.charAt(start) == ' ') {
+            start++;
         }
         
-        return res.toString();
+        int end = n - 1;
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+        
+        // Reverse each word and append to the result
+        for (int i = end; i >= start; i--) {
+            if (s.charAt(i) == ' ') {
+                continue;
+            }
+            
+            int j = i;
+            
+            // Find the start index of the current word
+            while (j >= start && s.charAt(j) != ' ') {
+                j--;
+            }
+            
+            // Append the reversed word to the result
+            for (int k = j + 1; k <= i; k++) {
+                sb.append(s.charAt(k));
+            }
+            
+			// Append a space between words except for the last word
+			if (j >= start) {
+				sb.append(' ');
+			}
+			
+			i = j + 1; // Move to the next word
+		}
+		
+		return sb.toString();
     }
 }
