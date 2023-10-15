a1ca7a2aaad5780d9b3afb4c8de45e7242461efc
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-15 06:38:55 +0530

    Time: 4 ms (59.24%), Space: 43.8 MB (11.06%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -12,14 +12,14 @@ class Solution {
         for(int i=n-1;i>=0;i--){
             while(!stack.isEmpty()&&nums2[stack.peek()]<=nums2[i])
                 stack.pop();
-            ng[i] = stack.isEmpty()?-1:nums2[stack.peek()];
+            ng[i] = stack.isEmpty()?-1:stack.peek();
             stack.push(i);
         }
         int[] res = new int[nums1.length];
         
         for(int i=0;i<nums1.length;i++){
             int nextGreater = ng[map.get(nums1[i])];
-            res[i] = nextGreater;
+            res[i] = nextGreater==-1?-1:nums2[nextGreater];
         }
             
         
