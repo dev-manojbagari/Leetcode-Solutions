8198aa852e97b3bd4f6563222429584fc5da9e2c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-15 06:38:14 +0530

    Time: 4 ms (59.24%), Space: 43.7 MB (26.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,29 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer,Integer> map = new HashMap<>();
-        int n = nums2.length;
+        
         for(int i=0;i<nums2.length;i++){
-            map.put(nums2[i],i);
+            map.put(nums2[i],i);    
         }
         
-        int[] ng = new int[n];
         Stack<Integer> stack = new Stack<>();
-        for(int i=nums2.length-1;i>=0;i--){
+        int n = nums2.length;
+        int[] ng = new int[n];
+        for(int i=n-1;i>=0;i--){
             while(!stack.isEmpty()&&nums2[stack.peek()]<=nums2[i])
                 stack.pop();
-                
-            ng[i] = stack.isEmpty()?-1:stack.peek();
+            ng[i] = stack.isEmpty()?-1:nums2[stack.peek()];
             stack.push(i);
-         }
-        
-        
-         int[] res  = new int[nums1.length];
-        
-         for(int i=0;i<nums1.length;i++){
-                int index = ng[map.get(nums1[i])];
-                res[i]=index!=-1?nums2[index]:-1;
-         }
-            
-            return res;
         }
+        int[] res = new int[nums1.length];
+        
+        for(int i=0;i<nums1.length;i++){
+            int nextGreater = ng[map.get(nums1[i])];
+            res[i] = nextGreater;
+        }
+            
+        
+        return res;
     }
+}
