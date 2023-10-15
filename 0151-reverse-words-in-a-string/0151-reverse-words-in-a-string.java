42a2556676b4e13f784906197fc873ae4d619507
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-15 11:43:51 +0530

    Time: 7 ms (76.84%), Space: 43.5 MB (50.05%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,30 +1,27 @@
 class Solution {
     public String reverseWords(String s) {
-       StringBuilder sb = new StringBuilder();
-	   Stack<String> stack = new Stack<>();
-
-	for (int i = 0; i < s.length(); i++) {
-	    char c = s.charAt(i);
-	    if (Character.isDigit(c) || Character.isAlphabetic(c)) {
-		sb.append(c);
-		if (i == s.length() - 1)
-		    stack.push(sb.toString());
-	    } else if (sb.length() != 0) {
-		stack.push(sb.toString());
-		sb = new StringBuilder();
-	    }
-	}
-
-	System.out.println(stack);
-
-	StringBuilder res = new StringBuilder();
-	while (!stack.isEmpty()) {
-	    res.append(stack.pop());
-	    if (stack.isEmpty())
-		break;
-	    res.append(" ");
-	}
-
-	return res.toString();
+        Stack<String> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                if(sb.length()>0)
+                    stack.push(sb.toString());
+                sb = new StringBuilder();
+            }else{
+                sb.append(s.charAt(i));
+            }    
+        }
+        if(sb.length()>0)
+            stack.push(sb.toString());
+        StringBuilder res  = new  StringBuilder();
+        while(!stack.isEmpty()){
+            res.append(stack.pop());
+            if(stack.isEmpty())
+                break;
+            res.append(" ");
+        }
+        
+        return res.toString();
     }
 }
