47d65152c10b5df6ca47ccee8614be1ca5e6460c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-15 12:58:31 +0530

    Time: 59 ms (34.11%), Space: 44.6 MB (73.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,28 +1,35 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int arraySum = Arrays.stream(nums).sum();
+        int arraySum=0;
+        for(int num:nums)
+            arraySum+=num;
         if(arraySum%2!=0)
             return false;
         return canPartition(nums,arraySum/2);
     }
     
-    boolean canPartition(int[] nums,int targetSum){
+    boolean canPartition(int[] nums,int sum){
         
-        boolean[][] dp = new boolean[targetSum+1][nums.length+1];
-        
-        for(int curSum=0;curSum<targetSum+1;curSum++){
-            for(int curLen=0;curLen<=nums.length;curLen++){
+        boolean[][] dp = new boolean[nums.length][sum+1];        
+       for(int index=0;index<nums.length;index++){ 
+            for(int curSum=0;curSum<=sum;curSum++){
                 if(curSum==0)
-                    dp[curSum][curLen]=true;
-                else if(curLen==0)
-                    dp[curSum][curLen]=false;
-                else if(curSum-nums[curLen-1]>=0)
-                    dp[curSum][curLen]= dp[curSum][curLen-1]||dp[curSum-nums[curLen-1]][curLen-1];
-                else 
-                    dp[curSum][curLen]= dp[curSum][curLen-1];
-            }
-        }
+                    dp[index][curSum]=true;
+                else if(index==0)
+                    dp[index][curSum]=false;
+                else{
+                    if(curSum-nums[index]>=0)
+                        dp[index][curSum] = dp[index-1][curSum]||dp[index-1][curSum-nums[index]];
+                    else
+                        dp[index][curSum] = dp[index-1][curSum];
+                    
+                }
+         }
+       }
         
-        return dp[targetSum][nums.length];
+        return dp[nums.length-1][sum];
+        
     }
+    
+    
 }
