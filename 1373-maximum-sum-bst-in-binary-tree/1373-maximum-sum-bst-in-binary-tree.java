9c9c71695be1239a45d6050602cd2afe2edec17d
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-16 10:49:22 +0530

    Time: 6 ms (86.41%), Space: 59.3 MB (37.55%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -14,39 +14,39 @@
  * }
  */
 class Solution {
-    class Tree{
-        boolean isBST=true;
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
+    class MinMax{
+        boolean isBst=true;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         int sum=0;
     }
     public int maxSumBST(TreeNode root) {
-        
-        int maxSum[] = {0};
-        maxSumBST(root,maxSum);
-        return maxSum[0];
+        int[] max = {0};
+        maxSumBST(root,max);
+        return max[0];
     }
     
-    private Tree maxSumBST(TreeNode root,int[] maxSum){
-        if(root==null)
-            return new Tree();
+    MinMax maxSumBST(TreeNode root,int[] max){
+        if(root==null){
+            return new MinMax();
+        }
         
-        Tree lst = maxSumBST(root.left,maxSum);
-        Tree rst = maxSumBST(root.right,maxSum);
-        Tree tree = new Tree();
-        if(lst.isBST&&rst.isBST&&lst.max<root.val&&root.val<rst.min){
-            tree.isBST = true;
-            tree.min = root.left!=null?lst.min:root.val;
-            tree.max= root.right!=null?rst.max:root.val;
-            tree.sum = root.val+lst.sum+rst.sum;
-            maxSum[0] = Math.max(maxSum[0],tree.sum);
+        MinMax left = maxSumBST(root.left,max);
+        MinMax right = maxSumBST(root.right,max);
+        MinMax minMax = new MinMax();
+        
+        if(left.isBst&&right.isBst&&left.max<root.val&&root.val<right.min){
+            minMax.isBst=true;
+            minMax.min = root.left!=null?left.min:root.val;
+            minMax.max = root.right!=null?right.max:root.val;
+            minMax.sum = left.sum+root.val+right.sum;
+            max[0] = Math.max(max[0],minMax.sum);
         }else
-            tree.isBST=false;
+            minMax.isBst=false;
         
-        return tree;
+        
+        return minMax;
     }
-    
-    
     
     
 }
