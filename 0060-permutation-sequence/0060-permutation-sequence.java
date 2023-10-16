c99edae33477aa2fd587bcefade602a926da5b74
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-16 12:43:16 +0530

    Time: 1 ms (99.09%), Space: 39.8 MB (73.54%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,28 +1,40 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        int fact[] = new int[n+1];
-        int mul=1;  
-        fact[0]=1;
+        int fact[] =  new int[]{1,1,2,6,24,120,720,5040,40320,362880};
+      //  for(int i=0;i<=9;)
+       //     fact[i]=fact(i);
         
-        for(int i=1;i<=n;i++){
-            fact[i]=fact[i-1]*i;
+        List<Integer> list = new ArrayList<>();
+        for(int i=1;i<=n;i++)
+            list.add(i);
+        StringBuilder res = new StringBuilder();
+        getPermutation(n,list,k,fact,res);
+        return res.toString();
+    }
+    
+    void getPermutation(int n,List<Integer> list,int k,int[] fact,StringBuilder res){
+        if(n==1){
+            res.append(list.get(list.size()-1));
+            return;
         }
+        int index= k/fact[n-1];
         
-        List<Integer> nums = new ArrayList<>();
+        if(k%fact[n-1]==0)
+            index--;
+
+        res.append(list.get(index));
         
-        for(int i=1;i<=n;i++){
-            nums.add(i);
-        }
+        list.remove(index);
         
-        StringBuilder sb = new StringBuilder();
-        k--;
-         for(int i=1;i<=n;i++){
-            int index = k/fact[n-i];
-            sb.append(String.valueOf(nums.get(index)));
-            nums.remove(index);
-            k = k-index*fact[n-i];
-         }
+        k = k-(index*fact[n-1]);
         
-        return sb.toString();
+        getPermutation(n-1,list,k,fact,res);
+    }
+    
+    int fact(int n){
+        if(n==0||n==1)
+            return 1;
+        
+        return n*fact(n-1);
     }
 }
