352c9c3d869914515ba139107e70297f1b91f192
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-16 16:47:25 +0530

    Time: 3 ms (76.94%), Space: 40.6 MB (98.87%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,27 +1,25 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        if(num1.equals("0")||num2.equals("0"))
+        if("0".equals(num1)||"0".equals(num2))
             return "0";
         int[] res = new int[num1.length()+num2.length()-1];
         
         for(int i=0;i<num1.length();i++){
             for(int j=0;j<num2.length();j++){
-                res[i+j] += (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
-            }
-        }
-        int carry=0;
-        for(int i=res.length-1;i>0;i--){
-            int sum = res[i]+carry;
-            res[i] = (sum)%10;
-            carry = (sum)/10;
+                res[i+j]+=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+            }    
         }
         
-        if(carry>0)
-            res[0] += carry;
+        
+        for(int i=res.length-1;i>0;i--){
+            res[i-1]+=res[i]/10;
+            res[i] = res[i]%10;
+        }
         
         StringBuilder sb = new StringBuilder();
-        for(int num:res)
+        for(int num:res){
             sb.append(num);
+        }
         
         return sb.toString();
     }
