c3c429b9a14f1eccff9b731337f9395c8597c3f9
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-16 11:38:39 +0530

    Time: 64 ms (38.65%), Space: 71.8 MB (47.36%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,42 +1,49 @@
 class Solution {
+    class Data{
+        int oneCount;
+        int zeroCount;
+        Data(int zeroCount,int oneCount){
+            this.zeroCount=zeroCount;
+            this.oneCount=oneCount;
+        }
+    }
     public int findMaxForm(String[] strs, int m, int n) {
-        return findMaxForm(0,strs,m,n,new Integer[strs.length+1][m+1][n+1]);
+        List<Data> list = new ArrayList<>();
+        for(String str:strs){
+            int count1=0,count0=0;
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)=='0')
+                    count0++;
+                else
+                    count1++;
+            }
+            if(count0<=m&&count1<=n)
+                list.add(new Data(count0,count1));
+        }
+        
+        return findMaxForm(0,list,m,n,new Integer[list.size()][m+1][n+1]);
     }
     
-    int findMaxForm(int i,String[] strs,int zeroCount,int oneCount,Integer[][][] dp){
-        if(i==strs.length||(zeroCount==0&&oneCount==0))
+    int findMaxForm(int index,List<Data> list,int maxZeros,int maxOnes,Integer[][][] dp){
+        if(index==list.size()||(maxZeros==0&&maxOnes==0))
+        {
             return 0;
+        }
         
-        if(dp[i][zeroCount][oneCount]!=null)
-            return dp[i][zeroCount][oneCount];
+        if(dp[index][maxZeros][maxOnes]!=null)
+            return dp[index][maxZeros][maxOnes];
         
-        int[] count = countZeroOne(strs[i]);
-        
+        int zeroCount = list.get(index).zeroCount;
+        int oneCount = list.get(index).oneCount;
         int ans =0;
-        int notInclude = findMaxForm(i+1,strs,zeroCount,oneCount,dp);
-        int include=0;
-        if(zeroCount>=count[0]&&oneCount>=count[1]){
-                
-            include = 1+ findMaxForm(i+1,strs,zeroCount-count[0],oneCount-count[1],dp);
+        if(zeroCount>maxZeros||oneCount>maxOnes){
+            return dp[index][maxZeros][maxOnes] =findMaxForm(index+1,list,maxZeros,maxOnes,dp);
         }
         
+        int exclude = findMaxForm(index+1,list,maxZeros,maxOnes,dp);
+        int include = 1+findMaxForm(index+1,list,maxZeros-zeroCount,maxOnes-oneCount,dp);
         
-        return dp[i][zeroCount][oneCount]=Math.max(include,notInclude);
+        return dp[index][maxZeros][maxOnes]=Math.max(exclude,include);
+        
     }
-    
-    Map<String,int[]> map = new HashMap<>();
-    int[] countZeroOne(String str){
-        if(map.containsKey(str))
-            return map.get(str);
-        int zero=0,one=0;
-        for(char c:str.toCharArray()){
-            if(c=='0')
-                zero++;
-            else
-                one++;
-        }
-        map.put(str,new int[]{zero,one});
-        return new int[]{zero,one};
-    }
-    
 }
