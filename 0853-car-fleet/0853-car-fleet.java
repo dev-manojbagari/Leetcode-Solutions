0d7a74fc5a63d6ee8f5fa704232451ed4aa3947e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-17 14:48:17 +0530

    Time: 106 ms (13.98%), Space: 57.9 MB (23.76%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,34 +1,21 @@
 class Solution {
-    class car {
-        int pos;
-        double time;
-        car(int pos,double time){
-            this.pos=pos;
-            this.time=time;
-        }
-    }
     public int carFleet(int target, int[] position, int[] speed) {
-        car[] cars = new car[position.length];
-        
+     TreeMap<Integer,Integer> treeMap = new TreeMap<>();
         for(int i=0;i<position.length;i++){
-            double time = (target-position[i])/(speed[i]*1.0);
-            cars[i]=new car(position[i],time);
+            treeMap.put(target-position[i],speed[i]);
         }
-            Arrays.sort(cars,(a,b)->b.pos-a.pos);
-        // for(car c:cars)
-        // {
-        //     System.out.println(c.pos+" "+c.time);
-        // }
-        int carFleet=1;
-        double time = cars[0].time;
-        for(int i=1;i<cars.length;i++){
-            if(cars[i].time>time){
-                carFleet++;
-                time = cars[i].time;
+        
+        double maxTimeToTarget = 0;
+        int fleet = 0;
+        for(int key:treeMap.keySet()){
+            int curDistance = key;
+            int curSpeed = treeMap.get(key);
+            double curTime = (curDistance*1.0)/curSpeed;
+            if(curTime>maxTimeToTarget){
+                maxTimeToTarget=curTime;
+                fleet++;
             }
         }
-        
-            
-        return carFleet;    
+        return fleet;        
     }
 }
