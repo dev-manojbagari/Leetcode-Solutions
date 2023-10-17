b42cd08a1c8e1d6ccdd62605c036861925441e57
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-17 15:04:00 +0530

    Time: 45 ms (89.66%), Space: 56.7 MB (22.69%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -6,27 +6,21 @@ class DetectSquares {
     }
     
     public void add(int[] point) {
-        if(count[point[0]][point[1]]==0){
+        if(count[point[0]][point[1]]==0)
             list.add(point);
-        }
-        
         count[point[0]][point[1]]++;
     }
     
     public int count(int[] point) {
-        int c =0;
-        
-        for(int[] curPoint:list){
-            if(curPoint[0]==point[0]&&curPoint[1]==point[1])
-                continue;
-                        
-            if(Math.abs(curPoint[0]-point[0])==Math.abs(curPoint[1]-point[1]))
-                c += count[curPoint[0]][point[1]]*count[point[0]][curPoint[1]]*count[curPoint[0]][curPoint[1]];
-            
+        int total=0;
+        for(int[] p:list){
+             if(p[0]==point[0]&&p[1]==point[1])
+                 continue;
+            if(Math.abs(p[0]-point[0])==Math.abs(p[1]-point[1])){
+                total += count[point[0]][p[1]]*count[p[0]][point[1]]*count[p[0]][p[1]];
+            }
         }
-        
-        
-        return c;
+        return total;   
     }
 }
 
