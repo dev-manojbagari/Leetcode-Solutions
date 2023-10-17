3ee0d12e5b18e4a5e4b934810885bbe2d36e7a13
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-17 07:38:45 +0530

    Time: 5 ms (36.86%), Space: 63.2 MB (5.06%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,23 +1,25 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-         if(s3.length()!=s1.length()+s2.length())
-              return false;
-        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
-        
-        
-        for(int i=0;i<s1.length()+1;i++){
-            for(int j=0;j<s2.length()+1;j++){
-                if(i==0&&j==0)
-                    dp[i][j]=true;
-                else if(i==0)
-                    dp[i][j] = dp[i][j-1]&&s3.charAt(i+j-1)==s2.charAt(j-1);
-                else if(j==0)
-                    dp[i][j] = dp[i-1][j]&&s3.charAt(i+j-1)==s1.charAt(i-1);
-                else
-                    dp[i][j] = dp[i-1][j]&&s3.charAt(i+j-1)==s1.charAt(i-1)|| dp[i][j-1]&&s3.charAt(i+j-1)==s2.charAt(j-1);
-            }
-        }
-        
-        return dp[s1.length()][s2.length()];
+        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
     }
+    
+    boolean isInterleave(int i,int j,int k,String s1,String s2,String s3,Boolean[][][] dp){
+        if(k==s3.length())
+            return i==s1.length()&&j==s2.length();
+        
+        if(dp[i][j][k]!=null)
+            return dp[i][j][k];
+        boolean ans = false;
+        if(i<s1.length()&&j<s2.length()&&s1.charAt(i)==s3.charAt(k)&&s2.charAt(j)==s3.charAt(k))
+            ans= isInterleave(i+1,j,k+1,s1,s2,s3,dp)||isInterleave(i,j+1,k+1,s1,s2,s3,dp);
+        else if(i<s1.length()&&s1.charAt(i)==s3.charAt(k)){
+            ans= isInterleave(i+1,j,k+1,s1,s2,s3,dp);
+        }else if(j<s2.length()&&s2.charAt(j)==s3.charAt(k)){
+            ans= isInterleave(i,j+1,k+1,s1,s2,s3,dp);
+        }else
+            ans= false;
+        
+        return dp[i][j][k]=ans;
+    }
+    
 }
