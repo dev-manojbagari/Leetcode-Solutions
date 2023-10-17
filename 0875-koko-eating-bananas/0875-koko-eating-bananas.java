6cf761bb806ff06528f7c67793751ee5fadab5cb
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-17 08:07:05 +0530

    Time: 11 ms (91.41%), Space: 43.6 MB (96.95%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,40 +1,35 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int max=Integer.MIN_VALUE;
+        int max=0;
+        for(int num:piles)
+            max=Math.max(max,num);
         
-        for(int num:piles){
-            max = Math.max(max,num);
-        }
-        
-        int left = 1,right=max;
-        int ans=0;
+        int left=1,right=max;
+        int ans =-1;
         
         while(left<=right){
-            int mid = left+(right-left)/2;
+            int mid= left+(right-left)/2;
             
-            if(countTime(mid,piles)<=h){
-                ans = mid;
+            if(countHour(piles,mid)<=h){
+                ans =mid;
                 right=mid-1;
             }else{
-                left =mid+1;
+                left=mid+1;
             }
         }
-        
         
         return ans;
     }
     
-    long countTime(int freq,int[] piles){
-        long count =0;
+    long countHour(int[] piles,int k){
+        long count=0;
         
-        for(int pile :piles){
-            count += pile/freq;
-            if(pile%freq!=0)
+        for(int i=0;i<piles.length;i++){
+            count+= piles[i]/k;
+            if((piles[i]%k)>0)
                 count++;
         }
         
         return count;
     }
-    
-    
 }
