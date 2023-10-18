4a72c6b59dd4942400234fa8e1589094cdab1dca
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-18 12:49:19 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (46.83%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,35 +1,33 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int row=matrix.length,col=matrix[0].length;
-        int left=matrix[0][0],right=matrix[row-1][col-1];
+        int min=matrix[0][0],max=matrix[matrix.length-1][matrix[0].length-1];
+
+        int left=min,right=max;
         int ans =0;
         while(left<=right){
-            int mid = left+(right-left)/2;
-            int cs = countSmaller(matrix,mid);
-            if(cs>=k){
-                 ans = mid;
-                 right=mid-1;}
-            else{
+            int mid= left+(right-left)/2;
+            int index = countSmallerOrEqual(matrix,mid);
+            if(index>=k){
+                ans = mid;
+                right=mid-1;
+            }else
                 left=mid+1;
-            }
         }
         
-        return ans;
-        
+        return ans;        
     }
     
-    int countSmaller(int[][] matrix,int val){
+    int countSmallerOrEqual(int[][] matrix,int val){
+        int count=0;
         int row = matrix.length-1,col=0;
-        int count =0;
-        while(col<matrix[0].length&&row>=0){
-            
+        
+        while(row>=0&&col<matrix[0].length){
             if(matrix[row][col]<=val){
-                count+=row+1; 
+                count+=row+1;
                 col++;
             }else
                 row--;
         }
-        return count;
+        return count;        
     }
-    
 }
