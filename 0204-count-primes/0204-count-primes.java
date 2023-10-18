7587f18d71bad7142a2a106fe2b4228f0e1e512e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-18 08:43:01 +0530

    Time: 372 ms (5.02%), Space: 47.8 MB (61.12%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        
+        for(int i=2;i<n;i++){
+            for(int j=2;i*j<n;j++){
+                notPrime[i*j]=true;
+            }
+        }
+        
+        int count=0;
+        for(int i=2;i<n;i++)
+            if(!notPrime[i])
+                count++;
+        
+        return count;
+                
+    }
+}
