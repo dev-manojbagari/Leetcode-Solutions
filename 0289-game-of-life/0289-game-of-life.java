5c448bcccebf221c190a749dec0af365c469db53
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-18 11:24:02 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (99.32%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,46 +1,47 @@
 class Solution {
-    int live = 2,dead=3;
+    int live=2,dead=3;
     public void gameOfLife(int[][] board) {
-        boolean vis[][] = new boolean[board.length][board[0].length];
-        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{-1,1},{1,1},{1,-1}};
-        
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                int count =countNeighbour(i,j,board,vis,dirs);
+        int rows=board.length,cols=board[0].length;
+        int[][] dirs ={{1,0},{0,1},{-1,0},{0,-1},{-1,1},{1,-1},{1,1},{-1,-1}};
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                int aliveNeighbors = countNeighbors(i,j,board,dirs);
                 
-                if(board[i][j]==0){
-                    if(count==3)
-                        board[i][j]=live;
-                }else if(board[i][j]==1){
-                    if(count<2||count>3)
+                if(board[i][j]==1){
+                    if(aliveNeighbors<2||aliveNeighbors>3){
                         board[i][j]=dead;
+                    }
+                }else if(board[i][j]==0){
+                    if(aliveNeighbors==3){
+                        board[i][j]=live;
+                    }
                 }
             }
         }
         
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==dead)
-                    board[i][j]=0;
-                else if(board[i][j]==live)
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(board[i][j]==live)
                     board[i][j]=1;
+                else if(board[i][j]==dead)
+                    board[i][j]=0;
             }
         }
     }
     
-    int countNeighbour(int i,int j,int[][] board,boolean[][] vis,int[][] dirs){
-           int count =0;        
-           for(int[] dir:dirs){
-               int x = i+dir[0];
-               int y = j+dir[1];
-               
-               if(x<0||x>=board.length||y<0||y>=board[0].length)
-                    continue; 
-                   
-                if(board[x][y]==1||board[x][y]==dead)
-                    count++;
-           }         
+    int countNeighbors(int i,int j,int[][] board,int[][] dirs){
+        int count=0;
+        for(int[] dir:dirs){
+            int x =i+dir[0];
+            int y =j+dir[1];
+            
+            if(x<0||x>=board.length||y<0||y>=board[0].length)
+                continue;
+            if(board[x][y]==1||board[x][y]==dead)
+                count++;
+        }        
         
         return count;        
+        
     }
 }
