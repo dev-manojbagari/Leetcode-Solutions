4de52715b463a540cf7ddc89da0cd6b3d96d93a9
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-19 09:41:26 +0530

    Time: 15 ms (40.65%), Space: 46.2 MB (26.67%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,36 +1,29 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int rows= mat.length;
-        int cols= mat[0].length;
-        
-        int[][] res = new int[rows][cols];
-
+        int rows=mat.length,cols=mat[0].length;
         Queue<int[]> q = new LinkedList<>();
-        
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(mat[i][j]==0){
                     q.offer(new int[]{i,j,0});
-                }
+                }        
             }
         }
-        
-        int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+        int[][] dirs={{0,1},{1,0},{-1,0},{0,-1}};       
+        int[][] res = new int[rows][cols];
         while(!q.isEmpty()){
             int[] data = q.poll();
-            int i  = data[0];
-            int j =  data[1];
-            
+            int i = data[0];
+            int j = data[1];
             for(int[] dir:dirs){
                 int x = i+dir[0];
                 int y = j+dir[1];
                 
-                if(x<0||x>=mat.length||y<0||y>=mat[0].length)
+                if(x<0||x>=rows||y<0||y>=cols||mat[x][y]==0)
                     continue;
-                
-                if(mat[x][y]==1&&res[x][y]==0){
-                res[x][y]=1+data[2];
-                q.offer(new int[]{x,y,1+data[2]});
+                if(res[x][y]==0){
+                    res[x][y]=1+data[2];
+                    q.offer(new int[]{x,y,1+data[2]});
                 }
             }
         }
