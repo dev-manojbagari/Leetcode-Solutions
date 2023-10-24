4de56ddf3d5d712ad7ed5f02503c4035032ffd19
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-24 17:14:02 +0530

    Time: 2 ms (67.50%), Space: 40 MB (43.18%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,40 +1,43 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        int fact[] = new int[10];
+        int[] fact = new int[10];
         for(int i=0;i<=9;i++)
             fact[i]=fact(i);
         
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list= new ArrayList<>();
         for(int i=1;i<=n;i++)
             list.add(i);
-        StringBuilder res = new StringBuilder();
-        getPermutation(n,list,k,fact,res);
-        return res.toString();
+        StringBuilder sb = new StringBuilder();
+        getPermutation(n,k,list,fact,sb);
+        
+        return sb.toString();
     }
     
-    void getPermutation(int n,List<Integer> list,int k,int[] fact,StringBuilder res){
+    void getPermutation(int n,int k,List<Integer> list,int[] fact, StringBuilder sb){
         if(n==1){
-            res.append(list.get(list.size()-1));
+            sb.append(list.get(list.size()-1));
             return;
         }
-        int index= k/fact[n-1];
+        int index = k/fact[n-1];
         
         if(k%fact[n-1]==0)
             index--;
-
-        res.append(list.get(index));
         
+        sb.append(list.get(index)+"");
         list.remove(index);
         
-        k = k-(index*fact[n-1]);
+        k = k-index*fact[n-1];
         
-        getPermutation(n-1,list,k,fact,res);
+        getPermutation(n-1,k,list,fact,sb);
     }
     
-    int fact(int n){
-        if(n==0||n==1)
+    
+    
+    
+    
+    int fact(int i){
+        if(i==0)
             return 1;
-        
-        return n*fact(n-1);
+        return i*fact(i-1);
     }
 }
