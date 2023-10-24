be0f45e228f461b7028862498b8f3281dad04e77
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-24 15:47:23 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (94.24%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int dp1 = 0;
-        int dp2 = 0;
-        int ans = 0;
+        int dp1=0;
+        int dp2=0;
         for(int i=0;i<cost.length;i++){
-            ans = Math.min(dp1,dp2)+cost[i];
+            int dp = cost[i]+Math.min(dp1,dp2);
             dp2=dp1;
-            dp1=ans;
+            dp1=dp;
         }
+        
         return Math.min(dp1,dp2);
     }
 }
