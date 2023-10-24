2c33cfbd06d8e72db056e9b11e0da93ce35e0548
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-24 15:54:57 +0530

    Time: 19 ms (64.17%), Space: 49.7 MB (60.97%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,26 +1,57 @@
 class MyHashMap {
-   int[] arr = new int[1000001];
+    //Load factor = 10000/size = 0.75
+    List<int[]>[] lists; //using List<List<int[]>> lists is fine as well.
+    int size = 13000;
+
+    //O(size)
+    /** Initialize your data structure here. */
     public MyHashMap() {
-        Arrays.fill(arr,-1);
+        lists = new ArrayList[size];
+        for(int i=0; i<lists.length; i++)
+            lists[i] = new ArrayList<>(); //don't use linkedlist in my version which makes put() O(L^2)
     }
     
+    public int getHashcode(int key)
+    {
+        return key%size;
+    }
+    
+    //O(L)
+    /** value will always be non-negative. */
     public void put(int key, int value) {
-        arr[key]=value;
+        int index = getHashcode(key);
+        for(int i=0; i<lists[index].size(); i++)
+        {
+            if(lists[index].get(i)[0]==key)
+            {
+                lists[index].get(i)[1]=value;
+                return;
+            }
+        }
+        lists[index].add(new int[]{key, value});
     }
     
+    //O(L)
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return arr[key];
+        int index = getHashcode(key);
+        for(int i=0; i<lists[index].size(); i++)
+        {
+            if(lists[index].get(i)[0]==key)
+                return lists[index].get(i)[1];
+        }
+        return -1;    
     }
     
+    //O(L)
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        arr[key]=-1;
+        int index = getHashcode(key);
+        for(int i=0; i<lists[index].size(); i++)
+        {
+            if(lists[index].get(i)[0]==key)
+                lists[index].remove(i);
+        }
+        return;
     }
 }
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
