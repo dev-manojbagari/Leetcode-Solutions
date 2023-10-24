a832df16b313a495eb4f5bd511e2283e006da638
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-24 15:57:23 +0530

    Time: 13 ms (99.64%), Space: 47 MB (82.07%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,57 +1,49 @@
-class MyHashMap {
-    //Load factor = 10000/size = 0.75
-    List<int[]>[] lists; //using List<List<int[]>> lists is fine as well.
-    int size = 13000;
+ class MyHashMap{
+        final ListNode[] nodes = new ListNode[10_000];
 
-    //O(size)
-    /** Initialize your data structure here. */
-    public MyHashMap() {
-        lists = new LinkedList[size];
-        for(int i=0; i<lists.length; i++)
-            lists[i] = new LinkedList<>(); //don't use linkedlist in my version which makes put() O(L^2)
-    }
-    
-    public int getHashcode(int key)
-    {
-        return key%size;
-    }
-    
-    //O(L)
-    /** value will always be non-negative. */
-    public void put(int key, int value) {
-        int index = getHashcode(key);
-        for(int i=0; i<lists[index].size(); i++)
-        {
-            if(lists[index].get(i)[0]==key)
-            {
-                lists[index].get(i)[1]=value;
-                return;
+        public void put(int key, int value){
+            int i = idx(key);
+            if(nodes[i] == null)
+                nodes[i] = new ListNode(-1, -1);
+            ListNode prev = find(nodes[i], key);
+            if(prev.next == null)
+                prev.next = new ListNode(key, value);
+            else prev.next.val = value;
+        }
+
+        public int get(int key){
+            int i = idx(key);
+            if(nodes[i] == null)
+                return -1;
+            ListNode node = find(nodes[i], key);
+            return node.next == null ? -1 : node.next.val;
+        }
+
+        public void remove(int key){
+            int i = idx(key);
+            if(nodes[i] != null){
+                ListNode prev = find(nodes[i], key);
+                if(prev.next != null)
+                    prev.next = prev.next.next;
             }
         }
-        lists[index].add(new int[]{key, value});
-    }
-    
-    //O(L)
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int get(int key) {
-        int index = getHashcode(key);
-        for(int i=0; i<lists[index].size(); i++)
-        {
-            if(lists[index].get(i)[0]==key)
-                return lists[index].get(i)[1];
+
+        int idx(int key){return Integer.hashCode(key) % nodes.length;}
+
+        ListNode find(ListNode bucket, int key){
+            ListNode node = bucket, prev = null;
+            for(; node != null && node.key != key; node = node.next)
+                prev = node;
+            return prev;
         }
-        return -1;    
-    }
-    
-    //O(L)
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    public void remove(int key) {
-        int index = getHashcode(key);
-        for(int i=0; i<lists[index].size(); i++)
-        {
-            if(lists[index].get(i)[0]==key)
-                lists[index].remove(i);
+
+        class ListNode{
+            int key, val;
+            ListNode next;
+
+            ListNode(int key, int val){
+                this.key = key;
+                this.val = val;
+            }
         }
-        return;
     }
-}
