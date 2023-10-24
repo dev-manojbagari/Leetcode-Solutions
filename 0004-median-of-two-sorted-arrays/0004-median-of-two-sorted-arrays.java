b91b2f8da19f04c70b7ad77b1cf381b2bdce7d2c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-24 18:59:49 +0530

    Time: 2 ms (65.32%), Space: 44.4 MB (82.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -6,30 +6,31 @@ class Solution {
             nums2=t;
         }
         
-        int n1=nums1.length,n2=nums2.length;
-        int left=0,right=n1;
+        int n1 = nums1.length,n2=nums2.length;
+        int left =0,right=n1;
         
         while(left<=right){
-            int cut1=left+(right-left)/2;
-            int cut2 = ((n1+n2)/2)-cut1;
+            int cut1 = left+(right-left)/2;
+            int cut2 = (n1+n2+1)/2-cut1;
             
-            int l1 = (cut1==0)?Integer.MIN_VALUE:nums1[cut1-1];
-            int r1 = (cut1==n1)?Integer.MAX_VALUE:nums1[cut1];
-          
-            int l2 = (cut2==0)?Integer.MIN_VALUE:nums2[cut2-1];
-            int r2 = (cut2==n2)?Integer.MAX_VALUE:nums2[cut2];
-          
+            int l1 = cut1==0?Integer.MIN_VALUE:nums1[cut1-1];
+            int r1 = cut1==n1?Integer.MAX_VALUE:nums1[cut1];
+            
+            int l2 = cut2==0?Integer.MIN_VALUE:nums2[cut2-1];
+            int r2 = cut2==n2?Integer.MAX_VALUE:nums2[cut2];
+            
             if(l1<=r2&&l2<=r1){
                 if((n1+n2)%2==0)
                     return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
                 else
-                    return Math.min(r1,r2);
+                    return Math.max(l1,l2);
             }else if(l1>r2){
-                right=cut1-1;
+                right =cut1-1;
             }else
-                left=cut1+1;
+                left =cut1+1;
+            
         }
         
-        return 0;
+        return 0.0;
     }
 }
