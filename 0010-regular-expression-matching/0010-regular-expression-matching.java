876aec40540cfb6c707f95437454e4ff2931295c
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-24 20:06:47 +0530

    Time: 1 ms (100.00%), Space: 40.8 MB (83.93%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,21 +1,32 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-          return isMatch(0,0,s,p,new Boolean[s.length()+1][p.length()+1]);
+        return isMatch(0,0,s,p,new Boolean[s.length()+1][p.length()+1]);
     }
     
-    boolean isMatch(int i,int j,String s,String p,Boolean[][] cache){
+    private boolean isMatch(int i,int j,String s,String p,Boolean[][] dp){
         if(j==p.length())
             return i==s.length();
+        if(i==s.length())
+        {
+            if(j+1<p.length()&&p.charAt(j+1)=='*'){
+                return isMatch(i,j+2,s,p,dp);
+            }else
+                return false;
+        }
         
-        if(cache[i][j]!=null)
-            return cache[i][j];
+        if(dp[i][j]!=null)
+            return dp[i][j];
         
-        boolean firstMatch = i<s.length()&&(p.charAt(j)==s.charAt(i)||p.charAt(j)=='.');
-      
-        if(j+1<p.length()&&p.charAt(j+1)=='*')
-            return cache[i][j]=isMatch(i,j+2,s,p,cache)||firstMatch&&isMatch(i+1,j,s,p,cache);
-        else
-            return cache[i][j]=firstMatch&&isMatch(i+1,j+1,s,p,cache);
-  
+        
+        boolean firstMatch = j<p.length()&&i<s.length()&&(p.charAt(j)=='.'||s.charAt(i)==p.charAt(j));
+        
+        
+        if(j+1<p.length()&&p.charAt(j+1)=='*'){
+            return dp[i][j]= isMatch(i,j+2,s,p,dp)||firstMatch&&isMatch(i+1,j,s,p,dp);
+        }else
+            return dp[i][j]= firstMatch&&isMatch(i+1,j+1,s,p,dp);
+        
+        
+        
     }
 }
