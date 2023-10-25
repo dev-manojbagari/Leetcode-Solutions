b5198e523ad71c5b092cc65867cefc94834f9395
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 10:30:21 +0530

    Time: 27 ms (41.91%), Space: 41.6 MB (83.04%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,27 +20,22 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        if(node==null)
-            return null;
+        if(node==null) return null;
         Map<Node,Node> map = new HashMap<>();
-        return cloneGraph(node,map);
-        
+      return  cloneGraph(node,map);
     }
     
     private Node cloneGraph(Node node,Map<Node,Node> map){
-        
         if(map.containsKey(node))
             return map.get(node);
         
         Node tempNode = new Node(node.val);
-        
+        tempNode.neighbors = new ArrayList<>();
         map.put(node,tempNode);
-        tempNode.neighbors=new ArrayList<>();
         
-        for(Node x:node.neighbors){
-            tempNode.neighbors.add(cloneGraph(x,map));
+        for(Node neighbor:node.neighbors){
+            tempNode.neighbors.add(cloneGraph(neighbor,map));
         }
-        
         return tempNode;
     }
 }
