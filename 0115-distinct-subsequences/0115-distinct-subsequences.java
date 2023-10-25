b87f78e87f3f7947a52f98f42b58354a3bfd0e1d
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 10:18:52 +0530

    Time: 16 ms (45.80%), Space: 49 MB (17.89%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,23 +1,21 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        return numDistinct(0,0,s,t,new Integer[s.length()+1][t.length()+1]);
+        return f(0,0,s.toCharArray(),t.toCharArray(),new Integer[s.length()][t.length()]);
     }
     
-    int numDistinct(int i,int j,String s,String t,Integer[][] dp){
-        if(j==t.length())
+    int f(int i,int j,char[] s,char[] t,Integer[][] state){
+        if(j==t.length)
             return 1;
-        if(i==s.length())
+        if(i==s.length)
             return 0;
         
-        if(dp[i][j]!=null)
-            return dp[i][j];
-        
-        if(s.charAt(i)==t.charAt(j))
-            return dp[i][j] = numDistinct(i+1,j+1,s,t,dp)+numDistinct(i+1,j,s,t,dp);
+        if(state[i][j]!=null)
+            return state[i][j];
+        int ans = 0;
+        if(s[i]==t[j])
+            ans += f(i+1,j+1,s,t,state)+f(i+1,j,s,t,state);
         else
-            return dp[i][j] = numDistinct(i+1,j,s,t,dp);
-        
+            ans += f(i+1,j,s,t,state);
+        return state[i][j]=ans;
     }
-    
-    
 }
