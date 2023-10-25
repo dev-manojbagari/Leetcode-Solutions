85364a829e1ca2cbfa448da3a1ce248d1e14134f
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 10:00:13 +0530

    Time: 5 ms (36.64%), Space: 63.3 MB (5.20%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        return f(0,0,0,s1,s2,s3,s1.length(),s2.length(),s3.length(),new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
+        return f(0,0,0,s1.toCharArray(),s2.toCharArray(),s3.toCharArray(),s1.length(),s2.length(),s3.length(),new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
     }
     
-    boolean f(int i,int j,int k,String s1,String s2,String s3,int in,int jn,int kn,Boolean[][][] dp){
+    boolean f(int i,int j,int k,char[] s1,char[] s2,char[] s3,int in,int jn,int kn,Boolean[][][] dp){
         if(i==in&&j==jn)
             return k==kn;
         
         if(dp[i][j][k]!=null)
             return dp[i][j][k];
         boolean ans=false;
-        if(i<in&&j<jn&&k<kn&&s1.charAt(i)==s3.charAt(k)&&s2.charAt(j)==s3.charAt(k))
+        if(i<in&&j<jn&&k<kn&&s1[i]==s3[k]&&s2[j]==s3[k])
             ans= f(i+1,j,k+1,s1,s2,s3,in,jn,kn,dp)||f(i,j+1,k+1,s1,s2,s3,in,jn,kn,dp);
-        else if(i<in&&k<kn&&s1.charAt(i)==s3.charAt(k)){
+        else if(i<in&&k<kn&&s1[i]==s3[k]){
                 ans= f(i+1,j,k+1,s1,s2,s3,in,jn,kn,dp);            
-        }else if(j<jn&&k<kn&&s2.charAt(j)==s3.charAt(k)){
+        }else if(j<jn&&k<kn&&s2[j]==s3[k]){
                 ans=f(i,j+1,k+1,s1,s2,s3,in,jn,kn,dp);
         }else
             ans = false;
