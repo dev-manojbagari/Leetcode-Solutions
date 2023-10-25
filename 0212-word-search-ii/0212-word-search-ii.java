38e101dc9ea54b8eb331f7b37fa04b3ecdb5a278
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 15:37:49 +0530

    Time: 150 ms (68.17%), Space: 43.8 MB (82.12%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,77 +1,77 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = new Trie();
-        
-        for(String word:words){
-            trie.add(word);    
-        }
+        int rows=board.length,cols=board[0].length;
         List<String> list = new ArrayList<>();
-        boolean[][] vis = new boolean[board.length][board[0].length];
-        
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                dfs(i,j,board,trie.head,list,vis);                
+        Trie trie = new Trie();
+        for(String word:words)
+            trie.add(word);
+        boolean[][] vis= new boolean[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                dfs(i,j,board,vis,trie.head,list);
             }
         }
-        
         return list;
     }
     
-    void dfs(int i,int j,char[][] board,Trie.Node trieNode,List<String> list,boolean[][] vis){
-        
+      void dfs(int i,int j,char[][] board,boolean[][] vis,Node curr,List<String> list){
         if(i<0||i>=board.length||j<0||j>=board[0].length||vis[i][j])
             return;
         
-        char c = board[i][j];
-        
-        if(trieNode.next[c-'a']==null)
-            return;
+          char c = board[i][j];
+          curr = curr.next[c-'a'];
+          
+          if(curr==null){
+              return;
+          }
+          
+          if(curr.isWord){
+              list.add(curr.word);
+              curr.isWord=false;
+          }
+          
+          
         vis[i][j]=true;
         
-        trieNode = trieNode.next[c-'a'];
-        
-        if(trieNode.isWord){
-            list.add(trieNode.word);
-            trieNode.isWord=false;
-        }
-        
-        dfs(i+1,j,board,trieNode,list,vis);
-        dfs(i,j+1,board,trieNode,list,vis);
-        dfs(i,j-1,board,trieNode,list,vis);
-        dfs(i-1,j,board,trieNode,list,vis);
+          dfs(i+1,j,board,vis,curr,list);
+          dfs(i,j+1,board,vis,curr,list);
+          dfs(i-1,j,board,vis,curr,list);
+          dfs(i,j-1,board,vis,curr,list);
         
         vis[i][j]=false;
+        
     }
-    
+   
+    class Node{
+            char val;
+            Node[] next;
+            boolean isWord;
+            String word;
+            Node(char val){
+                this.val = val;
+                next = new Node[26];
+            }
+        }
     class Trie{
+        
+        
+        
         Node head;
         
         Trie(){
             head = new Node('\0');
         }
         
-        class Node{
-            char val;
-            Node[] next;
-            boolean isWord;
-            String word;
-            Node(char val){
-                this.val =val;
-                next = new Node[26];
-            }
-        }
-        
         void add(String word){
             Node curr = head;
             for(char c:word.toCharArray()){
-                if(curr.next[c-'a']==null){
+                if(curr.next[c-'a']==null)
                     curr.next[c-'a']=new Node(c);
-                }
-                curr =curr.next[c-'a'];
+                curr=curr.next[c-'a'];
             }
             curr.isWord=true;
             curr.word=word;
         }
-        
     }
+    
 }
