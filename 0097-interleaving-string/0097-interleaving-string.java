e21e6fb5df556ef7758412b487021b8eb83a8c70
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 09:58:04 +0530

    Time: 6 ms (28.13%), Space: 63.6 MB (5.20%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        return isInterleave(0,0,0,s1,s2,s3,new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
+        return f(0,0,0,s1,s2,s3,s1.length(),s2.length(),s3.length(),new Boolean[s1.length()+1][s2.length()+1][s3.length()+1]);
     }
     
-    boolean isInterleave(int i,int j,int k,String s1,String s2,String s3,Boolean[][][] dp){
-        if(k==s3.length())
-            return i==s1.length()&&j==s2.length();
+    boolean f(int i,int j,int k,String s1,String s2,String s3,int in,int jn,int kn,Boolean[][][] dp){
+        if(i==in&&j==jn)
+            return k==kn;
         
         if(dp[i][j][k]!=null)
             return dp[i][j][k];
-        boolean ans = false;
-        if(i<s1.length()&&j<s2.length()&&s1.charAt(i)==s3.charAt(k)&&s2.charAt(j)==s3.charAt(k))
-            ans= isInterleave(i+1,j,k+1,s1,s2,s3,dp)||isInterleave(i,j+1,k+1,s1,s2,s3,dp);
-        else if(i<s1.length()&&s1.charAt(i)==s3.charAt(k)){
-            ans= isInterleave(i+1,j,k+1,s1,s2,s3,dp);
-        }else if(j<s2.length()&&s2.charAt(j)==s3.charAt(k)){
-            ans= isInterleave(i,j+1,k+1,s1,s2,s3,dp);
+        boolean ans=false;
+        if(i<in&&j<jn&&k<kn&&s1.charAt(i)==s3.charAt(k)&&s2.charAt(j)==s3.charAt(k))
+            ans= f(i+1,j,k+1,s1,s2,s3,in,jn,kn,dp)||f(i,j+1,k+1,s1,s2,s3,in,jn,kn,dp);
+        else if(i<in&&k<kn&&s1.charAt(i)==s3.charAt(k)){
+                ans= f(i+1,j,k+1,s1,s2,s3,in,jn,kn,dp);            
+        }else if(j<jn&&k<kn&&s2.charAt(j)==s3.charAt(k)){
+                ans=f(i,j+1,k+1,s1,s2,s3,in,jn,kn,dp);
         }else
-            ans= false;
+            ans = false;
         
         return dp[i][j][k]=ans;
     }
-    
 }
