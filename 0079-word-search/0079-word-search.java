a42f49502fa1d638377aca006cffe35e050acd1f
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 15:24:58 +0530

    Time: 152 ms (49.31%), Space: 40.2 MB (86.36%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,35 +1,29 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         boolean[][] vis = new boolean[board.length][board[0].length];
-        
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                 if(board[i][j]==word.charAt(0)){
-                     if(dfs(i,j,board,vis,0,word)){
-                         return true;
-                     }
-                 }        
+                if(dfs(0,i,j,board,word,vis))
+                    return true;
             }
         }
-        
         return false;
     }
     
-    
-    boolean dfs(int i,int j,char[][] board,boolean[][] vis,int index,String word){
+    boolean dfs(int index,int i,int j,char[][] board,String word,boolean[][] vis){
         if(index==word.length())
             return true;
         
-        if(i<0||i>=board.length||j<0||j>=board[0].length||vis[i][j]||word.charAt(index)!=board[i][j])
+        if(i<0||i>=board.length||j<0||j>=board[0].length||vis[i][j]||board[i][j]!=word.charAt(index))
             return false;
         
         vis[i][j]=true;
         
-        if(dfs(i+1,j,board,vis,index+1,word)||
-        dfs(i,j+1,board,vis,index+1,word)||
-        dfs(i-1,j,board,vis,index+1,word)||
-        dfs(i,j-1,board,vis,index+1,word))
-            return true;
+       if(dfs(index+1,i+1,j,board,word,vis)||
+          dfs(index+1,i,j+1,board,word,vis)||
+          dfs(index+1,i-1,j,board,word,vis)||
+          dfs(index+1,i,j-1,board,word,vis))
+             return true;
         
         vis[i][j]=false;
         
