bf46944edf88d3e318c0327bf45c37cb443a2978
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 12:33:46 +0530

    Time: 3 ms (98.60%), Space: 41.6 MB (92.39%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,33 +1,42 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder res = new StringBuilder();
-        int n=s.length();
-        int start=0,end=n-1;
-        while(start<=end&&s.charAt(start)==' ')
-            start++;
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int i=0,j=n-1;
         
-        if(start==n)
-            return "";
+        while(i<n&&s.charAt(i)==' ')
+            i++;
         
-        while(end>=start&&s.charAt(end)==' ')
-            end--;
+        while(j>=0&&s.charAt(j)==' ')
+            j--;
         
-        for(int i=end;i>=start;i--){
-            if(s.charAt(i)==' ')
-                continue;
-            int j=i;
-            while(j>=start&&s.charAt(j)!=' ')
-                j--;
+        for(int k=j;k>=i;k--){
+            while(k>=i&&s.charAt(k)==' ')
+                k--;
+            if(k==i-1)
+                break;
             
-            for(int k=j+1;k<=i;k++)
-                res.append(s.charAt(k));
+            int lastChar = k;
+            while(k>=i&&s.charAt(k)!=' ')
+                k--;
+            int first = k+1;
             
-            if(j>=start)
-                res.append(" ");
+            for(int l=first;l<=lastChar;l++){
+                sb.append(s.charAt(l));
+            }
+            if(first-1<i)
+                break;
+            sb.append(" ");
             
-            i=j;
+            k = first;
+            
         }
+            
         
-        return res.toString();
+        
+        
+        
+        
+        return sb.toString();
     }
 }
