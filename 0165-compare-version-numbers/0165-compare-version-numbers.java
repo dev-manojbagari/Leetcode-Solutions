e4c755b8b9978968fe20ef179467efdcc5fe7f1a
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-25 13:41:00 +0530

    Time: 1 ms (81.41%), Space: 40.5 MB (53.84%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -2,17 +2,21 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
+        
+        
         int i=0,j=0;
+        
         while(i<v1.length||j<v2.length){
-            int num1 = i<v1.length?Integer.parseInt(v1[i]):0;
-            int num2 = j<v2.length?Integer.parseInt(v2[j]):0;
             
-            if(num1>num2)
-                return 1;
-            else if(num1<num2)
-                return -1;
-            i++;
-            j++;
+            int num1 = i>=v1.length?0:Integer.parseInt(v1[i]);
+            int num2 = j>=v2.length?0:Integer.parseInt(v2[j]);
+            
+            if(num1==num2){
+                i++;
+                j++;
+            }else{
+                return Integer.compare(num1,num2);
+            }
         }
         
         return 0;
