8ab8234b7bbb20e436119c6d8995b4c259fee91e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-26 14:38:29 +0530

    Time: 9 ms (87.39%), Space: 43.9 MB (64.94%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -3,43 +3,40 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         if(p.length()>s.length())
             return res;
-        int[] cmap = new int[26];
+        int[] charCount = new int[26];
         
         for(char c:p.toCharArray()){
-            cmap[c-'a']++;
-        }
-        int start = 0,end=0;
-        int count =0;
-        for(;end<p.length();end++){
-            char c = s.charAt(end);
-            if(cmap[c-'a']>0){
-                count++;
-            }
-                cmap[c-'a']--;
+            charCount[c-'a']++;    
         }
         
-        if(count==p.length()){
-            res.add(start);
+        int start=0,end=0;
+        int count = p.length();
+        for(;end<p.length();end++){
+            char c = s.charAt(end);
+            charCount[c-'a']--;
+            if(charCount[c-'a']>=0)
+                count--;
         }
-            
+        
+        if(count==0)
+            res.add(0);
+        
         for(;end<s.length();end++){
             char c = s.charAt(start++);
-            cmap[c-'a']++;
-            if(cmap[c-'a']>0){
-                count--;
-            }
-            
-            c = s.charAt(end);
-            cmap[c-'a']--;
-            if(cmap[c-'a']>=0){
+            charCount[c-'a']++;
+            if(charCount[c-'a']>0){
                 count++;
             }
-            
-            if(count==p.length()){
+            c=s.charAt(end);
+            charCount[c-'a']--;
+            if(charCount[c-'a']>=0)
+                count--;
+            if(count==0)
                 res.add(start);
-            }
-        }    
-                
-        return res;
+        }
+        
+        
+        
+        return res;        
     }
 }
