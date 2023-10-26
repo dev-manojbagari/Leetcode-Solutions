598e13fe4f93a9d8a83057da4b5091a09e074515
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-26 07:20:04 +0530

    Time: 5 ms (90.10%), Space: 43.9 MB (62.86%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -6,28 +6,28 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         
         for(char c:s.toCharArray()){
-            if('0'<=c&&c<='9'){
-                num = num*10+c-'0';
+            if(c=='('){
+                stack.push(result);
+                stack.push(sign);
+                sign=1;
+                result=0;
             }else if(c=='+'){
                 result += sign*num;
-                sign=1;
+                sign = 1;
                 num=0;
             }else if(c=='-'){
                 result += sign*num;
-                sign=-1;
+                sign = -1;
                 num=0;
-            }else if(c=='('){
-                stack.push(result);
-                stack.push(sign);
-                result=0;
-                sign=1;
             }else if(c==')'){
-                result +=sign*num;
+                 result +=sign*num;
                 num=0;
                 int prevSign = stack.pop();
                 int prevResult = stack.pop();
                 result = prevSign*result;
-                result += prevResult;
+                result += prevResult;;
+            }else if('0'<=c&&c<='9'){
+                num=num*10+c-'0';
             }
         }
         
