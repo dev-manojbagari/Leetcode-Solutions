621324beb9b633f158cda2c5ea6e4887cc055a28
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-26 15:15:09 +0530

    Time: 49 ms (54.49%), Space: 54.1 MB (36.22%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,61 +1,63 @@
 class Solution {
     int count=0;
     public int reversePairs(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
+        mergeSort(0,nums.length-1,nums);
         return count;
     }
     
-    void mergeSort(int[] nums,int start,int end){
+    void mergeSort(int start,int end,int[] nums){
         if(start>=end)
-            return;
+            return ;
+        
         int mid = start+(end-start)/2;
         
-        mergeSort(nums,start,mid);
-        mergeSort(nums,mid+1,end);
+        mergeSort(start,mid,nums);
+        mergeSort(mid+1,end,nums);
         
-        int i=start,j=mid+1;
+        int i= start,j=mid+1;
         
         while(i<=mid){
-            while(j<=end&&(nums[i]/2.0)>nums[j]){
+            while(j <= end && nums[i]/2.0 > nums[j]){
                 j++;
             }
             i++;
-            count += j-(mid+1);
+            count += j-mid-1;
         }
         
-                
         
-        sortedMerge(nums,start,mid,end);
+        sortedMerge(start,mid,end,nums);
     }
-
-    void sortedMerge(int[] nums,int start,int mid,int end){
-          int[] L = new int[mid-start+1]; 
-          int[] R = new int[end-mid];
+    
+    void sortedMerge(int start,int mid,int end ,int[] nums){
         
-          for(int i=0;i<L.length;i++){
-              L[i] = nums[start+i];
-          }
+        int[] L  = new int[mid-start+1];
+        int[] R = new int[end-mid];
         
-          for(int i=0;i<R.length;i++){
-              R[i] = nums[mid+1+i];
-          }
+        for(int i=0;i<L.length;i++){
+            L[i] = nums[start+i];
+        }
         
-         int i=0,j=0,k=start;
+        for(int i=0;i<R.length;i++){
+            R[i] = nums[mid+1+i];
+        }
+        
+        int i=0,j=0,k=start;
         
         while(i<L.length&&j<R.length){
             if(L[i]<=R[j]){
-                nums[k++] = L[i++];
-            }else{
-                nums[k++] = R[j++];
-            }
+                nums[k++]=L[i++];
+            }else
+                nums[k++]=R[j++];
         }
         
         while(i<L.length){
-            nums[k++] = L[i++];
+                nums[k++]=L[i++];
         }
-        
+ 
         while(j<R.length){
-            nums[k++] = R[j++];
+                nums[k++]=R[j++];
         }
-    }    
+    }
+    
+    
 }
