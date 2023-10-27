9725c02ae7d13a1dc5cce9d7217e2f9460bf37fe
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-27 05:49:01 +0530

    Time: 2 ms (83.13%), Space: 40.6 MB (26.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+        
         for(char c:s.toCharArray()){
-            if(c=='('){
-                stack.push(')');
+            if(c=='['){
+                stack.push(']');
             }else if(c=='{'){
                 stack.push('}');
-            }else if(c=='['){
-                stack.push(']');
+            }else if(c=='('){
+                stack.push(')');
             }else{
                 if(stack.isEmpty()||stack.peek()!=c)
                     return false;
@@ -15,6 +16,6 @@ class Solution {
             }
         }
         
-        return stack.isEmpty();
+        return stack.isEmpty(); 
     }
 }
