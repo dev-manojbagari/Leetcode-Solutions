412a53decc5f44496813f79b9b44398dc1695844
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-27 17:22:44 +0530

    Time: 63 ms (19.40%), Space: 44.4 MB (82.09%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -1,40 +1,41 @@
 class Solution {
-
     public List<String> maxNumOfSubstrings(String s) {
         List<String> res = new ArrayList<>();
-        Map<Character, int[]> map = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!map.containsKey(c)) map.put(c, new int[] { i, i }); else map.get(s.charAt(i))[1] = i;
+        
+        Map<Character,int[]> map = new HashMap<>();
+        
+        for(int i=0;i<s.length();i++){
+            if(!map.containsKey(s.charAt(i)))
+                map.put(s.charAt(i),new int[]{i,i});
+            else
+                map.get(s.charAt(i))[1]=i;
         }
-
-        int prevEnd = -1;
-        for (int i = 0; i < s.length(); i++) {
+        
+        int prevEnd= -1;
+        for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             int start = map.get(c)[0];
-            if (start == i) {
-                int curEnd = getEnd(s, start, map.get(c)[1], map);
-                if (curEnd != -1) {
-                    if (curEnd <= prevEnd) {
-                        res.set(res.size() - 1, s.substring(start, curEnd + 1));
-                    } else {
-                        res.add(s.substring(start, curEnd + 1));
-                    }
-                    prevEnd = curEnd;
+            int end = map.get(c)[1];
+            if(start==i){
+                int curEnd = getEnd(start,end,s,map);
+                if(curEnd!=-1){
+                    if(curEnd<=prevEnd){
+                        res.set(res.size()-1,s.substring(start,curEnd+1));
+                    }else
+                        res.add(s.substring(start,curEnd+1));
+                    prevEnd= curEnd;
                 }
             }
         }
-
+        
         return res;
     }
-
-    private int getEnd(String s, int start, int end, Map<Character, int[]> map) {
-        for (int i = start; i < end; i++) {
-            char c = s.charAt(i);
-            if (map.get(c)[0] < start) return -1;
-
-            end = Math.max(end, map.get(c)[1]);
+    int getEnd(int start,int end ,String str,Map<Character,int[]> map){
+        for(int i=start;i<=end;i++){
+            if(map.get(str.charAt(i))[0]<start){
+                return -1;
+            }
+            end = Math.max(end,map.get(str.charAt(i))[1]);
         }
         return end;
     }
