d81e613ce252cca82911d33940af74fee93866d2
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-27 08:31:20 +0530

    Time: 38 ms (91.73%), Space: 68.9 MB (72.79%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -2,44 +2,47 @@ class Solution {
     public int numBusesToDestination(int[][] routes, int source, int target) {
         if(source==target)
             return 0;
-        Map<Integer,List<Integer>> map = new HashMap<>();
+        Map<Integer,List<Integer>> stopMap = new HashMap<>();
         
-        for(int bus=0;bus<routes.length;bus++){
-            for(int j=0;j<routes[bus].length;j++){
-                int curStop = routes[bus][j];
-                map.computeIfAbsent(curStop,(k->new ArrayList<>())).add(bus);    
+        for(int i=0;i<routes.length;i++){
+            for(int j=0;j<routes[i].length;j++){
+                if(!stopMap.containsKey(routes[i][j]))
+                    stopMap.put(routes[i][j],new ArrayList<>());
+                stopMap.get(routes[i][j]).add(i);
             }
         }
         
-        if(!map.containsKey(source)||!map.containsKey(target))
+        if(!stopMap.containsKey(source)||!stopMap.containsKey(target))
             return -1;
         
-        Queue<Integer> q  = new LinkedList<>();
-        Set<Integer> busTaken = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
         Set<Integer> stopTaken = new HashSet<>();
+        Set<Integer> busTaken = new HashSet<>();
         q.offer(source);
         stopTaken.add(source);
-        int buses =0;
-        
+        int busCount = 0;
         while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
+            int levelSize = q.size();
+            for(int i=0;i<levelSize;i++){
                 int curStop = q.poll();
-                for(int bus:map.get(curStop)){
-                    if(busTaken.contains(bus)) continue;
-                    busTaken.add(bus);
-                    for(int stop:routes[bus]){
-                        if(stop==target)
-                            return buses+1;
-                        if(stopTaken.contains(stop)) continue;
-                        stopTaken.add(stop);
-                        q.offer(stop);
+                for(int bus:stopMap.get(curStop)){
+                    if(busTaken.contains(bus)){
+                        continue;
                     }
-                }                
+                    busTaken.add(bus);
+                    for(int nextStop:routes[bus]){
+                        if(stopTaken.contains(nextStop)){
+                            continue;
+                        }
+                        if(nextStop==target)
+                            return busCount+1;
+                        stopTaken.add(nextStop);
+                        q.offer(nextStop);
+                    }
+                }
             }
-            buses++;
+            busCount++;
         }
-        
         return -1;
     }
 }
