43ace5f3e02642126fe90facf331f859fc77dfa7
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-27 16:33:15 +0530

    Time: 114 ms (22.13%), Space: 100.8 MB (26.46%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -1,32 +1,34 @@
 class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
-         int[] q = queries.clone();
-         Arrays.sort(q);
-         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-         TreeMap<Integer,Integer> treeMap = new TreeMap<>();
-         Map<Integer,Integer> map = new HashMap<>();
-        int index=0;
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int[] q = queries.clone();
+        Arrays.sort(q);
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        TreeMap<Integer,Integer> treeMap = new TreeMap<>();
+        int index=0,n=intervals.length;
         
         for(int i=0;i<q.length;i++){
-             int query = q[i];
-             while(index<intervals.length&&intervals[index][0]<=query){
-                 treeMap.put(intervals[index][1]-intervals[index][0]+1,intervals[index][1]);
-                 index++;
-             }
             
-             while(!treeMap.isEmpty()&&treeMap.firstEntry().getValue()<query)
-                    treeMap.pollFirstEntry();
-             
-             map.put(query,treeMap.isEmpty()?-1:treeMap.firstKey());
-             
-         }
-        
-         int[] res =new int[q.length];
-        
-        for(int i=0;i<queries.length;i++){
-            res[i]=map.get(queries[i]);
+            while(index<n&&intervals[index][0]<=q[i]){
+                int start = intervals[index][0],end=intervals[index][1];
+                treeMap.put(end-start+1,end);
+                index++;
+            }
+            
+            while(!treeMap.isEmpty()&&treeMap.firstEntry().getValue()<q[i]){
+                treeMap.pollFirstEntry();
+            }
+            
+            map.put(q[i],treeMap.isEmpty()?-1:treeMap.firstKey());
         }
-            
+        
+        int[] res = new int[q.length];
+        int i=0;
+        for(int num:queries)
+            res[i++]=map.get(num);
+        
         return res;
+        
     }
 }
