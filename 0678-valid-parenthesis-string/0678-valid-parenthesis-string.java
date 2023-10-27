d30cbd8a75edb7dd1a8338e810b08f9de890a3bf
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-27 06:03:47 +0530

    Time: 1 ms (41.52%), Space: 40.6 MB (27.19%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Stack<Integer> charIdStack = new Stack<>();
+        Stack<Integer> openIdStack = new Stack<>();
         Stack<Integer> starIdStack = new Stack<>();
         
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             if(c=='('){
-                charIdStack.push(i);
+                openIdStack.push(i);
             }else if(c=='*'){
                 starIdStack.push(i);
             }else{
-                if(charIdStack.isEmpty()&&starIdStack.isEmpty())
+                if(openIdStack.isEmpty()&&starIdStack.isEmpty())
                     return false;
-                if(!charIdStack.isEmpty())
-                    charIdStack.pop();
+                if(!openIdStack.isEmpty())
+                    openIdStack.pop();
                 else
                     starIdStack.pop();
             }
         }
         
-        while(!charIdStack.isEmpty()&&!starIdStack.isEmpty()){
-            if(charIdStack.pop()>starIdStack.pop())
+        while(!openIdStack.isEmpty()&&!starIdStack.isEmpty()){
+            if(openIdStack.pop()>starIdStack.pop())
                 return false;
         }
         
         
-        return charIdStack.isEmpty();
+        return openIdStack.isEmpty();
     }
 }
