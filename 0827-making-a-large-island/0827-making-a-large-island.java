988de78ab7f9639a14e0c37e734d5369150f2a10
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-27 09:28:29 +0530

    Time: 116 ms (50.28%), Space: 87 MB (7.17%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -38,45 +38,48 @@ class Solution {
     }
     
     public int largestIsland(int[][] grid) {
-        int rows = grid.length, cols = grid[0].length;
-        DSU dsu = new DSU(rows * cols);
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        for(int curRow = 0; curRow < rows; curRow++) {
-            for(int curCol = 0; curCol < cols; curCol++) {
-                if(grid[curRow][curCol] == 1) {
-                    for(int[] dir : dirs) {
-                        int x = dir[0] + curRow;
-                        int y = dir[1] + curCol;
+        int rows=grid.length,cols=grid[0].length;
+        DSU dsu = new DSU(rows*cols);
+        int[][] dirs= {{1,0},{-1,0},{0,1},{0,-1}};
+        for(int curRow=0;curRow<rows;curRow++){
+            for(int curCol=0;curCol<cols;curCol++){
+                if(grid[curRow][curCol]==1){
+                    for(int[] dir:dirs){
+                        int x = dir[0]+curRow;
+                        int y = dir[1]+curCol;
                         
-                        if(x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == 0)
+                        if(x<0||x>=rows||y<0||y>=cols||grid[x][y]==0)
                             continue;
-                        dsu.unionBySize(x * cols + y, curRow * cols + curCol);
+                        dsu.unionBySize(x*cols+y,curRow*cols+curCol);
                     }
                 }
             }
         }
-        int max = Arrays.stream(dsu.size).max().getAsInt();
+       int max=Arrays.stream(dsu.size).max().getAsInt();
         Set<Integer> set = new HashSet<>();
-        for(int curRow = 0; curRow < rows; curRow++) {
-            for(int curCol = 0; curCol < cols; curCol++) {
-                if(grid[curRow][curCol] == 0) {
-                    for(int[] dir : dirs) {
-                        int x = dir[0] + curRow;
-                        int y = dir[1] + curCol;
+        for(int curRow=0;curRow<rows;curRow++){
+            for(int curCol=0;curCol<cols;curCol++){
+                if(grid[curRow][curCol]==0){
+                    for(int[] dir:dirs){
+                        int x = dir[0]+curRow;
+                        int y = dir[1]+curCol;
                         
-                        if(x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == 0)
+                        if(x<0||x>=rows||y<0||y>=cols||grid[x][y]==0)
                             continue;
-                        if(dsu.findUP(x * cols + y) != dsu.findUP(curRow * cols + curCol))
-                            set.add(dsu.findUP(x * cols + y));
+                        if(dsu.findUP(x*cols+y)!=dsu.findUP(curRow*cols+curCol))
+                            set.add(dsu.findUP(x*cols+y));
                     }
                     int total = 1;
-                    for(int i : set)
+                    for(int i:set)
                         total += dsu.size[i];
-                    max = Math.max(max, total);
+                    max= Math.max(max,total);
                     set.clear();
                 }
+                
             }
         }
+        
+        
         return max;
     }
 }
