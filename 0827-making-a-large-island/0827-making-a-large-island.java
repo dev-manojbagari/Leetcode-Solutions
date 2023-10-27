291af438d5d34ed85bf4afc9ea4492ed1e1fd535
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-27 09:23:06 +0530

    Time: 110 ms (53.96%), Space: 85.6 MB (12.29%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -1,87 +1,82 @@
 class Solution {
-            class DSU{
+    
+    class DSU{
         int[] parent;
         int[] size;
-        
         DSU(int n){
             parent = new int[n];
-            size = new int[n];
-
-            for(int i=0;i<n;i++)
-            {
+            size= new int[n];
+            for(int i=0;i<n;i++){
                 parent[i]=i;
                 size[i]=1;
             }
+            
         }
         
-        int findUP(int x){
-            if(parent[x]==x)
-                return x;
-            int up = findUP(parent[x]);
-            parent[x]=up;
+        int findUP(int a){
+            if(parent[a]==a)
+                return a;
+            int up = findUP(parent[a]);
+            parent[a]=up;
             return up;
         }
-        
+    
         void unionBySize(int a,int b){
             int upA = findUP(a);
             int upB = findUP(b);
+            
             if(upA==upB)
                 return;
-            if(size[upA]>=size[upB]){
-                size[upA]+=size[upB];
-                parent[upB]=upA;
-            }else{
+            if(size[upA]<=size[upB]){
                 size[upB]+=size[upA];
                 parent[upA]=upB;
+            }else{
+                size[upA]+=size[upB];
+                parent[upB]=upA;
             }
-            
         }
     }
+    
     public int largestIsland(int[][] grid) {
-        int rows=grid.length,cols=grid[0].length;
-        DSU dsu = new DSU(rows*cols);
-        int[][] dirs = {{1,0},{-1,0},{0,-1},{0,1}};
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==1){
-                    
-                    for(int[] dir:dirs){
-                        int x = i+dir[0];
-                        int y = j+dir[1];
+        int rows = grid.length, cols = grid[0].length;
+        DSU dsu = new DSU(rows * cols);
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(int curRow = 0; curRow < rows; curRow++) {
+            for(int curCol = 0; curCol < cols; curCol++) {
+                if(grid[curRow][curCol] == 1) {
+                    for(int[] dir : dirs) {
+                        int x = dir[0] + curRow;
+                        int y = dir[1] + curCol;
                         
-                        if(x<0||x>=grid.length||y<0||y>=grid[0].length||grid[x][y]==0)
+                        if(x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == 0)
                             continue;
-                        dsu.unionBySize(i*cols+j,x*cols+y);
+                        dsu.unionBySize(x * cols + y, curRow * cols + curCol);
                     }
                 }
             }
         }
+        int max = Arrays.stream(dsu.size).max().getAsInt();
         Set<Integer> set = new HashSet<>();
-        int max=Arrays.stream(dsu.size).max().getAsInt();
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==0){
-                    
-                    for(int[] dir:dirs){
-                        int x = i+dir[0];
-                        int y = j+dir[1];
+        for(int curRow = 0; curRow < rows; curRow++) {
+            for(int curCol = 0; curCol < cols; curCol++) {
+                if(grid[curRow][curCol] == 0) {
+                    for(int[] dir : dirs) {
+                        int x = dir[0] + curRow;
+                        int y = dir[1] + curCol;
                         
-                        if(x<0||x>=grid.length||y<0||y>=grid[0].length||grid[x][y]==0)
+                        if(x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == 0)
                             continue;
-                        if(dsu.findUP(i*cols+j)!=dsu.findUP(x*cols+y))
-                            set.add(dsu.findUP(x*cols+y));
+                        if(dsu.findUP(x * cols + y) != dsu.findUP(curRow * cols + curCol))
+                            set.add(dsu.findUP(x * cols + y));
                     }
+                    int total = 1;
+                    for(int i : set)
+                        total += dsu.size[i];
+                    max = Math.max(max, total);
+                    set.clear();
                 }
-                
-                int total = 1;
-                for(int up:set){
-                    total += dsu.size[up];
-                }
-                max = Math.max(max,total);
-                set.clear();
             }
         }
         return max;
-        
     }
 }
