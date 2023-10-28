5fb4b6a6b0a1b8f4df4ebde251f254cc1a034f84
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-28 07:51:37 +0530

    Time: 17 ms (10.57%), Space: 44.4 MB (51.33%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,24 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        return f(0,coins,amount,new Integer[coins.length+1][amount+1]);
+        return change(0,amount ,coins,new Integer[coins.length+1][amount+1]);
     }
     
-    int f(int index,int[] coins,int amount,Integer[][] state){
+    int change(int index,int amount ,int[] coins,Integer[][] state){
         if(amount==0)
             return 1;
+
         if(amount<0||index==coins.length)
             return 0;
         
         if(state[index][amount]!=null)
             return state[index][amount];
         
-        int exclude = f(index+1,coins,amount,state);
-        int include=0;
-        if(coins[index]<=amount)
-            include = f(index,coins,amount-coins[index],state);
+        int ans = 0;
+        ans += change(index+1,amount,coins,state);
+        ans +=change(index,amount-coins[index],coins,state);
         
-        return state[index][amount]= include+exclude;
+        return state[index][amount]=ans;
     }
+    
     
 }
