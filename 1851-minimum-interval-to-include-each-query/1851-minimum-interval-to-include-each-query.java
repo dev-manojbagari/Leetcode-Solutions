0324011f53ef55b67eac84195cdfa0fdb8a774cf
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-28 14:53:27 +0530

    Time: 117 ms (18.60%), Space: 100.3 MB (31.51%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -1,34 +1,29 @@
 class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
+        TreeMap<Integer,Integer> treeMap = new TreeMap<>();
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         int[] q = queries.clone();
         Arrays.sort(q);
         Map<Integer,Integer> map = new HashMap<>();
-        
-        TreeMap<Integer,Integer> treeMap = new TreeMap<>();
         int index=0,n=intervals.length;
-        
-        for(int i=0;i<q.length;i++){
+        for(int query:q){
             
-            while(index<n&&intervals[index][0]<=q[i]){
-                int start = intervals[index][0],end=intervals[index][1];
+            while(index<n&&intervals[index][0]<=query){
+                int start=intervals[index][0],end=intervals[index][1];
                 treeMap.put(end-start+1,end);
                 index++;
             }
             
-            while(!treeMap.isEmpty()&&treeMap.firstEntry().getValue()<q[i]){
+            while(!treeMap.isEmpty()&&treeMap.firstEntry().getValue()<query){
                 treeMap.pollFirstEntry();
             }
-            
-            map.put(q[i],treeMap.isEmpty()?-1:treeMap.firstKey());
+
+            map.put(query,treeMap.isEmpty()?-1:treeMap.firstKey());
         }
-        
         int[] res = new int[q.length];
-        int i=0;
-        for(int num:queries)
-            res[i++]=map.get(num);
+        for(int i=0;i<queries.length;i++)
+            res[i] = map.get(queries[i]);
         
         return res;
-        
     }
 }
