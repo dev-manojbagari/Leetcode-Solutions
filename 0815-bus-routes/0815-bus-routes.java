474a9732e3d201cf78818ce94dc8efae03237af2
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-28 12:24:54 +0530

    Time: 46 ms (83.75%), Space: 69.6 MB (71.46%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -6,9 +6,12 @@ class Solution {
         
         for(int i=0;i<routes.length;i++){
             for(int j=0;j<routes[i].length;j++){
-                if(!stopMap.containsKey(routes[i][j]))
-                    stopMap.put(routes[i][j],new ArrayList<>());
-                stopMap.get(routes[i][j]).add(i);
+                int curBus = i;
+                int curStop = routes[i][j];
+                if(!stopMap.containsKey(curStop))
+                    stopMap.put(curStop,new ArrayList<>());
+                
+                    stopMap.get(curStop).add(curBus);
             }
         }
         
@@ -16,33 +19,37 @@ class Solution {
             return -1;
         
         Queue<Integer> q = new LinkedList<>();
-        Set<Integer> stopTaken = new HashSet<>();
         Set<Integer> busTaken = new HashSet<>();
+        Set<Integer> stopTaken = new HashSet<>();
+        
         q.offer(source);
         stopTaken.add(source);
-        int busCount = 0;
+        int busCount=0;
+        
         while(!q.isEmpty()){
-            int levelSize = q.size();
-            for(int i=0;i<levelSize;i++){
+            int size=q.size();
+            for(int i=0;i<size;i++){
                 int curStop = q.poll();
+            
                 for(int bus:stopMap.get(curStop)){
-                    if(busTaken.contains(bus)){
-                        continue;
-                    }
-                    busTaken.add(bus);
-                    for(int nextStop:routes[bus]){
-                        if(stopTaken.contains(nextStop)){
+                   if(busTaken.contains(bus))
+                      continue;
+                   busTaken.add(bus);
+                
+                   for(int nextStop :routes[bus]){
+                        if(stopTaken.contains(nextStop))
                             continue;
-                        }
                         if(nextStop==target)
                             return busCount+1;
-                        stopTaken.add(nextStop);
-                        q.offer(nextStop);
-                    }
-                }
-            }
-            busCount++;
+                       stopTaken.add(nextStop);
+                       q.offer(nextStop);
+                  }
+             }
         }
+        busCount++;
+    }
+        
+        
         return -1;
     }
 }
