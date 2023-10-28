cd89caba0f879dcb5b4025db226617b5597679d2
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-28 14:29:56 +0530

    Time: 11 ms (84.76%), Space: 44.4 MB (13.77%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,63 +1,67 @@
 class Solution {
     class DSU{
-    int[] parent;
-    int[] size;
-    DSU(int n){
-        parent = new int[n];
-        size= new int[n];
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    
-    int findUP(int a){
-        if(parent[a]==a)
-            return a;
-        int up = findUP(parent[a]);
-        parent[a]=up;
-        return up;
-    }
+        int[] parent;
+        int[] size;
+        
+        DSU(int n){
+            parent = new int[n];
+            size = new int[n];
 
-    void unionBySize(int a,int b){
-        int upA = findUP(a);
-        int upB = findUP(b);
-        
-        if(upA==upB)
-            return;
-        if(size[upA]<=size[upB]){
-            size[upB]+=size[upA];
-            parent[upA]=upB;
-        }else{
-            size[upA]+=size[upB];
-            parent[upB]=upA;
-        }
-    }
-}
-    public int removeStones(int[][] stones) {
-             int m=Integer.MIN_VALUE;
-        int n = Integer.MIN_VALUE;
-        
-        for(int i=0;i<stones.length;i++){
-            m = Math.max(m,stones[i][0]);
-            n=  Math.max(n,stones[i][1]);
-        }
-        
-        DSU dsu = new DSU(m+n+2);
-        Set<Integer> set = new HashSet<>();
-        for(int i=0;i<stones.length;i++){
-            for(int j=0;j<stones[0].length;j++){
-                int row = stones[i][0];
-                int col = stones[i][1]+m+1;
-                dsu.unionBySize(row,col);
-                set.add(row);
-                set.add(col);
+            for(int i=0;i<n;i++)
+            {
+                parent[i]=i;
+                size[i]=1;
             }
         }
         
-        int c=0;
-        for(int i:set)
-            if(dsu.findUP(i)==i)
+        int findUP(int x){
+            if(parent[x]==x)
+                return x;
+            int up = findUP(parent[x]);
+            parent[x]=up;
+            return up;
+        }
+        
+        void unionBySize(int a,int b){
+            int upA = findUP(a);
+            int upB = findUP(b);
+            if(upA==upB)
+                return;
+            if(size[upA]>=size[upB]){
+                size[upA]+=size[upB];
+                parent[upB]=upA;
+            }else{
+                size[upB]+=size[upA];
+                parent[upA]=upB;
+            }
+            
+        }
+    }
+    
+    public int removeStones(int[][] stones) {
+        int maxRowNo = Integer.MIN_VALUE;
+        int maxColNo = Integer.MIN_VALUE;
+        
+        for(int i=0;i<stones.length;i++){
+            int row= stones[i][0];
+            int col = stones[i][1];
+            maxRowNo = Math.max(maxRowNo,row);
+            maxColNo = Math.max(maxColNo,col);
+        }
+        
+        DSU dsu = new DSU(maxRowNo+maxColNo+2);
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<stones.length;i++){
+            int row = stones[i][0];
+            int col = stones[i][1]+maxRowNo+1;
+            dsu.unionBySize(row,col);
+            set.add(row);
+            set.add(col);
+        }
+        
+        int c =0;
+        for(int x:set)
+            if(dsu.findUP(x)==x)
                 c++;
         
         
