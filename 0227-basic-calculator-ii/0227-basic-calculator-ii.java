95253f9bcbb6caa406e6a29ea8444e8f198a2e27
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-28 07:21:50 +0530

    Time: 23 ms (42.62%), Space: 45.2 MB (43.36%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,38 +1,34 @@
 class Solution {
     public int calculate(String s) {
-        char curSign = '+';
-        int curNum=0;
+        char prevSign = '+';
         Stack<Integer> stack = new Stack<>();
+        int num=0;
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            if(isDigit(c)){
-                curNum = curNum*10+c-'0';
+            
+            if(Character.isDigit(c)){
+                num = num*10+c-'0';
             }
             
-            if(!isDigit(c)&&c!=' '||i==s.length()-1){
-                if(curSign=='+'){
-                    stack.push(curNum);
-                }else if(curSign=='-'){
-                    stack.push(-curNum);
-                }else if(curSign=='*'){
-                    stack.push(stack.pop()*curNum);
-                }else{
-                    stack.push(stack.pop()/curNum);
+            if((!Character.isDigit(c)&&c!=' ')||i==s.length()-1){
+                if(prevSign=='+'){
+                    stack.push(num);
+                }else if(prevSign=='-'){
+                    stack.push(-num);
+                }else if(prevSign=='*'){
+                    stack.push(stack.pop()*num);
+                }else if(prevSign=='/'){
+                    stack.push(stack.pop()/num);
                 }
-                curSign = c;
-                curNum=0;
+                prevSign=c;
+                num=0;
             }
         }
         
-        int total=0;
-        while(!stack.isEmpty()){
-            total += stack.pop();
-        }
+        int total = 0;
+        while(!stack.isEmpty())
+            total+= stack.pop();
         
         return total;
-    }
-    
-    boolean isDigit(char c){
-        return '0'<=c&&c<='9';
     }
 }
