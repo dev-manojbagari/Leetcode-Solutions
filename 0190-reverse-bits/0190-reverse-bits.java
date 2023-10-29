c4c86621cc6e02df39bfd6e7e7ceb99210426506
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-29 12:12:46 +0530

    Time: 1 ms (32.75%), Space: 40.5 MB (72.50%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,10 +3,11 @@ public class Solution {
     public int reverseBits(int n) {
         int rev=0;
         for(int i=0;i<32;i++){
-            rev=rev<<1;
             if((n&1)==1)
                 rev++;
             n=n>>1;
+            if(i!=31)
+            rev=rev<<1;
         }
             
         return rev;
