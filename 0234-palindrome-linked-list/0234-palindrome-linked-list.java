fc7cf87e38f594366c5b858e1c6d6b56e3e8baca
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-29 12:59:03 +0530

    Time: 6 ms (46.32%), Space: 55.2 MB (93.76%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -13,21 +13,30 @@ class Solution {
         if(head==null||head.next==null)
             return true;
         ListNode mid = getMid(head);
-        ListNode midNext = mid.next;
+        ListNode midNext = reverse(mid.next);
         mid.next=null;
         ListNode ptr1 = head;
-        ListNode ptr2 = reverse(midNext);
+        ListNode ptr2 = midNext;
         
         while(ptr1!=null&&ptr2!=null){
             if(ptr1.val!=ptr2.val){
+                mid.next=reverse(midNext);
                 return false;
             }
             ptr1=ptr1.next;
             ptr2=ptr2.next;
         }
         
-        
+        mid.next=reverse(midNext);
         return true;
+    }
+    
+    void printList(ListNode head){
+        ListNode curr= head;
+        while(curr!=null){
+            System.out.print(curr.val+"->");
+            curr=curr.next;
+        }
     }
     
     ListNode getMid(ListNode node){
