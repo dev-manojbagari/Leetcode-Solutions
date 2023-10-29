6e68b8073bdb6363ff3b84feedb80a2fcdb59a79
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-29 16:46:49 +0530

    Time: 1 ms (99.90%), Space: 48.7 MB (23.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,12 +3,12 @@ class Solution {
         int majElem = nums[0];
         int count =1;
         for(int i=1;i<nums.length;i++){
-            if(nums[i]==majElem){
+            if(majElem==nums[i]){
                 count++;
             }else{
                 count--;
                 if(count==0){
-                    majElem =nums[i];
+                    majElem=nums[i];
                     count=1;
                 }
             }
