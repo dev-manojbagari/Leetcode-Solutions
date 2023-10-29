5fb225e89715baf9ada647d993cc301d91e5f239
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-29 18:15:29 +0530

    Time: 1 ms (98.64%), Space: 59.6 MB (21.89%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,24 +3,26 @@ class Solution {
         int n = nums.length;
         for(int i=0;i<n;i++){
             while(1<=nums[i]&&nums[i]<=n){
-                if(nums[nums[i]-1]!= nums[i]){
+                if(nums[nums[i]-1]!=nums[i]){
                     swap(nums,nums[i]-1,i);
-                }else{
-                  break;  
-                }
+                }else
+                    break;
             }
         }
         
-        for(int i=0;i<n;i++)
+        
+        for(int i=0;i<n;i++){
             if(nums[i]!=i+1)
                 return i+1;
+        }
         
         return n+1;
     }
     
     void swap(int[] nums,int i,int j){
-        int temp = nums[i];
+        int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
+    
 }
