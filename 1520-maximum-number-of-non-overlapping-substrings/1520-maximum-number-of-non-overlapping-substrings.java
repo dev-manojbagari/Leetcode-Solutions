ff88b84c98cb39854bb0738b922c752f575b7fb6
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-30 09:08:07 +0530

    Time: 62 ms (22.73%), Space: 44.6 MB (45.45%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -3,47 +3,47 @@ class Solution {
         int start,end;
         Pair(int start,int end){
             this.start=start;
-            this.end=end;
+            this.end =end;
         }
     }
     public List<String> maxNumOfSubstrings(String s) {
+        List<String> list = new ArrayList<>();
         Map<Character,Pair> map = new HashMap<>();
         
         for(int i=0;i<s.length();i++){
-            if(!map.containsKey(s.charAt(i)))
-                map.put(s.charAt(i),new Pair(i,i));
+            char c = s.charAt(i);
+            if(!map.containsKey(c))
+                map.put(c,new Pair(i,i));
             else
-                map.get(s.charAt(i)).end=i;
+                map.get(c).end=i;
         }
         
         int prevEnd = -1;
-        List<String> list = new ArrayList<>();
         for(int i=0;i<s.length();i++){
             int start = map.get(s.charAt(i)).start;
             int end = map.get(s.charAt(i)).end;
             if(i==start){
-                int curEnd = getEnd(start,end,s,map);
+                int curEnd = getEnd(s,start,end,map);
                 if(curEnd==-1) continue;
-                String subStr =  s.substring(start,curEnd+1);
-                if(curEnd<=prevEnd){
+                String subStr= s.substring(start,curEnd+1);
+                if(curEnd<prevEnd){
                     list.set(list.size()-1,subStr);
-                }else{
+                }else
                     list.add(subStr);
-                }
-                prevEnd=curEnd;
+                
+                prevEnd = curEnd;
             }
         }
         
         return list;
     }
-    
-    int getEnd(int start,int end,String s,Map<Character,Pair> map){
+    int getEnd(String s,int start,int end,Map<Character,Pair> map){
         for(int i=start;i<end;i++){
             if(map.get(s.charAt(i)).start<start)
                 return -1;
             end = Math.max(end,map.get(s.charAt(i)).end);
         }
         
-        return end;        
+        return end;
     }
 }
