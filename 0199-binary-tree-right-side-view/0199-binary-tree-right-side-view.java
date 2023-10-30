0bcde606e652bb50f0cb9a94d23b3d0dc1fd78d7
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-10-30 15:25:02 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (65.09%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -16,23 +16,17 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        
         levelOrder(root,list,0);
-        
         return list;
     }
     
-    void levelOrder(TreeNode root,List<Integer> list,int level){
-        if(root==null)
-            return;
+     void levelOrder(TreeNode root,List<Integer> list ,int curLevel){
+            if(root==null)
+                return;
+            if(curLevel==list.size())
+                list.add(root.val);
         
-        if(list.size()==level){
-            list.add(root.val);
-        }
-        
-        
-        levelOrder(root.right,list,level+1);
-        levelOrder(root.left,list,level+1);
+            levelOrder(root.right,list,curLevel+1);
+            levelOrder(root.left,list,curLevel+1);
     }
-    
 }
